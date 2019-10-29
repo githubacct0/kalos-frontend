3e50928990def54d86438be8ec230c1dff00a1c4
githubacct0 <githubacct0@outlook.com>
2019-10-29 15:34:01 -0400

    service call detail screen

---
+++ modules/ServiceCallDetail/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ServiceCallDetail } from "./main";
+
+ReactDOM.render(
+  <ServiceCallDetail eventID={86091} />,
+  document.getElementById("root")
+);
