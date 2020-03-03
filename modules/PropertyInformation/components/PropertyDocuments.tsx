caf74b25f66d75f5171bb1132b54baa21be01b28
githubacct0 <githubacct0@outlook.com>
2020-03-03 15:30:21 +0100

    delete Property Document

--- modules/PropertyInformation/components/PropertyDocuments.tsx
+++ modules/PropertyInformation/components/PropertyDocuments.tsx
@@ -3,10 +3,11 @@ import IconButton from '@material-ui/core/IconButton';
 import MailIcon from '@material-ui/icons/Mail';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { DocumentClient, Document } from '@kalos-core/kalos-rpc/Document';
-import { S3Client, URLObject } from '@kalos-core/kalos-rpc/S3File';
+import { S3Client, URLObject, FileObject } from '@kalos-core/kalos-rpc/S3File';
 import { InfoTable, Data } from '../../ComponentsLibrary/InfoTable';
 import { SectionBar } from '../../ComponentsLibrary/SectionBar';
 import { Link } from '../../ComponentsLibrary/Link';
+import { ConfirmDelete } from '../../ComponentsLibrary/ConfirmDelete';
 import { ENDPOINT, ROWS_PER_PAGE } from '../../../constants';
 import { makeFakeRows } from '../../../helpers';
 
@@ -22,6 +23,7 @@ interface State {
   entries: Entry[];
   loading: boolean;
   error: boolean;
+  deleting?: Entry;
   count: number;
   page: number;
 }
@@ -35,6 +37,7 @@ export class PropertyDocuments extends PureComponent<Props, State> {
       entries: [],
       loading: true,
       error: false,
+      deleting: undefined,
       count: 0,
       page: 0,
     };
@@ -42,6 +45,7 @@ export class PropertyDocuments extends PureComponent<Props, State> {
   }
 
   load = async () => {
+    this.setState({ loading: true });
     const { userID, propertyId } = this.props;
     const entry = new Document();
     entry.setUserId(userID);
@@ -71,46 +75,85 @@ export class PropertyDocuments extends PureComponent<Props, State> {
     window.open(dlURL.url, '_blank');
   };
 
+  handleDelete = async () => {
+    const { deleting } = this.state;
+    if (deleting) {
+      try {
+        this.handleSetDeleting()();
+        this.setState({ loading: true });
+        const { filename, type } = deleting;
+        const S3 = new S3Client(ENDPOINT);
+        const file = new FileObject();
+        file.setKey(filename);
+        file.setBucket(type === 5 ? 'testbuckethelios' : 'kalosdocs-prod');
+        await S3.Delete(file);
+        const entry = new Document();
+        entry.setId(deleting.id);
+        await this.DocumentClient.Delete(entry);
+        await this.load();
+      } catch (e) {
+        this.setState({ error: true, loading: false });
+      }
+    }
+  };
+
   handleChangePage = (page: number) => {
     this.setState({ page }, this.load);
   };
 
+  handleSetDeleting = (deleting?: Entry) => () => this.setState({ deleting });
+
   render() {
-    const { props, state, handleDownload, handleChangePage } = this;
+    const {
+      props,
+      state,
+      handleDownload,
+      handleChangePage,
+      handleSetDeleting,
+      handleDelete,
+    } = this;
     const { className, userID, propertyId } = props;
-    const { entries, loading, error, count, page } = state;
+    const { entries, loading, error, count, page, deleting } = state;
     const data: Data = loading
       ? makeFakeRows()
-      : entries.map(({ id, filename, type, description: value }) => [
-          {
-            value: (
-              <Link href="" onClick={handleDownload(filename, type)}>
-                {value}
-              </Link>
-            ),
-            actions: [
-              <IconButton
-                key={0}
-                style={{ marginLeft: 4 }}
-                size="small"
-                onClick={() => {
-                  document.location.href = [
-                    '/index.cfm?action=admin:properties.docemail',
-                    `user_id=${userID}`,
-                    `document_id=${id}`,
-                    `property_id=${propertyId}`,
-                    `p=2`,
-                  ].join('&');
-                }}
-              >
-                <MailIcon />
-              </IconButton>,
-              <IconButton key={1} style={{ marginLeft: 4 }} size="small">
-                <DeleteIcon />
-              </IconButton>,
-            ],
-          },
-        ]);
+      : entries.map(entry => {
+          const { id, filename, type, description: value } = entry;
+          return [
+            {
+              value: (
+                <Link href="" onClick={handleDownload(filename, type)}>
+                  {value}
+                </Link>
+              ),
+              actions: [
+                <IconButton
+                  key={0}
+                  style={{ marginLeft: 4 }}
+                  size="small"
+                  onClick={() => {
+                    document.location.href = [
+                      '/index.cfm?action=admin:properties.docemail',
+                      `user_id=${userID}`,
+                      `document_id=${id}`,
+                      `property_id=${propertyId}`,
+                      `p=2`,
+                    ].join('&');
+                  }}
+                >
+                  <MailIcon />
+                </IconButton>,
+                <IconButton
+                  key={1}
+                  style={{ marginLeft: 4 }}
+                  size="small"
+                  onClick={handleSetDeleting(entry)}
+                >
+                  <DeleteIcon />
+                </IconButton>,
+              ],
+            },
+          ];
+        });
     return (
       <div className={className}>
         <SectionBar
@@ -140,6 +183,15 @@ export class PropertyDocuments extends PureComponent<Props, State> {
             hoverable
           />
         </SectionBar>
+        {deleting && (
+          <ConfirmDelete
+            open
+            onClose={handleSetDeleting()}
+            onConfirm={handleDelete}
+            kind="Property Document"
+            name={deleting.description}
+          />
+        )}
       </div>
     );
   }
