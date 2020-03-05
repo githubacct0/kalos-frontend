5f10ba4e41d41c086d79511b606808ada1cb500f
githubacct0 <githubacct0@outlook.com>
2020-03-05 19:07:17 +0100

    CustomerInformation: UserGroupLinks

--- modules/PropertyInformation/components/CustomerInformation.tsx
+++ modules/PropertyInformation/components/CustomerInformation.tsx
@@ -1,5 +1,9 @@
 import React, { FC, useState, useEffect, useCallback } from 'react';
 import { UserClient, User } from '@kalos-core/kalos-rpc/User';
+import {
+  UserGroupLinkClient,
+  UserGroupLink,
+} from '@kalos-core/kalos-rpc/UserGroupLink';
 import { makeStyles } from '@material-ui/core/styles';
 import { ENDPOINT, USA_STATES, BILLING_TERMS } from '../../../constants';
 import { InfoTable, Data } from '../../ComponentsLibrary/InfoTable';
@@ -7,11 +11,15 @@ import { Modal } from '../../ComponentsLibrary/Modal';
 import { Form, Schema } from '../../ComponentsLibrary/Form';
 import { SectionBar } from '../../ComponentsLibrary/SectionBar';
 import { ConfirmDelete } from '../../ComponentsLibrary/ConfirmDelete';
+import { Field, Value } from '../../ComponentsLibrary/Field';
 import { getRPCFields, formatDateTime } from '../../../helpers';
 
 const UserClientService = new UserClient(ENDPOINT);
+const UserGroupLinkClientService = new UserGroupLinkClient(ENDPOINT);
 
 type Entry = User.AsObject;
+
+type GroupLink = UserGroupLink.AsObject;
 
 const SCHEMA: Schema<Entry> = [
   [{ label: 'Personal Details', headline: true }],
@@ -93,6 +101,20 @@ const SCHEMA_PROPERTY_NOTIFICATION: Schema<Entry> = [
   ],
 ];
 
+const GROUPS = [
+  { id: 1, name: 'Residential Service' },
+  { id: 2, name: 'Commercial Service' },
+  { id: 3, name: 'Residential Construction' },
+  { id: 4, name: 'Commercial Construction' },
+  { id: 5, name: 'Commercial Contractor' },
+  { id: 6, name: 'Residential Contractor' },
+  { id: 7, name: 'Vacation Rental Manager' },
+  { id: 8, name: 'HOA Manager' },
+  { id: 9, name: 'Vendor' },
+  { id: 10, name: 'Villages' },
+  { id: 11, name: 'Managed Commercial' },
+];
+
 const useStyles = makeStyles(theme => ({
   wrapper: {
     display: 'flex',
@@ -118,6 +140,21 @@ const useStyles = makeStyles(theme => ({
   pendingBilling: {
     marginBottom: theme.spacing(),
   },
+  editForm: {
+    display: 'flex',
+  },
+  groups: {
+    width: 250,
+    marginLeft: theme.spacing(1),
+  },
+  groupLinks: {
+    paddingTop: theme.spacing(),
+    paddingBottom: theme.spacing(),
+    paddingLeft: theme.spacing(2),
+  },
+  group: {
+    marginBottom: 0,
+  },
 }));
 
 interface Props {
@@ -127,6 +164,8 @@ interface Props {
 
 export const CustomerInformation: FC<Props> = ({ userID, propertyId }) => {
   const [customer, setCustomer] = useState<Entry>(new User().toObject());
+  const [groupLinks, setGroupLinks] = useState<GroupLink[]>([]);
+  const [groupLinksInitial, setGroupLinksInitial] = useState<GroupLink[]>([]);
   const [editing, setEditing] = useState<boolean>(false);
   const [saving, setSaving] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
@@ -140,6 +179,13 @@ export const CustomerInformation: FC<Props> = ({ userID, propertyId }) => {
   const classes = useStyles();
 
   const load = useCallback(async () => {
+    const groupLink = new UserGroupLink();
+    groupLink.setUserId(userID);
+    const { resultsList: groupLinks } = (
+      await UserGroupLinkClientService.BatchGet(groupLink)
+    ).toObject();
+    setGroupLinks(groupLinks);
+    setGroupLinksInitial(groupLinks);
     const entry = new User();
     entry.setId(userID);
     try {
@@ -148,12 +194,14 @@ export const CustomerInformation: FC<Props> = ({ userID, propertyId }) => {
     } catch (e) {
       setError(true);
     }
-  }, [userID, setCustomer, setError]);
+  }, [userID, setCustomer, setError, setGroupLinks, setGroupLinksInitial]);
 
-  const handleToggleEditing = useCallback(() => setEditing(!editing), [
-    editing,
-    setEditing,
-  ]);
+  const handleToggleEditing = useCallback(() => {
+    setEditing(!editing);
+    if (!editing) {
+      setGroupLinks(groupLinksInitial);
+    }
+  }, [editing, setEditing, setGroupLinks, groupLinksInitial]);
 
   const handleSetNotificationEditing = useCallback(
     (notificationEditing: boolean) => () =>
@@ -172,6 +220,40 @@ export const CustomerInformation: FC<Props> = ({ userID, propertyId }) => {
     [setDeleting],
   );
 
+  const saveGroupLinks = useCallback(
+    async (groupLinks: GroupLink[], groupLinksInitial: GroupLink[]) => {
+      const actions: {
+        operation: 'Create' | 'Delete';
+        entry: UserGroupLink;
+      }[] = [];
+      for (let i = 0; i < GROUPS.length; i += 1) {
+        const id = GROUPS[i].id;
+        const isInGroupLinks = groupLinks.find(({ groupId }) => groupId === id);
+        const isInGroupLinksInitial = groupLinksInitial.find(
+          ({ groupId }) => groupId === id,
+        );
+        const entry = new UserGroupLink();
+        if (isInGroupLinksInitial && !isInGroupLinks) {
+          entry.setId(isInGroupLinksInitial.id);
+          actions.push({ operation: 'Delete', entry });
+        }
+        if (!isInGroupLinksInitial && isInGroupLinks) {
+          entry.setUserId(userID);
+          entry.setGroupId(id);
+          actions.push({ operation: 'Create', entry });
+        }
+      }
+      await Promise.all(
+        actions.map(
+          async ({ operation, entry }) =>
+            await UserGroupLinkClientService[operation](entry),
+        ),
+      );
+      setGroupLinksInitial(groupLinks);
+    },
+    [userID, setGroupLinksInitial],
+  );
+
   const handleSave = useCallback(
     async (data: Entry) => {
       setSaving(true);
@@ -187,11 +269,20 @@ export const CustomerInformation: FC<Props> = ({ userID, propertyId }) => {
       entry.setFieldMaskList(fieldMaskList);
       const customer = await UserClientService.Update(entry);
       setCustomer(customer);
+      await saveGroupLinks(groupLinks, groupLinksInitial);
       setSaving(false);
       setEditing(false);
       handleSetNotificationEditing(false)();
     },
-    [setSaving, userID, setCustomer, setEditing, handleSetNotificationEditing],
+    [
+      setSaving,
+      userID,
+      setCustomer,
+      setEditing,
+      handleSetNotificationEditing,
+      groupLinks,
+      groupLinksInitial,
+    ],
   );
 
   const handleDelete = useCallback(async () => {
@@ -210,6 +301,19 @@ export const CustomerInformation: FC<Props> = ({ userID, propertyId }) => {
       setNotificationViewing(true);
     }
   }, [customer, load, setNotificationViewing]);
+
+  const handleChangeLinkGroup = useCallback(
+    (groupId: number) => (value: Value) => {
+      const newGroupLink = new UserGroupLink();
+      newGroupLink.setGroupId(groupId);
+      newGroupLink.setUserId(userID);
+      const newGroupLinks = value
+        ? [...groupLinks, newGroupLink.toObject()]
+        : groupLinks.filter(item => item.groupId !== groupId);
+      setGroupLinks(newGroupLinks);
+    },
+    [groupLinks, userID, setGroupLinks],
+  );
 
   const {
     id,
@@ -352,14 +456,35 @@ export const CustomerInformation: FC<Props> = ({ userID, propertyId }) => {
         </div>
       </SectionBar>
       <Modal open={editing} onClose={handleToggleEditing}>
-        <Form<Entry>
-          title="Edit Customer Information"
-          schema={SCHEMA}
-          data={customer}
-          onSave={handleSave}
-          onClose={handleToggleEditing}
-          disabled={saving}
-        />
+        <div className={classes.editForm}>
+          <Form<Entry>
+            title="Edit Customer Information"
+            schema={SCHEMA}
+            data={customer}
+            onSave={handleSave}
+            onClose={handleToggleEditing}
+            disabled={saving}
+          />
+          <div className={classes.groups}>
+            <SectionBar title="Mailing lists" />
+            <div className={classes.groupLinks}>
+              {GROUPS.map(({ id, name }) => (
+                <Field
+                  key={id}
+                  label={name}
+                  type="checkbox"
+                  onChange={handleChangeLinkGroup(id)}
+                  value={
+                    groupLinks.find(({ groupId }) => groupId === id) ? 1 : 0
+                  }
+                  name={`group_${id}`}
+                  className={classes.group}
+                  disabled={saving}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
       </Modal>
       <Modal
         open={notificationEditing || notificationViewing}
