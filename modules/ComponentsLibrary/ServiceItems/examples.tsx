883a89c12d9d2d9de8caf633fa8162a1135e7fea
githubacct0 <githubacct0@outlook.com>
2020-03-13 21:25:57 +0100

    ComponentsLibrary: ServiceItems example

--- modules/ComponentsLibrary/ServiceItems/examples.tsx
+++ modules/ComponentsLibrary/ServiceItems/examples.tsx
@@ -6,6 +6,7 @@ export default () => (
     <ServiceItems userID={2573} propertyId={6552} loggedUserId={101253} />
     <hr />
     <ServiceItems
+      title="Lorem Ipsum"
       userID={2573}
       propertyId={6552}
       loggedUserId={101253}
