cb2e677a9d0a061179c6bc5a31b91eb50c6b97e8
githubacct0 <githubacct0@outlook.com>
2020-05-26 19:25:00 +0200

    ComponentsLibrary: CalendarHeader

--- modules/ComponentsLibrary/CalendarHeader/examples.tsx
+++ modules/ComponentsLibrary/CalendarHeader/examples.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import { startOfWeek } from 'date-fns';
+import Typography from '@material-ui/core/Typography';
 import { CalendarHeader } from './';
+import { ExampleTitle } from '../helpers';
 
 export default () => (
   <>
-    <CalendarHeader />
+    <ExampleTitle>Default</ExampleTitle>
+    <CalendarHeader
+      selectedDate={startOfWeek(new Date())}
+      userName="John Smith"
+      onDateChange={console.log}
+      onSubmit={() => console.log('SUBMIT')}
+    />
+    <ExampleTitle>with submitLabel children</ExampleTitle>
+    <CalendarHeader
+      selectedDate={startOfWeek(new Date())}
+      userName="John Smith"
+      onDateChange={console.log}
+      onSubmit={() => console.log('SUBMIT')}
+      submitLabel="Approve Entries"
+    >
+      <Typography variant="subtitle2">
+        Total: <strong>135</strong>
+      </Typography>
+      <Typography variant="subtitle2">
+        Subtotal: <strong>82</strong>
+      </Typography>
+    </CalendarHeader>
+    <ExampleTitle>with submitDisabled</ExampleTitle>
+    <CalendarHeader
+      selectedDate={startOfWeek(new Date())}
+      userName="John Smith"
+      onDateChange={console.log}
+      onSubmit={() => console.log('SUBMIT')}
+      submitDisabled
+    />
   </>
 );
--- modules/ComponentsLibrary/CalendarHeader/index.tsx
+++ modules/ComponentsLibrary/CalendarHeader/index.tsx
@@ -1,11 +1,104 @@
 import React, { FC } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import MuiToolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Button } from '../Button';
+import { WeekPicker } from '../WeekPicker';
 
-interface Props {}
+interface Props {
+  selectedDate: Date;
+  userName: string;
+  onDateChange: (value: Date) => void;
+  onSubmit: () => void;
+  submitLabel?: string;
+  submitDisabled?: boolean;
+}
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme =>
+  createStyles({
+    bar: {
+      background: theme.palette.primary.dark,
+      color: theme.palette.primary.contrastText,
+      justifyContent: 'space-between',
+      flexWrap: 'wrap',
+      [theme.breakpoints.down('sm')]: {
+        padding: theme.spacing(2),
+      },
+      [theme.breakpoints.down(400)]: {
+        flexDirection: 'column',
+        alignItems: 'stretch',
+      },
+    },
+    userName: {
+      margin: `0 ${theme.spacing(2)}px`,
+      textAlign: 'center',
+      [theme.breakpoints.down(400)]: {
+        margin: theme.spacing(2),
+      },
+    },
+    info: {
+      flex: '1 0 auto',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      '& p': {
+        color: 'white',
+      },
+      [theme.breakpoints.down(400)]: {
+        flexDirection: 'column',
+      },
+    },
+    children: {
+      marginRight: theme.spacing(2),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-end',
+      flexWrap: 'wrap',
+      [theme.breakpoints.down(400)]: {
+        marginRight: 0,
+        alignItems: 'center',
+      },
+    },
+  }),
+);
 
-export const CalendarHeader: FC<Props> = ({}) => {
+export const CalendarHeader: FC<Props> = ({
+  selectedDate,
+  userName,
+  onDateChange,
+  onSubmit,
+  submitLabel = 'Submit',
+  submitDisabled = false,
+  children,
+}) => {
   const classes = useStyles();
-  return <div>CalendarHeader</div>;
+  return (
+    <MuiToolbar className={classes.bar}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <WeekPicker
+          white
+          label="Set a Period"
+          inputVariant="outlined"
+          size="small"
+          value={selectedDate}
+          onChange={onDateChange}
+        />
+        <Typography className={classes.userName} variant="subtitle1">
+          {userName}
+        </Typography>
+        <Box className={classes.info}>
+          <Box className={classes.children}>{children}</Box>
+          <Button
+            onClick={onSubmit}
+            label={submitLabel}
+            disabled={submitDisabled}
+          />
+        </Box>
+      </MuiPickersUtilsProvider>
+    </MuiToolbar>
+  );
 };
