39f23a996751f8aa9773eb55c7afbe96f916d585
githubacct0 <githubacct0@outlook.com>
2020-05-26 18:17:40 +0200

    ComponentsLibrary: CalendarCard

--- modules/ComponentsLibrary/CalendarCard/examples.tsx
+++ modules/ComponentsLibrary/CalendarCard/examples.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { CalendarCard } from './';
+import { LoremIpsum } from '../helpers';
 
 export default () => (
   <>
-    <CalendarCard />
+    <CalendarCard
+      title="Lorem ipsum"
+      statusColor="#FF8888"
+      onClick={() => console.log('CLICK')}
+    >
+      <LoremIpsum />
+    </CalendarCard>
+    <CalendarCard
+      title="Dolor sit"
+      statusColor="#88FF88"
+      onClick={() => console.log('CLICK')}
+    >
+      <LoremIpsum />
+    </CalendarCard>
   </>
 );
--- modules/ComponentsLibrary/CalendarCard/index.tsx
+++ modules/ComponentsLibrary/CalendarCard/index.tsx
@@ -1,11 +1,57 @@
 import React, { FC } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
 
-interface Props {}
+interface Props {
+  title: string;
+  statusColor: string;
+  onClick: () => void;
+}
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme => ({
+  card: {
+    margin: `${theme.spacing(1)}px 0`,
+    roundColor: theme.palette.primary.light,
+  },
+  cardHeader: {
+    padding: theme.spacing(1),
+  },
+  cardContent: {
+    padding: `0 ${theme.spacing(1)}px ${theme.spacing(1)}px`,
+  },
+  colorIndicator: {
+    display: 'block',
+    width: theme.spacing(2),
+    height: theme.spacing(2),
+    borderRadius: theme.spacing(1),
+  },
+}));
 
-export const CalendarCard: FC<Props> = ({}) => {
+export const CalendarCard: FC<Props> = ({
+  title,
+  statusColor,
+  onClick,
+  children,
+}) => {
   const classes = useStyles();
-  return <div>CalendarCard</div>;
+  return (
+    <Card className={classes.card} onClick={onClick}>
+      <CardActionArea>
+        <CardHeader
+          className={classes.cardHeader}
+          avatar={
+            <span
+              className={classes.colorIndicator}
+              style={{ backgroundColor: statusColor }}
+            />
+          }
+          title={title}
+        />
+        <CardContent className={classes.cardContent}>{children}</CardContent>
+      </CardActionArea>
+    </Card>
+  );
 };
