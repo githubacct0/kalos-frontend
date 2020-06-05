265e3cbfe5d3402e2dec5ce76af513f80dc2e9cf
githubacct0 <githubacct0@outlook.com>
2020-06-05 21:00:13 +0200

    PrintHeaderSubtitleItem example

--- modules/ComponentsLibrary/PrintHeader/examples.tsx
+++ modules/ComponentsLibrary/PrintHeader/examples.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import { PrintHeader } from './';
+import { PrintHeader, PrintHeaderSubtitleItem } from './';
 
 export default () => (
   <>
-    <PrintHeader title="Lorem ipsum" />
+    <PrintHeader
+      title="Lorem ipsum"
+      subtitle={
+        <>
+          {[...Array(5)].map((_, idx) => (
+            <PrintHeaderSubtitleItem
+              key={idx}
+              label={`Label ${idx + 1}:`}
+              value={`value ${idx + 1}`}
+            />
+          ))}
+        </>
+      }
+    />
   </>
 );
