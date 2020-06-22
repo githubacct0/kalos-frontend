c453d54303c5732974c5c08d41e4aa011f95a27d
githubacct0 <githubacct0@outlook.com>
2020-06-22 20:08:04 +0200

    SpiffReport filter form

--- modules/ComponentsLibrary/Reports/index.tsx
+++ modules/ComponentsLibrary/Reports/index.tsx
@@ -67,7 +67,9 @@ const UNDER_CONSTRUCTION = (
   <div style={{ padding: 10, fontFamily: 'arial' }}>Under construction...</div>
 );
 
-const WEEK_OPTIONS = getWeekOptions(52, -31).reverse();
+const WEEK_OPTIONS_0 = getWeekOptions(52, -31).reverse();
+const WEEK_OPTIONS_1 = getWeekOptions(52);
+
 const LAST_12_MONTHS_0 = makeLast12MonthsOptions(false);
 const LAST_12_MONTHS_1 = makeLast12MonthsOptions(false, -1);
 
@@ -176,7 +178,7 @@ const SCHEMA_SERVICE_CALL_METRICS_REPORT: Schema<FilterForm> = [
     {
       name: 'week',
       label: 'Select Week',
-      options: WEEK_OPTIONS,
+      options: WEEK_OPTIONS_0,
       required: true,
     },
   ],
@@ -583,7 +585,7 @@ export const Reports: FC<Props> = ({ loggedUserId }) => {
           {
             name: 'week',
             label: 'Select Report Date',
-            options: WEEK_OPTIONS,
+            options: WEEK_OPTIONS_1,
             required: true,
           },
         ],
