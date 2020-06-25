4095a84c4247cc899c5ae1229be5e56774bf9fd8
githubacct0 <githubacct0@outlook.com>
2020-06-25 20:32:49 +0200

    Print css fix

--- modules/ComponentsLibrary/PrintTable/index.tsx
+++ modules/ComponentsLibrary/PrintTable/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ReactNode, CSSProperties } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import './styles.css';
 
 type Style = {
   noBorders?: boolean;
@@ -18,38 +19,6 @@ interface Props extends Style {
   equalColWidths?: boolean;
 }
 
-const useStyles = makeStyles(theme => {
-  const border = ({ noBorders }: Style): CSSProperties => ({
-    borderBottomWidth: noBorders ? 0 : 1,
-    borderBottomStyle: noBorders ? 'none' : 'solid',
-    borderBottomColor: noBorders
-      ? theme.palette.common.white
-      : theme.palette.grey[300],
-  });
-  return {
-    table: style => ({
-      width: '100%',
-      ...theme.typography.body1,
-      fontSize: 10,
-      ...border(style),
-      borderCollapse: 'collapse',
-      marginBottom: theme.spacing(),
-    }),
-    th: style => ({
-      ...border(style),
-      borderBottomColor: theme.palette.grey[500],
-      textAlign: 'left',
-      padding: theme.spacing(0.25),
-      verticalAlign: 'top',
-    }),
-    td: style => ({
-      ...border(style),
-      padding: theme.spacing(0.25),
-      verticalAlign: 'top',
-    }),
-  };
-});
-
 export const PrintTable: FC<Props> = ({
   columns,
   nowraps = [],
@@ -61,15 +30,16 @@ export const PrintTable: FC<Props> = ({
   styles = {},
   equalColWidths = false,
 }) => {
-  const classes = useStyles({ noBorders });
   return (
-    <table className={className + ' ' + classes.table} style={styles}>
+    <table
+      className={clsx('PrintTable', className, { noBorders })}
+      style={styles}
+    >
       <thead>
         <tr>
           {columns.map((column, idxColumn) => (
             <th
               key={idxColumn}
-              className={classes.th}
               style={{
                 ...(typeof column === 'object'
                   ? { textAlign: column.align }
@@ -91,7 +61,6 @@ export const PrintTable: FC<Props> = ({
             {cells.map((cell, idxColumn) => (
               <td
                 key={idxColumn}
-                className={classes.td}
                 style={{
                   ...(typeof columns[idxColumn] === 'object'
                     ? { textAlign: (columns[idxColumn] as Column).align }
