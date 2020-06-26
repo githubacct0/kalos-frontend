6febfd4e64600b6d86f5b9df2019028db23566e2
githubacct0 <githubacct0@outlook.com>
2020-06-26 16:12:13 +0200

    PrintHeader css

--- modules/ComponentsLibrary/PrintHeader/index.tsx
+++ modules/ComponentsLibrary/PrintHeader/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, ReactNode } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import './styles.css';
 
 export interface Props {
   title: string;
@@ -12,62 +12,25 @@ interface SubtitleItemProps {
   value: ReactNode;
 }
 
-const useStyles = makeStyles(theme => ({
-  wrapper: {
-    display: 'flex',
-    alignItems: 'center',
-    marginBottom: theme.spacing(),
-  },
-  logo: {
-    width: 'auto',
-    height: 30,
-    filter: 'invert()',
-  },
-  content: {
-    flexGrow: 1,
-    marginLeft: theme.spacing(2),
-    textAlign: 'center',
-  },
-  title: {
-    ...theme.typography.body1,
-    fontSize: 20,
-    fontWeight: 900,
-  },
-  subtitle: {
-    fontFamily: theme.typography.body1.fontFamily,
-    fontSize: 10,
-  },
-  subtitleItem: {
-    display: 'inline-block',
-    marginRight: theme.spacing(2),
-  },
-}));
-
-export const PrintHeader: FC<Props> = ({ title, subtitle }) => {
-  const classes = useStyles();
-  return (
-    <div className={classes.wrapper}>
-      <img
-        src="https://app.kalosflorida.com/app/assets/images/kalos-logo-new.png"
-        alt="Kalos Service"
-        className={classes.logo}
-      />
-      <div className={classes.content}>
-        <div className={classes.title}>{title}</div>
-        <div className={classes.subtitle}>{subtitle}</div>
-      </div>
+export const PrintHeader: FC<Props> = ({ title, subtitle }) => (
+  <div className="PrintHeader">
+    <img
+      src="https://app.kalosflorida.com/app/assets/images/kalos-logo-new.png"
+      alt="Kalos Service"
+      className="PrintHeader_logo"
+    />
+    <div className="PrintHeader_content">
+      <div className="PrintHeader_title">{title}</div>
+      {subtitle && <div className="PrintHeader_subtitle">{subtitle}</div>}
     </div>
-  );
-};
+  </div>
+);
 
 export const PrintHeaderSubtitleItem: FC<SubtitleItemProps> = ({
   label,
   value,
-}) => {
-  const classes = useStyles();
-  return (
-    <span className={classes.subtitleItem}>
-      {label} <strong>{value}</strong>
-    </span>
-  );
-};
+}) => (
+  <span className="PrintHeader_subtitle_item">
+    {label} <strong>{value}</strong>
+  </span>
+);
