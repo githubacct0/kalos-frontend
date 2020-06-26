97608668d4af9b0ddb4c605779461b5442b473bf
githubacct0 <githubacct0@outlook.com>
2020-06-26 16:14:40 +0200

    PrintList css

--- modules/ComponentsLibrary/PrintList/index.tsx
+++ modules/ComponentsLibrary/PrintList/index.tsx
@@ -1,32 +1,16 @@
 import React, { FC, ReactNode } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import './styles.css';
 
 export interface Props {
   items: ReactNode[];
 }
 
-const useStyles = makeStyles(theme => ({
-  wrapper: {
-    fontFamily: theme.typography.body1.fontFamily,
-    fontSize: 10,
-    marginTop: theme.spacing(),
-    marginBottom: theme.spacing(),
-    paddingLeft: theme.spacing(2.5),
-  },
-  item: {
-    marginBottom: theme.spacing(0.5),
-  },
-}));
-
-export const PrintList: FC<Props> = ({ items }) => {
-  const classes = useStyles();
-  return (
-    <ul className={classes.wrapper}>
-      {items.map((item, idx) => (
-        <li key={idx} className={classes.item}>
-          {item}
-        </li>
-      ))}
-    </ul>
-  );
-};
+export const PrintList: FC<Props> = ({ items }) => (
+  <ul className="PrintList">
+    {items.map((item, idx) => (
+      <li key={idx} className="PrintList_item">
+        {item}
+      </li>
+    ))}
+  </ul>
+);
