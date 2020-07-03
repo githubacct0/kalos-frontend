2b7893d5b993b504d6455a28f47933f4241393fc
githubacct0 <githubacct0@outlook.com>
2020-07-03 16:56:56 +0200

    QuoteSelector examples

--- modules/ComponentsLibrary/QuoteSelector/examples.tsx
+++ modules/ComponentsLibrary/QuoteSelector/examples.tsx
@@ -1,4 +1,12 @@
 import React from 'react';
 import { QuoteSelector } from './';
+import { ExampleTitle } from '../helpers';
 
-export default () => <QuoteSelector />;
+export default () => (
+  <>
+    <ExampleTitle>default</ExampleTitle>
+    <QuoteSelector serviceCallId={86246} />
+    <ExampleTitle>onAdd</ExampleTitle>
+    <QuoteSelector serviceCallId={86246} onAdd={console.log} />
+  </>
+);
