903fa729ed2f17611f45f683dae04ec3a908229e
githubacct0 <githubacct0@outlook.com>
2020-07-22 16:48:24 +0200

    Styles refactoring: CalendarHeader

--- modules/ComponentsLibrary/CalendarHeader/index.tsx
+++ modules/ComponentsLibrary/CalendarHeader/index.tsx
@@ -1,13 +1,13 @@
 import React, { FC } from 'react';
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
 import MuiToolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { Button } from '../Button';
 import { WeekPicker } from '../WeekPicker';
 import { Actions, ActionsProps } from '../Actions';
+import './styles.less';
 
 interface Props {
   selectedDate: Date;
@@ -20,57 +20,6 @@ interface Props {
   actions?: ActionsProps;
 }
 
-const RWD_WIDTH = 750;
-
-const useStyles = makeStyles(theme =>
-  createStyles({
-    bar: {
-      background: theme.palette.primary.dark,
-      color: theme.palette.primary.contrastText,
-      justifyContent: 'space-between',
-      flexWrap: 'wrap',
-      [theme.breakpoints.down('sm')]: {
-        padding: theme.spacing(2),
-      },
-      [theme.breakpoints.down(RWD_WIDTH)]: {
-        flexDirection: 'column',
-        alignItems: 'stretch',
-      },
-    },
-    title: {
-      margin: `0 ${theme.spacing(2)}px`,
-      textAlign: 'center',
-      [theme.breakpoints.down(RWD_WIDTH)]: {
-        margin: theme.spacing(2),
-      },
-    },
-    info: {
-      flex: '1 0 auto',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'flex-end',
-      alignItems: 'center',
-      '& p': {
-        color: 'white',
-      },
-      [theme.breakpoints.down(RWD_WIDTH)]: {
-        flexDirection: 'column',
-      },
-    },
-    children: {
-      marginRight: theme.spacing(2),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'flex-end',
-      flexWrap: 'wrap',
-      [theme.breakpoints.down(RWD_WIDTH)]: {
-        marginRight: 0,
-        alignItems: 'center',
-      },
-    },
-  }),
-);
-
 export const CalendarHeader: FC<Props> = ({
   selectedDate,
   title,
@@ -81,35 +30,32 @@ export const CalendarHeader: FC<Props> = ({
   weekStartsOn = 0,
   actions,
   children,
-}) => {
-  const classes = useStyles();
-  return (
-    <MuiToolbar className={classes.bar}>
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <WeekPicker
-          white
-          label="Set a Period"
-          inputVariant="outlined"
-          size="small"
-          value={selectedDate}
-          onChange={onDateChange}
-          weekStartsOn={weekStartsOn}
-        />
-        <Typography className={classes.title} variant="subtitle1">
-          {title}
-        </Typography>
-        <Box className={classes.info}>
-          <Box className={classes.children}>{children}</Box>
-          {onSubmit && (
-            <Button
-              onClick={onSubmit}
-              label={submitLabel}
-              disabled={submitDisabled}
-            />
-          )}
-          {actions && <Actions actions={actions} fixed />}
-        </Box>
-      </MuiPickersUtilsProvider>
-    </MuiToolbar>
-  );
-};
+}) => (
+  <MuiToolbar className="CalendarHeader">
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <WeekPicker
+        white
+        label="Set a Period"
+        inputVariant="outlined"
+        size="small"
+        value={selectedDate}
+        onChange={onDateChange}
+        weekStartsOn={weekStartsOn}
+      />
+      <Typography className="CalendarHeaderTitle" variant="subtitle1">
+        {title}
+      </Typography>
+      <Box className="CalendarHeaderInfo">
+        <Box className="CalendarHeaderChildren">{children}</Box>
+        {onSubmit && (
+          <Button
+            onClick={onSubmit}
+            label={submitLabel}
+            disabled={submitDisabled}
+          />
+        )}
+        {actions && <Actions actions={actions} fixed />}
+      </Box>
+    </MuiPickersUtilsProvider>
+  </MuiToolbar>
+);
