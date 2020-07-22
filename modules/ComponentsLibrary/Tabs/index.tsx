4aaf86d18062fe722448be5c0b47e19d4a3600d9
githubacct0 <githubacct0@outlook.com>
2020-07-22 18:06:46 +0200

    Styles refactoring: Tabs

--- modules/ComponentsLibrary/Tabs/index.tsx
+++ modules/ComponentsLibrary/Tabs/index.tsx
@@ -2,7 +2,6 @@ import React, { FC, useState, useCallback, ReactNode } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import TabsUI from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import { makeStyles } from '@material-ui/core/styles';
 
 interface Props {
   tabs: {
@@ -13,15 +12,12 @@ interface Props {
   onChange?: (idx: number) => void;
 }
 
-const useStyles = makeStyles(theme => ({}));
-
 const a11yProps = (idx: number) => ({
   id: `scrollable-auto-tab-${idx}`,
   'aria-controls': `scrollable-auto-tabpanel-${idx}`,
 });
 
 export const Tabs: FC<Props> = ({ tabs, defaultOpenIdx = 0, onChange }) => {
-  const classes = useStyles();
   const [tabIdx, setTabIdx] = useState<number>(defaultOpenIdx);
   const handleChange = useCallback(
     (_, newValue: number) => {
