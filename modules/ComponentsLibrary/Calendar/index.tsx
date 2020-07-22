189091134bef750ccb60e6d0abd8171b233f1878
githubacct0 <githubacct0@outlook.com>
2020-07-22 16:56:53 +0200

    Styles refactoring: Calendar

--- modules/ComponentsLibrary/Calendar/index.tsx
+++ modules/ComponentsLibrary/Calendar/index.tsx
@@ -1,37 +1,23 @@
 import React, { FC } from 'react';
+import clsx from 'clsx';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Alert from '@material-ui/lab/Alert';
-import { makeStyles } from '@material-ui/core/styles';
+import './styles.less';
 
 interface Props {
   error?: string;
   className?: string;
 }
 
-const useStyles = makeStyles(theme => ({
-  wrapper: {
-    overflow: 'auto',
-  },
-  week: {
-    minWidth: 1500,
-    display: 'grid',
-    gridTemplateColumns: 'repeat(7, minmax(0, 1fr))',
-    gridGap: theme.spacing(2),
-  },
-}));
-
-export const Calendar: FC<Props> = ({ error, className, children }) => {
-  const classes = useStyles();
-  return (
-    <Box className={classes.wrapper}>
-      {error ? (
-        <Alert severity="error">{error}</Alert>
-      ) : (
-        <Container className={className + ' ' + classes.week} maxWidth={false}>
-          {children}
-        </Container>
-      )}
-    </Box>
-  );
-};
+export const Calendar: FC<Props> = ({ error, className, children }) => (
+  <Box className="Calendar">
+    {error ? (
+      <Alert severity="error">{error}</Alert>
+    ) : (
+      <Container className={clsx('CalendarWeek', className)} maxWidth={false}>
+        {children}
+      </Container>
+    )}
+  </Box>
+);
