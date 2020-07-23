95e4f0a30941905b61bcd128925f5e25d0375062
githubacct0 <githubacct0@outlook.com>
2020-07-23 21:39:49 +0200

    Styles refactoring: Tooltip

--- modules/ComponentsLibrary/Tooltip/index.tsx
+++ modules/ComponentsLibrary/Tooltip/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC, ReactElement } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import TooltipUI, { TooltipProps } from '@material-ui/core/Tooltip';
 import Fade from '@material-ui/core/Fade';
+import './styles.less';
 
 type Style = {
   maxWidth?: number;
@@ -11,36 +11,21 @@ type Props = Style &
     content: string | ReactElement;
   };
 
-const useStyles = makeStyles(theme => ({
-  tooltip: ({ maxWidth }: Style) => ({
-    ...theme.typography.body1,
-    backgroundColor: theme.palette.common.white,
-    color: theme.palette.common.black,
-    borderRadius: 0,
-    boxSizing: 'content-box',
-    margin: 4,
-    boxShadow: theme.shadows[5],
-    maxWidth,
-  }),
-}));
-
 export const Tooltip: FC<Props> = ({
   content,
   placement = 'left',
   children,
   maxWidth = 220,
-}) => {
-  const classes = useStyles({ maxWidth });
-  return (
-    <TooltipUI // TODO: make it clickable for mobile devices
-      title={content}
-      placement={placement}
-      classes={{ tooltip: classes.tooltip }}
-      enterTouchDelay={50}
-      leaveTouchDelay={60000}
-      TransitionComponent={Fade}
-    >
-      {children}
-    </TooltipUI>
-  );
-};
+}) => (
+  <TooltipUI // TODO: make it clickable for mobile devices
+    title={content}
+    placement={placement}
+    classes={{ tooltip: 'Tooltip' }}
+    enterTouchDelay={50}
+    leaveTouchDelay={60000}
+    TransitionComponent={Fade}
+    style={{ maxWidth }}
+  >
+    {children}
+  </TooltipUI>
+);
