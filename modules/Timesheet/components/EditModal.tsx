121ace606bb61cbcd7961d8685c9e0c6cf7ffc6b
githubacct0 <githubacct0@outlook.com>
2020-07-27 20:27:42 +0200

    Styles refactoring: Timesheet EditModal

--- modules/Timesheet/components/EditModal.tsx
+++ modules/Timesheet/components/EditModal.tsx
@@ -1,9 +1,11 @@
 import React, { FC, useCallback, useState } from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { format, roundToNearestMinutes } from 'date-fns';
 import { Button } from '../../ComponentsLibrary/Button';
-import { TimesheetLine, TimesheetLineClient } from '@kalos-core/kalos-rpc/TimesheetLine';
+import {
+  TimesheetLine,
+  TimesheetLineClient,
+} from '@kalos-core/kalos-rpc/TimesheetLine';
 import {
   ServicesRenderedClient,
   ServicesRendered,
@@ -13,68 +15,119 @@ import { Form, Schema } from '../../ComponentsLibrary/Form';
 import { useConfirm } from '../../ComponentsLibrary/ConfirmService';
 import { getRPCFields } from '../../../helpers';
 import { ENDPOINT } from '../../../constants';
+import './editModal.less';
 
 const srClient = new ServicesRenderedClient(ENDPOINT);
 const tslClient = new TimesheetLineClient(ENDPOINT);
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    buttonGroup: {
-      display: 'flex',
-      justifyContent: 'center',
-    },
-  }),
-);
-
 type Entry = TimesheetLine.AsObject;
 
 interface EntryWithDate extends Entry {
-  date?: string,
+  date?: string;
 }
 
 type Props = {
   entry: EntryWithDate;
-  timesheetOwnerId: number,
-  userId: number,
-  timesheetAdministration: boolean,
-  create?: boolean,
-  action: 'create' | 'update' | 'convert' | 'delete' | 'approve' | 'reject' | '',
-  onSave: (entry: TimesheetLine.AsObject, action?: 'delete' | 'approve' | 'reject') => void;
+  timesheetOwnerId: number;
+  userId: number;
+  timesheetAdministration: boolean;
+  create?: boolean;
+  action:
+    | 'create'
+    | 'update'
+    | 'convert'
+    | 'delete'
+    | 'approve'
+    | 'reject'
+    | '';
+  onSave: (
+    entry: TimesheetLine.AsObject,
+    action?: 'delete' | 'approve' | 'reject',
+  ) => void;
   onClose: () => void;
 };
 
-const EditTimesheetModal: FC<Props> = ({ entry, timesheetOwnerId, userId, timesheetAdministration, action, onSave, onClose }): JSX.Element => {
+const EditTimesheetModal: FC<Props> = ({
+  entry,
+  timesheetOwnerId,
+  userId,
+  timesheetAdministration,
+  action,
+  onSave,
+  onClose,
+}): JSX.Element => {
   const confirm = useConfirm();
-  const classes = useStyles();
   const [saving, setSaving] = useState<boolean>(false);
   const SCHEMA: Schema<EntryWithDate> = [
-    [{ label: 'Reference', name: 'referenceNumber' },],
-    [{ label: 'Brief Description', name: 'briefDescription' },],
-    [{ label: 'Class Code', name: 'classCodeId', type: 'classCode', required: true},],
-    [{ label: 'Department', name: 'departmentCode', type: 'department', required: true},],
+    [{ label: 'Reference', name: 'referenceNumber' }],
+    [{ label: 'Brief Description', name: 'briefDescription' }],
+    [
+      {
+        label: 'Class Code',
+        name: 'classCodeId',
+        type: 'classCode',
+        required: true,
+      },
+    ],
+    [
+      {
+        label: 'Department',
+        name: 'departmentCode',
+        type: 'department',
+        required: true,
+      },
+    ],
     [
       { label: 'Date', name: 'date', type: 'mui-date', required: true },
-      { label: 'Started', name: 'timeStarted', type: 'mui-time', required: true },
-      { label: 'Finished', name: 'timeFinished', type: 'mui-time', required: true},
+      {
+        label: 'Started',
+        name: 'timeStarted',
+        type: 'mui-time',
+        required: true,
+      },
+      {
+        label: 'Finished',
+        name: 'timeFinished',
+        type: 'mui-time',
+        required: true,
+      },
     ],
-    [{ label: 'Notes', name: 'notes', multiline: true },],
+    [{ label: 'Notes', name: 'notes', multiline: true }],
   ];
   const { id = 0 } = entry;
-  const data = {...entry};
+  const data = { ...entry };
   if (data.timeStarted) {
     data.date = format(new Date(data.timeStarted), 'yyyy-MM-dd');
-    data.timeStarted = format(roundToNearestMinutes(new Date(data.timeStarted), {nearestTo: 15}), 'yyyy-MM-dd HH:mm');
-    data.timeFinished = format(roundToNearestMinutes(new Date(data.timeFinished), {nearestTo: 15}), 'yyyy-MM-dd HH:mm');
+    data.timeStarted = format(
+      roundToNearestMinutes(new Date(data.timeStarted), { nearestTo: 15 }),
+      'yyyy-MM-dd HH:mm',
+    );
+    data.timeFinished = format(
+      roundToNearestMinutes(new Date(data.timeFinished), { nearestTo: 15 }),
+      'yyyy-MM-dd HH:mm',
+    );
   } else {
     data.date = format(new Date(), 'yyyy-MM-dd');
-    data.timeStarted = format(roundToNearestMinutes(new Date(), {nearestTo: 15}), 'yyyy-MM-dd HH:mm');
-    data.timeFinished = format(roundToNearestMinutes(new Date(), {nearestTo: 15}), 'yyyy-MM-dd HH:mm');
+    data.timeStarted = format(
+      roundToNearestMinutes(new Date(), { nearestTo: 15 }),
+      'yyyy-MM-dd HH:mm',
+    );
+    data.timeFinished = format(
+      roundToNearestMinutes(new Date(), { nearestTo: 15 }),
+      'yyyy-MM-dd HH:mm',
+    );
   }
   const handleUpdate = useCallback(
     async (data: EntryWithDate) => {
       setSaving(true);
-      data.timeStarted = `${format(new Date(data.date || ''), 'yyyy-MM-dd')} ${format(new Date(data.timeStarted), 'HH:mm')}`;
-      data.timeFinished = `${format(new Date(data.date || ''), 'yyyy-MM-dd')} ${format(new Date(data.timeFinished), 'HH:mm')}`;
+      data.timeStarted = `${format(
+        new Date(data.date || ''),
+        'yyyy-MM-dd',
+      )} ${format(new Date(data.timeStarted), 'HH:mm')}`;
+      data.timeFinished = `${format(
+        new Date(data.date || ''),
+        'yyyy-MM-dd',
+      )} ${format(new Date(data.timeFinished), 'HH:mm')}`;
       delete data.date;
       const req = new TimesheetLine();
       req.setId(id);
@@ -90,17 +143,20 @@ const EditTimesheetModal: FC<Props> = ({ entry, timesheetOwnerId, userId, timesh
       setSaving(false);
       onSave(result);
     },
-    [
-      setSaving,
-      entry,
-    ],
+    [setSaving, entry],
   );
 
   const handleCreate = useCallback(
     async (data: EntryWithDate) => {
       setSaving(true);
-      data.timeStarted = `${format(new Date(data.date || ''), 'yyyy-MM-dd')} ${format(new Date(data.timeStarted), 'HH:mm')}`;
-      data.timeFinished = `${format(new Date(data.date || ''), 'yyyy-MM-dd')} ${format(new Date(data.timeFinished), 'HH:mm')}`;
+      data.timeStarted = `${format(
+        new Date(data.date || ''),
+        'yyyy-MM-dd',
+      )} ${format(new Date(data.timeStarted), 'HH:mm')}`;
+      data.timeFinished = `${format(
+        new Date(data.date || ''),
+        'yyyy-MM-dd',
+      )} ${format(new Date(data.timeFinished), 'HH:mm')}`;
       delete data.date;
       data.technicianUserId = timesheetOwnerId;
       data.servicesRenderedId = entry.servicesRenderedId;
@@ -121,75 +177,68 @@ const EditTimesheetModal: FC<Props> = ({ entry, timesheetOwnerId, userId, timesh
       setSaving(false);
       onSave(result);
     },
-    [
-      setSaving,
-      entry,
-      action,
-    ],);
+    [setSaving, entry, action],
+  );
 
-  const handleApprove = useCallback(
-    async () => {
-      confirm({
-        catchOnCancel: true,
-        description: `Are you sure you want to ${timesheetAdministration ? 'Approve' : 'Submit'} this Timesheet?`
-      }).then( async () => {
-        const dateTime = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
+  const handleApprove = useCallback(async () => {
+    confirm({
+      catchOnCancel: true,
+      description: `Are you sure you want to ${
+        timesheetAdministration ? 'Approve' : 'Submit'
+      } this Timesheet?`,
+    }).then(async () => {
+      const dateTime = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
+      setSaving(true);
+      const req = new TimesheetLine();
+      req.setId(id);
+      if (timesheetAdministration) {
+        req.setAdminApprovalUserId(userId);
+        req.setAdminApprovalDatetime(dateTime);
+        req.setFieldMaskList(['AdminApprovalUserId', 'AdminApprovalDatetime']);
+      } else {
+        req.setUserApprovalDatetime(dateTime);
+        req.setFieldMaskList(['UserApprovalDatetime']);
+      }
+      const result = await tslClient.Update(req);
+      setSaving(false);
+      onSave(result);
+    });
+  }, [id, userId, timesheetAdministration]);
+
+  const handleDelete = useCallback(async () => {
+    confirm({
+      catchOnCancel: true,
+      description: 'Are you sure you want to delete this Timecard?',
+    })
+      .then(async () => {
         setSaving(true);
         const req = new TimesheetLine();
         req.setId(id);
-        if (timesheetAdministration) {
-          req.setAdminApprovalUserId(userId);
-          req.setAdminApprovalDatetime(dateTime);
-          req.setFieldMaskList(['AdminApprovalUserId', 'AdminApprovalDatetime']);
-        } else {
-          req.setUserApprovalDatetime(dateTime);
-          req.setFieldMaskList(['UserApprovalDatetime']);
-        }
-        const result = await tslClient.Update(req);
+        const result = await tslClient.Delete(req);
         setSaving(false);
-        onSave(result);
-      });
-    },
-    [id, userId, timesheetAdministration]
-  );
-
-  const handleDelete = useCallback(
-    async () => {
-      confirm({
-        catchOnCancel: true,
-        description: "Are you sure you want to delete this Timecard?"
+        onSave(result, 'delete');
       })
-        .then( async () => {
-          setSaving(true);
-          const req = new TimesheetLine();
-          req.setId(id);
-          const result = await tslClient.Delete(req);
-          setSaving(false);
-          onSave(result, 'delete');
-        })
-        .catch(() => {})
-    },
-    [
-      setSaving,
-      entry,
-    ],
-  );
+      .catch(() => {});
+  }, [setSaving, entry]);
 
-  const handleSave = useCallback((data) => {
-    switch (action) {
-      case 'update':
-        handleUpdate(data);
-        break;
-      case 'create':
-        handleCreate(data);
-        break;
-      case 'convert':
-        handleCreate(data);
-        break;
-      default:
-        return false;
-    }
-  }, [action]);
+  const handleSave = useCallback(
+    data => {
+      switch (action) {
+        case 'update':
+          handleUpdate(data);
+          break;
+        case 'create':
+          handleCreate(data);
+          break;
+        case 'convert':
+          handleCreate(data);
+          break;
+        default:
+          return false;
+      }
+    },
+    [action],
+  );
 
   return (
     <Modal open onClose={onClose}>
@@ -201,22 +250,27 @@ const EditTimesheetModal: FC<Props> = ({ entry, timesheetOwnerId, userId, timesh
         onClose={onClose}
         disabled={saving}
       >
-        {!!entry?.eventId && (
-          <span>Source: {entry?.eventId}</span>
-        )}
+        {!!entry?.eventId && <span>Source: {entry?.eventId}</span>}
         {action === 'update' && (
-          <ButtonGroup className={classes.buttonGroup} disabled={saving}>
+          <ButtonGroup
+            className="TimesheetEditModalButtonGroup"
+            disabled={saving}
+          >
             <Button
               label={timesheetAdministration ? 'Approve' : 'Submit'}
               onClick={handleApprove}
               disabled={timesheetAdministration && !entry.userApprovalDatetime}
             />
-            <Button label="Delete" onClick={handleDelete} />
+            <Button
+              label="Delete"
+              onClick={handleDelete}
+              className="TimesheetEditModalDelete"
+            />
           </ButtonGroup>
         )}
       </Form>
     </Modal>
-  )
+  );
 };
 
 export default EditTimesheetModal;
