f208106cd7c5a94295c2a0ac8e863590695dabe4
githubacct0 <githubacct0@outlook.com>
2020-07-27 19:18:35 +0200

    Styles refactoring: Timesheet main

--- modules/Timesheet/main.tsx
+++ modules/Timesheet/main.tsx
@@ -1,7 +1,11 @@
-import React, { createContext, useEffect, useReducer, useCallback } from "react";
+import React, {
+  createContext,
+  useEffect,
+  useReducer,
+  useCallback,
+} from 'react';
 import { startOfWeek, subDays } from 'date-fns';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import EventIcon from '@material-ui/icons/Event';
@@ -12,7 +16,11 @@ import AssessmentIcon from '@material-ui/icons/Assessment';
 import Alert from '@material-ui/lab/Alert';
 import { UserClient } from '@kalos-core/kalos-rpc/User';
 import { ServicesRendered } from '@kalos-core/kalos-rpc/ServicesRendered';
-import { TimesheetLineClient, TimesheetLine, TimesheetReq } from '@kalos-core/kalos-rpc/TimesheetLine';
+import {
+  TimesheetLineClient,
+  TimesheetLine,
+  TimesheetReq,
+} from '@kalos-core/kalos-rpc/TimesheetLine';
 import { TransactionClient } from '@kalos-core/kalos-rpc/Transaction';
 import customTheme from '../Theme/main';
 import { AddNewButton } from '../ComponentsLibrary/AddNewButton';
@@ -24,24 +32,11 @@ import { ENDPOINT } from '../../constants';
 import { loadUserById } from '../../helpers';
 import { getShownDates, reducer } from './reducer';
 import ReceiptsIssueDialog from './components/ReceiptsIssueDialog';
+import './styles.less';
 
 const userClient = new UserClient(ENDPOINT);
 const tslClient = new TimesheetLineClient(ENDPOINT);
 const txnClient = new TransactionClient(ENDPOINT);
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    wrapper: {
-      overflow: 'auto',
-    },
-    week: {
-      minWidth: 1500,
-      display: 'grid',
-      gridTemplateColumns: 'repeat(7, minmax(0, 1fr))',
-      gridGap: theme.spacing(2),
-    },
-  }),
-);
 
 type Props = {
   userId: number;
@@ -60,13 +55,12 @@ export const EditTimesheetContext = createContext<EditTimesheetContext>({
 
 const getWeekStart = (userId: number, timesheetOwnerId: number) => {
   const today = new Date();
-  return userId === timesheetOwnerId ?
-    startOfWeek(today) :
-    startOfWeek(subDays(today, 7));
+  return userId === timesheetOwnerId
+    ? startOfWeek(today)
+    : startOfWeek(subDays(today, 7));
 };
 
 const Timesheet = ({ userId, timesheetOwnerId }: Props) => {
-  const classes = useStyles();
   const [state, dispatch] = useReducer(reducer, {
     user: undefined,
     owner: undefined,
@@ -92,7 +86,7 @@ const Timesheet = ({ userId, timesheetOwnerId }: Props) => {
       shown: false,
       hasReceiptsIssue: false,
       receiptsIssueStr: '',
-    }
+    },
   });
   const {
     user,
@@ -107,13 +101,20 @@ const Timesheet = ({ userId, timesheetOwnerId }: Props) => {
     error,
     receiptsIssue,
   } = state;
-  const handleOnSave = (card: TimesheetLine.AsObject, action?: 'delete' | 'approve' | 'reject') => {
-    dispatch({ type: 'saveTimecard', data: card, action: action || editing.action });
+  const handleOnSave = (
+    card: TimesheetLine.AsObject,
+    action?: 'delete' | 'approve' | 'reject',
+  ) => {
+    dispatch({
+      type: 'saveTimecard',
+      data: card,
+      action: action || editing.action,
+    });
   };
 
   const handleAddNewTimeshetCardClicked = () => {
     if (!checkReceiptIssue()) return;
-    dispatch({type: 'addNewTimesheet'});
+    dispatch({ type: 'addNewTimesheet' });
   };
 
   const editTimesheetCard = (card: TimesheetLine.AsObject) => {
@@ -130,15 +131,39 @@ const Timesheet = ({ userId, timesheetOwnerId }: Props) => {
   };
 
   const addNewOptions = [
-    { icon: <EventIcon />, name: 'Timecard', action: handleAddNewTimeshetCardClicked },
-    { icon: <TimerOffIcon />, name: 'Request Off', url: 'https://app.kalosflorida.com/index.cfm?action=admin:timesheet.addTimeOffRequest' },
-    { icon: <AddAlertIcon />, name: 'Reminder', url: 'https://app.kalosflorida.com/index.cfm?action=admin:service.addReminder' },
-    { icon: <AssignmentIndIcon />, name: 'Task', url: 'https://app.kalosflorida.com/index.cfm?action=admin:tasks.addtask' },
-    { icon: <AssessmentIcon />, name: 'Timesheet Weekly Report', action: () => {console.log('Timesheet Weekly Report')} },
+    {
+      icon: <EventIcon />,
+      name: 'Timecard',
+      action: handleAddNewTimeshetCardClicked,
+    },
+    {
+      icon: <TimerOffIcon />,
+      name: 'Request Off',
+      url:
+        'https://app.kalosflorida.com/index.cfm?action=admin:timesheet.addTimeOffRequest',
+    },
+    {
+      icon: <AddAlertIcon />,
+      name: 'Reminder',
+      url:
+        'https://app.kalosflorida.com/index.cfm?action=admin:service.addReminder',
+    },
+    {
+      icon: <AssignmentIndIcon />,
+      name: 'Task',
+      url: 'https://app.kalosflorida.com/index.cfm?action=admin:tasks.addtask',
+    },
+    {
+      icon: <AssessmentIcon />,
+      name: 'Timesheet Weekly Report',
+      action: () => {
+        console.log('Timesheet Weekly Report');
+      },
+    },
   ];
 
   const handleDateChange = (value: Date) => {
-    dispatch({ type: 'changeDate', value })
+    dispatch({ type: 'changeDate', value });
   };
 
   const checkTimeout = (): boolean => {
@@ -170,32 +195,36 @@ const Timesheet = ({ userId, timesheetOwnerId }: Props) => {
       const ids = [];
       let overlapped = false;
       for (let i = 0; i < shownDates.length; i++) {
-        let dayList = [...data[shownDates[i]].timesheetLineList]
-          .sort((a, b) =>
-            new Date(a.timeStarted).getTime() - new Date(b.timeStarted).getTime()
-          );
-        let result = dayList.reduce((acc, current, idx, arr) => {
-          if (idx === 0) {
-            acc.idList.push(current.id);
-            return acc;
-          }
-          let previous = arr[idx-1];
-          let previousEnd = new Date(previous.timeFinished).getTime();
-          let currentStart = new Date(current.timeStarted).getTime();
-          let overlap = (previousEnd > currentStart);
-          if (overlap) {
-            overlapped = true;
-          } else {
-            acc.ranges.push({
-              previous: previous,
-              current: current
-            });
-            if (!current.adminApprovalUserId) {
-              acc.idList.push(current.id)
+        let dayList = [...data[shownDates[i]].timesheetLineList].sort(
+          (a, b) =>
+            new Date(a.timeStarted).getTime() -
+            new Date(b.timeStarted).getTime(),
+        );
+        let result = dayList.reduce(
+          (acc, current, idx, arr) => {
+            if (idx === 0) {
+              acc.idList.push(current.id);
+              return acc;
             }
-          }
-          return acc;
-        }, {ranges: [], idList: []});
+            let previous = arr[idx - 1];
+            let previousEnd = new Date(previous.timeFinished).getTime();
+            let currentStart = new Date(current.timeStarted).getTime();
+            let overlap = previousEnd > currentStart;
+            if (overlap) {
+              overlapped = true;
+            } else {
+              acc.ranges.push({
+                previous: previous,
+                current: current,
+              });
+              if (!current.adminApprovalUserId) {
+                acc.idList.push(current.id);
+              }
+            }
+            return acc;
+          },
+          { ranges: [], idList: [] },
+        );
 
         if (overlapped) {
           break;
@@ -228,7 +257,7 @@ const Timesheet = ({ userId, timesheetOwnerId }: Props) => {
           owner: userResult,
           hasReceiptsIssue: hasIssue,
           receiptsIssueStr: issueStr,
-        }
+        },
       });
     } else {
       const ownerResult = await loadUserById(timesheetOwnerId);
@@ -239,7 +268,7 @@ const Timesheet = ({ userId, timesheetOwnerId }: Props) => {
           owner: ownerResult,
           hasReceiptsIssue: hasIssue,
           receiptsIssueStr: issueStr,
-        }
+        },
       });
     }
   };
@@ -265,7 +294,11 @@ const Timesheet = ({ userId, timesheetOwnerId }: Props) => {
       const req = new TimesheetReq();
       req.setServicesRendered(sr);
       req.setTimesheetLine(tl);
-      const result = await tslClient.GetTimesheet(req, `${shownDates[0]}%`, `${shownDates[shownDates.length - 1]}%`);
+      const result = await tslClient.GetTimesheet(
+        req,
+        `${shownDates[0]}%`,
+        `${shownDates[shownDates.length - 1]}%`,
+      );
       dispatch({ type: 'fetchedTimesheetData', data: result });
     })();
   }, [shownDates]);
@@ -294,28 +327,30 @@ const Timesheet = ({ userId, timesheetOwnerId }: Props) => {
             pendingEntries={pendingEntries}
           />
           {error && (
-            <Alert severity="error" onClose={() => dispatch({ type: 'error', text: '' })}>
+            <Alert
+              severity="error"
+              onClose={() => dispatch({ type: 'error', text: '' })}
+            >
               {error}
             </Alert>
           )}
-          <Box className={classes.wrapper}>
-              {hasAccess ? (
-                <Container className={classes.week} maxWidth={false}>
-                  {shownDates.map((date: string) => (
-                    <Column
-                      key={date}
-                      date={date}
-                      data={data[date]}
-                      loading={fetchingTimesheetData}
-                    />
-                  ))}
-                </Container>
-              ) : (
-                <Alert severity="error">
-                  You have no access to this timesheet.
-                </Alert>
-              )
-            }
+          <Box className="Timesheet">
+            {hasAccess ? (
+              <Container className="TimesheetWeek" maxWidth={false}>
+                {shownDates.map((date: string) => (
+                  <Column
+                    key={date}
+                    date={date}
+                    data={data[date]}
+                    loading={fetchingTimesheetData}
+                  />
+                ))}
+              </Container>
+            ) : (
+              <Alert severity="error">
+                You have no access to this timesheet.
+              </Alert>
+            )}
           </Box>
           {editing.modalShown && (
             <EditTimesheetModal
@@ -328,9 +363,7 @@ const Timesheet = ({ userId, timesheetOwnerId }: Props) => {
               action={editing.action}
             />
           )}
-          {hasAccess && (
-            <AddNewButton options={addNewOptions} />
-          )}
+          {hasAccess && <AddNewButton options={addNewOptions} />}
         </EditTimesheetContext.Provider>
       </ConfirmServiceProvider>
       {receiptsIssue.shown && (
