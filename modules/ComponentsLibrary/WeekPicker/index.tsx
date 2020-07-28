f998006f8e516ad2cf3f83a61e378e5441113b0c
githubacct0 <githubacct0@outlook.com>
2020-07-28 19:03:09 +0200

    Styles refactoring: WeekPicker

--- modules/ComponentsLibrary/WeekPicker/index.tsx
+++ modules/ComponentsLibrary/WeekPicker/index.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react';
 import clsx from 'clsx';
 import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 import { TextFieldProps } from '@material-ui/core/TextField';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import format from 'date-fns/format';
 import isValid from 'date-fns/isValid';
@@ -11,50 +10,7 @@ import endOfWeek from 'date-fns/endOfWeek';
 import startOfWeek from 'date-fns/startOfWeek';
 import isWithinInterval from 'date-fns/isWithinInterval';
 import { DatePicker } from '../CustomControls';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    dayWrapper: {
-      position: 'relative',
-    },
-    day: {
-      width: 36,
-      height: 36,
-      fontSize: theme.typography.caption.fontSize,
-      margin: '0 2px',
-      color: 'inherit',
-    },
-    customDayHighlight: {
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      left: '2px',
-      right: '2px',
-      border: `1px solid ${theme.palette.secondary.main}`,
-      borderRadius: '50%',
-    },
-    nonCurrentMonthDay: {
-      color: theme.palette.text.disabled,
-    },
-    highlightNonCurrentMonthDay: {
-      color: '#676767',
-    },
-    highlight: {
-      background: theme.palette.primary.main,
-      color: theme.palette.common.white,
-    },
-    firstHighlight: {
-      extend: 'highlight',
-      borderTopLeftRadius: '50%',
-      borderBottomLeftRadius: '50%',
-    },
-    endHighlight: {
-      extend: 'highlight',
-      borderTopRightRadius: '50%',
-      borderBottomRightRadius: '50%',
-    },
-  }),
-);
+import './styles.less';
 
 type Props = {
   label: string;
@@ -75,7 +31,6 @@ export const WeekPicker: FC<Props> = ({
   white,
   weekStartsOn = 0,
 }) => {
-  const classes = useStyles();
   const options = { weekStartsOn };
   const handleWeekChange = (date: MaterialUiPickersDate) => {
     onChange(startOfWeek(date || value || new Date().valueOf(), options));
@@ -108,14 +63,15 @@ export const WeekPicker: FC<Props> = ({
     const isLastDay = isSameDay(dateClone, end);
 
     const wrapperClassName = clsx({
-      [classes.highlight]: dayIsBetween,
-      [classes.firstHighlight]: isFirstDay,
-      [classes.endHighlight]: isLastDay,
+      ['WeekPickerHighlight']: dayIsBetween,
+      ['WeekPickerFirstHighlight']: isFirstDay,
+      ['WeekPickerEndHighlight']: isLastDay,
     });
 
-    const dayClassName = clsx(classes.day, {
-      [classes.nonCurrentMonthDay]: !dayInCurrentMonth,
-      [classes.highlightNonCurrentMonthDay]: !dayInCurrentMonth && dayIsBetween,
+    const dayClassName = clsx('WeekPickerDay', {
+      ['WeekPickerNonCurrentMonthDay']: !dayInCurrentMonth,
+      ['WeekPickerHighlightNonCurrentMonthDay']:
+        !dayInCurrentMonth && dayIsBetween,
     });
 
     return (
