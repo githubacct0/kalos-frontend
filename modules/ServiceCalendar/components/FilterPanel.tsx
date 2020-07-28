2407a9c5ee07d5b59d83dab1070df5afded3e454
githubacct0 <githubacct0@outlook.com>
2020-07-28 17:03:10 +0200

    Styles refactoring: ServiceCalendar FilterPanel

--- modules/ServiceCalendar/components/FilterPanel.tsx
+++ modules/ServiceCalendar/components/FilterPanel.tsx
@@ -1,31 +1,10 @@
 import React from 'react';
-import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Typography from '@material-ui/core/Typography';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%',
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular,
-    },
-    desc: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: 400,
-      color: theme.palette.grey.A200,
-      marginLeft: theme.spacing(1),
-    },
-    panelDetails: {
-      flexDirection: 'column',
-    },
-  }),
-);
+import './filterPanel.less';
 
 type Props = {
   title: string;
@@ -33,31 +12,30 @@ type Props = {
   expanded: boolean;
   handleChange: () => void;
   children: React.ReactNode;
-}
+};
 
 const FilterPanel = ({
   title,
   selectedCount,
   expanded,
   handleChange,
-  children
-}: Props) => {
-  const classes = useStyles();
-  return (
-    <ExpansionPanel expanded={expanded} onChange={handleChange}>
-      <ExpansionPanelSummary
-        expandIcon={<ExpandMoreIcon />}
-      >
-        <Typography className={classes.heading}>{title}</Typography>
-        {!!selectedCount && (
-          <Typography className={classes.desc}>| {selectedCount} Selected</Typography>
-        )}
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails className={classes.panelDetails}>
-        {children}
-      </ExpansionPanelDetails>
-    </ExpansionPanel>
-  );
-};
+  children,
+}: Props) => (
+  <ExpansionPanel expanded={expanded} onChange={handleChange}>
+    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+      <Typography className="ServiceCalendarFilterPanelHeading">
+        {title}
+      </Typography>
+      {!!selectedCount && (
+        <Typography className="ServiceCalendarFilterPanelDesc">
+          | {selectedCount} Selected
+        </Typography>
+      )}
+    </ExpansionPanelSummary>
+    <ExpansionPanelDetails className="ServiceCalendarFilterPanelPanelDetails">
+      {children}
+    </ExpansionPanelDetails>
+  </ExpansionPanel>
+);
 
 export default FilterPanel;
