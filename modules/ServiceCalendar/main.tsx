35cf7a252396dcbbaef15a03287629d9852d3136
githubacct0 <githubacct0@outlook.com>
2020-07-28 17:31:14 +0200

    Styles refactoring: ServiceCalendar main

--- modules/ServiceCalendar/main.tsx
+++ modules/ServiceCalendar/main.tsx
@@ -1,14 +1,25 @@
-import React, { createContext, useCallback, useEffect, useReducer } from 'react';
+import React, {
+  createContext,
+  useCallback,
+  useEffect,
+  useReducer,
+} from 'react';
 import clsx from 'clsx';
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import { format, startOfWeek, startOfMonth, endOfMonth, eachDayOfInterval, addDays } from 'date-fns';
+import {
+  format,
+  startOfWeek,
+  startOfMonth,
+  endOfMonth,
+  eachDayOfInterval,
+  addDays,
+} from 'date-fns';
 import { User, UserClient } from '@kalos-core/kalos-rpc/User';
 import { Event, EventClient } from '@kalos-core/kalos-rpc/Event/index';
 import { TimeoffRequest } from '@kalos-core/kalos-rpc/compiled-protos/timeoff_request_pb';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import Box from '@material-ui/core/Box';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import * as jspb from 'google-protobuf';
 import customTheme from '../Theme/main';
@@ -20,80 +31,69 @@ import { AddNewButton } from '../ComponentsLibrary/AddNewButton';
 import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
 import AddAlertIcon from '@material-ui/icons/AddAlert';
 import EventIcon from '@material-ui/icons/Event';
+import './styles.less';
 
 type Props = {
   userId: number;
-}
+};
 
 const userClient = new UserClient(ENDPOINT);
 const eventClient = new EventClient(ENDPOINT);
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    wrapper: {
-      overflow: 'auto',
-    },
-    week: {
-      minWidth: 1500,
-      display: 'grid',
-      gridTemplateColumns: 'repeat(7, minmax(0, 1fr))',
-      gridGap: theme.spacing(2),
-    },
-  }),
-);
 
 const today = new Date();
 
 const getDefaultSelectedDate = (viewBy: string): Date => {
   switch (viewBy) {
-  case 'day':
-    return today;
-  case 'week':
-    return startOfWeek(today);
-  case 'month':
-    return startOfMonth(today);
-  default:
-    return today;
+    case 'day':
+      return today;
+    case 'week':
+      return startOfWeek(today);
+    case 'month':
+      return startOfMonth(today);
+    default:
+      return today;
   }
 };
 
 const getShownDates = (viewBy: string, date?: Date): string[] => {
   switch (viewBy) {
-  case 'day':
-    return [format(date || today, 'yyyy-MM-dd')];
-  case 'week': {
-    const firstDay = date || startOfWeek(today);
-    const lastDay = addDays(firstDay, 6);
-    const days = eachDayOfInterval({ start: firstDay, end: lastDay });
-    return days.map(date => format(date, 'yyyy-MM-dd'));
-  }
-  case 'month': {
-    const firstDay = date || startOfMonth(today);
-    const lastDay = endOfMonth(date || today);
-    const days = eachDayOfInterval({ start: firstDay, end: lastDay });
-    return days.map(date => format(date, 'yyyy-MM-dd'));
-  }
-  default:
-    return [];
+    case 'day':
+      return [format(date || today, 'yyyy-MM-dd')];
+    case 'week': {
+      const firstDay = date || startOfWeek(today);
+      const lastDay = addDays(firstDay, 6);
+      const days = eachDayOfInterval({ start: firstDay, end: lastDay });
+      return days.map(date => format(date, 'yyyy-MM-dd'));
+    }
+    case 'month': {
+      const firstDay = date || startOfMonth(today);
+      const lastDay = endOfMonth(date || today);
+      const days = eachDayOfInterval({ start: firstDay, end: lastDay });
+      return days.map(date => format(date, 'yyyy-MM-dd'));
+    }
+    default:
+      return [];
   }
 };
 
 type MapList = {
-  [key:string]: string,
-}
+  [key: string]: string;
+};
 
-const mapToObject = (mapping: jspb.Map<string|number, string>): MapList =>
-  mapping.toArray()
+const mapToObject = (mapping: jspb.Map<string | number, string>): MapList =>
+  mapping
+    .toArray()
     .reduce((acc: MapList, [key, val]: [string | number, string]) => {
-      acc[key] = val; return acc;
+      acc[key] = val;
+      return acc;
     }, {});
 
 type Filters = {
-  customers: string[],
-  jobType: number,
-  jobSubType: number,
-  zip: string[],
-  propertyUse: string[],
+  customers: string[];
+  jobType: number;
+  jobSubType: number;
+  zip: string[];
+  propertyUse: string[];
 };
 
 type State = {
@@ -105,81 +105,81 @@ type State = {
   speedDialOpen: boolean;
   viewBy: string | '';
   defaultView?: string;
-  selectedDate: Date | '',
+  selectedDate: Date | '';
   shownDates: string[];
   filters: Filters;
-}
+};
 
 type Action =
-  | { type: 'setUser', value: User.AsObject }
+  | { type: 'setUser'; value: User.AsObject }
   | { type: 'fetchingCalendarData' }
-  | { type: 'fetchedCalendarData', data: CalendarData }
-  | { type: 'viewBy', value: string }
+  | { type: 'fetchedCalendarData'; data: CalendarData }
+  | { type: 'viewBy'; value: string }
   | { type: 'speedDialOpen' }
-  | { type: 'changeSelectedDate', value: Date}
-  | { type: 'changeFilters', value: Filters}
-  | { type: 'defaultView', value: string};
+  | { type: 'changeSelectedDate'; value: Date }
+  | { type: 'changeFilters'; value: Filters }
+  | { type: 'defaultView'; value: string };
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-  case 'setUser': {
-    return {
-      ...state,
-      user: action.value,
-      defaultView: action.value.calendarPref,
-    };
-  }
-  case 'fetchingCalendarData': {
-    return {
-      ...state,
-      fetchingCalendarData: true,
-    };
-  }
-  case 'fetchedCalendarData': {
-    const CalendarData = action.data;
-    return {
-      ...state,
-      datesMap: CalendarData.getDatesMap(),
-      customersMap: mapToObject(CalendarData.getCustomersMap()),
-      zipCodesMap: mapToObject(CalendarData.getZipCodesMap()),
-      fetchingCalendarData: false,
-    };
-  }
-  case 'defaultView': {
-    return {
-      ...state,
-      defaultView: action.value,
-    };
-  }
-  case 'viewBy': {
-    return {
-      ...state,
-      viewBy: action.value,
-      selectedDate: getDefaultSelectedDate(action.value),
-      shownDates: getShownDates(action.value),
-    };
-  }
-  case 'speedDialOpen': {
-    return {
-      ...state,
-      speedDialOpen: !state.speedDialOpen,
-    };
-  }
-  case 'changeSelectedDate': {
-    return {
-      ...state,
-      selectedDate: action.value,
-      shownDates: getShownDates(state.viewBy, action.value),
-    };
-  }
-  case 'changeFilters' : {
-    return {
-      ...state,
-      filters: action.value,
-    };
-  }
-  default:
-    return {...state};
+    case 'setUser': {
+      return {
+        ...state,
+        user: action.value,
+        defaultView: action.value.calendarPref,
+      };
+    }
+    case 'fetchingCalendarData': {
+      return {
+        ...state,
+        fetchingCalendarData: true,
+      };
+    }
+    case 'fetchedCalendarData': {
+      const CalendarData = action.data;
+      return {
+        ...state,
+        datesMap: CalendarData.getDatesMap(),
+        customersMap: mapToObject(CalendarData.getCustomersMap()),
+        zipCodesMap: mapToObject(CalendarData.getZipCodesMap()),
+        fetchingCalendarData: false,
+      };
+    }
+    case 'defaultView': {
+      return {
+        ...state,
+        defaultView: action.value,
+      };
+    }
+    case 'viewBy': {
+      return {
+        ...state,
+        viewBy: action.value,
+        selectedDate: getDefaultSelectedDate(action.value),
+        shownDates: getShownDates(action.value),
+      };
+    }
+    case 'speedDialOpen': {
+      return {
+        ...state,
+        speedDialOpen: !state.speedDialOpen,
+      };
+    }
+    case 'changeSelectedDate': {
+      return {
+        ...state,
+        selectedDate: action.value,
+        shownDates: getShownDates(state.viewBy, action.value),
+      };
+    }
+    case 'changeFilters': {
+      return {
+        ...state,
+        filters: action.value,
+      };
+    }
+    default:
+      return { ...state };
   }
 };
 
@@ -221,55 +221,74 @@ type CalendarData = {
 };
 
 type CalendarDataContext = {
-  fetchingCalendarData?: boolean,
-  datesMap?: jspb.Map<string, CalendarDay>,
-  customersMap?: MapList,
-  zipCodesMap?: MapList,
-  initialFilters: Filters,
-  filters: Filters,
-  changeFilters: (value: Filters) => void,
-}
+  fetchingCalendarData?: boolean;
+  datesMap?: jspb.Map<string, CalendarDay>;
+  customersMap?: MapList;
+  zipCodesMap?: MapList;
+  initialFilters: Filters;
+  filters: Filters;
+  changeFilters: (value: Filters) => void;
+};
 
 export const CalendarDataContext = createContext<CalendarDataContext>({
   initialFilters: initialFilters,
   filters: initialFilters,
   changeFilters: () => {},
 });
-export const EmployeesContext = createContext<EmployeesContext>({ employees: [], employeesLoading: false });
+export const EmployeesContext = createContext<EmployeesContext>({
+  employees: [],
+  employeesLoading: false,
+});
 
 const addNewOptions = [
-  { icon: <AssignmentIndIcon />, name: 'Task', url: 'https://app.kalosflorida.com/index.cfm?action=admin:service.addServiceCallGeneral' },
-  { icon: <AddAlertIcon />, name: 'Reminder', url: 'https://app.kalosflorida.com/index.cfm?action=admin:service.addReminder' },
-  { icon: <EventIcon />, name: 'Service Call', url: 'https://app.kalosflorida.com/index.cfm?action=admin:tasks.addtask' },
+  {
+    icon: <AssignmentIndIcon />,
+    name: 'Task',
+    url:
+      'https://app.kalosflorida.com/index.cfm?action=admin:service.addServiceCallGeneral',
+  },
+  {
+    icon: <AddAlertIcon />,
+    name: 'Reminder',
+    url:
+      'https://app.kalosflorida.com/index.cfm?action=admin:service.addReminder',
+  },
+  {
+    icon: <EventIcon />,
+    name: 'Service Call',
+    url: 'https://app.kalosflorida.com/index.cfm?action=admin:tasks.addtask',
+  },
 ];
 
 const ServiceCalendar = ({ userId }: Props) => {
-  const classes = useStyles();
-  const [{
-    user,
-    defaultView,
-    fetchingCalendarData,
-    datesMap,
-    customersMap,
-    zipCodesMap,
-    viewBy,
-    shownDates,
-    selectedDate,
-    filters
-  }, dispatch] = useReducer(reducer, initialState);
+  const [
+    {
+      user,
+      defaultView,
+      fetchingCalendarData,
+      datesMap,
+      customersMap,
+      zipCodesMap,
+      viewBy,
+      shownDates,
+      selectedDate,
+      filters,
+    },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const fetchUser = async () => {
     const req = new User();
     req.setId(userId);
     const result = await userClient.Get(req);
-    dispatch({ type: 'setUser', value: result, });
+    dispatch({ type: 'setUser', value: result });
   };
 
   if (defaultView && !viewBy) {
     dispatch({ type: 'viewBy', value: defaultView });
   }
 
-  const fetchEmployees = useCallback( async (page) => {
+  const fetchEmployees = useCallback(async page => {
     const user = new User();
     user.setIsActive(1);
     user.setIsEmployee(1);
@@ -277,7 +296,9 @@ const ServiceCalendar = ({ userId }: Props) => {
     return (await userClient.BatchGet(user)).toObject();
   }, []);
 
-  const { data:employees, isLoading:employeesLoading } = useFetchAll(fetchEmployees);
+  const { data: employees, isLoading: employeesLoading } = useFetchAll(
+    fetchEmployees,
+  );
 
   useEffect(() => {
     userClient.GetToken('test', 'test');
@@ -285,14 +306,14 @@ const ServiceCalendar = ({ userId }: Props) => {
   }, []);
 
   useEffect(() => {
-    if(shownDates.length) {
+    if (shownDates.length) {
       dispatch({ type: 'fetchingCalendarData' });
       (async () => {
         const req = new Event();
         req.setDateStarted(shownDates[0]);
         req.setDateEnded(shownDates[shownDates.length - 1]);
         const data = await eventClient.GetCalendarData(req);
-        dispatch({type: 'fetchedCalendarData', data});
+        dispatch({ type: 'fetchedCalendarData', data });
       })();
     }
   }, [shownDates]);
@@ -301,12 +322,15 @@ const ServiceCalendar = ({ userId }: Props) => {
     dispatch({ type: 'viewBy', value });
   }, []);
 
-  const changeSelectedDate = useCallback((value: Date): void => {
-    dispatch({ type: 'changeSelectedDate', value });
-  }, [viewBy]);
+  const changeSelectedDate = useCallback(
+    (value: Date): void => {
+      dispatch({ type: 'changeSelectedDate', value });
+    },
+    [viewBy],
+  );
 
   const changeFilters = useCallback((value: Filters): void => {
-    dispatch({ type: 'changeFilters', value});
+    dispatch({ type: 'changeFilters', value });
   }, []);
 
   const setDefaultView = useCallback(() => {
@@ -344,8 +368,11 @@ const ServiceCalendar = ({ userId }: Props) => {
           />
         </MuiPickersUtilsProvider>
         <EmployeesContext.Provider value={{ employees, employeesLoading }}>
-          <Box className={classes.wrapper}>
-            <Container className={clsx(viewBy !== 'day' && classes.week)} maxWidth={false}>
+          <Box className="ServiceCalendarWrapper">
+            <Container
+              className={clsx(viewBy !== 'day' && 'ServiceCalendarWeek')}
+              maxWidth={false}
+            >
               {shownDates.map(date => (
                 <Column
                   key={date}
