46e42137a252f655c6d7c6177b0d37f85a06b99f
githubacct0 <githubacct0@outlook.com>
2020-08-14 18:23:10 +0200

    remove old side menu

--- modules/PageWrapper/main.tsx
+++ modules/PageWrapper/main.tsx
@@ -1,19 +1,31 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import StylesProvider from '@material-ui/styles/StylesProvider';
 import customTheme from '../Theme/main';
 import SideMenu, { Props } from '../SideMenu/main';
 import './styles.less';
 
-export const PageWrapper: FC<Props> = ({ children, ...props }) => (
-  <StylesProvider injectFirst>
-    <ThemeProvider theme={customTheme.lightTheme}>
-      <div>
-        <div className="PageWrapperMenu">
-          <SideMenu {...props} />
+export const PageWrapper: FC<Props> = ({ children, ...props }) => {
+  const [initiated, setInitiated] = useState<boolean>(false);
+  useEffect(() => {
+    if (!initiated) {
+      setInitiated(true);
+      const oldSideMenu = document.getElementsByClassName('main-header')[0];
+      if (oldSideMenu) {
+        oldSideMenu.remove();
+      }
+    }
+  }, [initiated, setInitiated]);
+  return (
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={customTheme.lightTheme}>
+        <div>
+          <div className="PageWrapperMenu">
+            <SideMenu {...props} />
+          </div>
+          <div className="PageWrapperContent">{children}</div>
         </div>
-        <div className="PageWrapperContent">{children}</div>
-      </div>
-    </ThemeProvider>
-  </StylesProvider>
-);
+      </ThemeProvider>
+    </StylesProvider>
+  );
+};
