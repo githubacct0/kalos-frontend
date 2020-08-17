7e583a78d867e9d6d58311c9f1fe2adceee1dd37
githubacct0 <githubacct0@outlook.com>
2020-08-17 17:44:01 +0200

    PropertyTasks with page wrapper

--- modules/PropertyTasks/main.tsx
+++ modules/PropertyTasks/main.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
-import ThemeProvider from '@material-ui/styles/ThemeProvider';
-import customTheme from '../Theme/main';
 import { Tasks } from '../ComponentsLibrary/Tasks';
+import { PageWrapper } from '../PageWrapper/main';
 
 interface Props {
   loggedUserId: number;
@@ -9,11 +8,11 @@ interface Props {
 }
 
 export const PropertyTasks: FC<Props> = ({ propertyId, loggedUserId }) => (
-  <ThemeProvider theme={customTheme.lightTheme}>
+  <PageWrapper userID={loggedUserId}>
     <Tasks
       externalCode="properties"
       externalId={propertyId}
       loggedUserId={loggedUserId}
     />
-  </ThemeProvider>
+  </PageWrapper>
 );
