1c4aec80c6170314cb4ab999014f28fb400fe415
githubacct0 <githubacct0@outlook.com>
2020-08-17 20:05:10 +0200

    PDFMaker with page wrapper

--- modules/PDFMaker/index.tsx
+++ modules/PDFMaker/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { PDFMaker } from './main';
+import { StyledPage } from '../PageWrapper/styled';
 
 ReactDOM.render(
-  <PDFMaker
-    name="Robert Milejczak"
-    dateStr="2020/02/05"
-    title="Create PDF"
-    amount={40.54}
-    pdfType="Retrievable Receipt"
-  />,
-  document.getElementById('root'),
+  <StyledPage>
+    <PDFMaker
+      name="Robert Milejczak"
+      dateStr="2020/02/05"
+      title="Create PDF"
+      amount={40.54}
+      pdfType="Retrievable Receipt"
+    />
+  </StyledPage>,
+  document.getElementById('root')
 );
