8cd15aefb78ebf417c2e95968bc19f022704a164
githubacct0 <githubacct0@outlook.com>
2020-08-17 20:26:49 +0200

    AddServiceCallGeneral with page wrapper

--- modules/AddServiceCallGeneral/main.tsx
+++ modules/AddServiceCallGeneral/main.tsx
@@ -1,10 +1,9 @@
 import React, { FC } from 'react';
-import ThemeProvider from '@material-ui/styles/ThemeProvider';
-import customTheme from '../Theme/main';
 import { AddServiceCall, Props } from './components/AddServiceCall';
+import { PageWrapper } from '../PageWrapper/main';
 
-export const AddServiceCallGeneral: FC<Props> = props => (
-  <ThemeProvider theme={customTheme.lightTheme}>
+export const AddServiceCallGeneral: FC<Props> = (props) => (
+  <PageWrapper userID={props.loggedUserId}>
     <AddServiceCall {...props} />
-  </ThemeProvider>
+  </PageWrapper>
 );
