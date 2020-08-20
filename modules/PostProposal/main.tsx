7f48fa92a0bb7af247a3dc73ca1792299ea9eb51
githubacct0 <githubacct0@outlook.com>
2020-08-20 20:59:11 +0200

    PostProposal with components library

--- modules/PostProposal/main.tsx
+++ modules/PostProposal/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
+import Alert from '@material-ui/lab/Alert';
 import { ENDPOINT } from '../../constants';
+import { Button } from '../ComponentsLibrary/Button';
+import { Link } from '../ComponentsLibrary/Link';
 import { S3Client, URLObject } from '@kalos-core/kalos-rpc/S3File';
 import { PageWrapper, PageWrapperProps } from '../PageWrapper/main';
 
@@ -24,56 +24,30 @@ export function PostProposal({ userID, userName, jobNumber, ...props }: props) {
   };
   return (
     <PageWrapper {...props} userID={userID}>
-      <Grid
-        container
-        direction="column"
-        justify="space-around"
-        alignItems="center"
-        style={{ padding: 20, height: '100%' }}
-      >
-        <Typography
-          variant="h4"
-          component="span"
-          align="center"
-          style={{ marginBottom: 10 }}
-        >
-          Thank you for reviewing your proposal {userName}!
-        </Typography>
-        <Button
-          onClick={downloadPDF}
-          variant="outlined"
-          style={{ marginBottom: 10 }}
-        >
-          Click to obtain a copy of the document for your records
-        </Button>
-        <Typography
-          variant="body1"
-          component="span"
-          align="center"
-          style={{ marginBottom: 10 }}
-        >
+      <Alert>
+        <big>Thank you for reviewing your proposal {userName}!</big>
+      </Alert>
+      <Button
+        label="Click to obtain a copy of the document for your records"
+        onClick={downloadPDF}
+        variant="outlined"
+        style={{ marginRight: 8 }}
+      />
+      <Alert severity="info">
+        <div>
           We appreciate your business. If you have any issues or concerns please
           contact us and as to speak with our parts coordinator.
-        </Typography>
-        <Typography
-          variant="h5"
-          component="a"
-          align="center"
-          href="tel:3522437099"
-          style={{ marginBottom: 10 }}
-        >
-          Call us at: (352) 243-7099
-        </Typography>
-        <Typography
-          variant="h5"
-          component="a"
-          align="center"
-          href="mailto:office@kalosflorida.com"
-          style={{ marginBottom: 10 }}
-        >
-          Email us at: office@kalosflorida.com
-        </Typography>
-      </Grid>
+        </div>
+        <div>
+          Call us at: <Link href="tel:3522437099">(352) 243-7099</Link>
+        </div>
+        <div>
+          Email us at:{' '}
+          <Link href="mailto:office@kalosflorida.com">
+            office@kalosflorida.com
+          </Link>
+        </div>
+      </Alert>
     </PageWrapper>
   );
 }
@@ -84,5 +58,5 @@ async function fetchPDF(userID: number, jobNumber: number) {
   req.setKey(`${jobNumber}_approved_proposal_${userID}.pdf`);
   req.setBucket('testbuckethelios');
   const dlURL = await S3.GetDownloadURL(req);
-  window.location.assign(dlURL.url);
+  window.open(dlURL.url, '_blank');
 }
