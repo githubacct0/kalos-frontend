85556fabb5be459921ce207e25a9fc81013e0442
githubacct0 <githubacct0@outlook.com>
2020-09-01 16:48:15 +0200

    TransactionUser - add photo from gallery

--- modules/TransactionUser/components/card.tsx
+++ modules/TransactionUser/components/card.tsx
@@ -23,13 +23,20 @@ import { TaskClient } from '@kalos-core/kalos-rpc/Task';
 import CloseIcon from '@material-ui/icons/CloseSharp';
 import { PDFMaker } from '../../ComponentsLibrary/PDFMaker';
 import ReIcon from '@material-ui/icons/RefreshSharp';
-import { timestamp } from '../../../helpers';
+import {
+  timestamp,
+  FileType,
+  upsertFile,
+  upsertTransactionDocument,
+} from '../../../helpers';
 import { ENDPOINT } from '../../../constants';
 import { EmailClient, EmailConfig } from '@kalos-core/kalos-rpc/Email';
 import { Field } from '../../ComponentsLibrary/Field';
 import { SectionBar } from '../../ComponentsLibrary/SectionBar';
 import { Button } from '../../ComponentsLibrary/Button';
 import { NoteField } from './NoteField';
+import { FileGallery } from '../../ComponentsLibrary/FileGallery';
+import { Modal } from '../../ComponentsLibrary/Modal';
 import './card.css';
 
 interface props {
@@ -45,6 +52,7 @@ interface props {
 
 interface state {
   txn: Transaction.AsObject;
+  pendingAddFromGallery: boolean;
 }
 
 const hardcodedList = [
@@ -74,6 +82,7 @@ export class TxnCard extends React.PureComponent<props, state> {
     super(props);
     this.state = {
       txn: props.txn,
+      pendingAddFromGallery: false,
     };
     this.EmailClient = new EmailClient(ENDPOINT);
     this.TxnClient = new TransactionClient(ENDPOINT);
@@ -95,13 +104,13 @@ export class TxnCard extends React.PureComponent<props, state> {
   async makeLog<K extends keyof Transaction.AsObject>(
     prop: K,
     oldValue: Transaction.AsObject[K],
-    newValue: Transaction.AsObject[K]
+    newValue: Transaction.AsObject[K],
   ) {
     try {
       const log = new TransactionActivity();
       const { userID, userName } = this.props;
       log.setDescription(
-        `User ${userName} updated txn ${prop}: ${oldValue} changed to ${newValue} `
+        `User ${userName} updated txn ${prop}: ${oldValue} changed to ${newValue} `,
       );
       log.setUserId(userID);
       await this.LogClient.Create(log);
@@ -143,7 +152,7 @@ export class TxnCard extends React.PureComponent<props, state> {
     const { txn } = this.state;
     try {
       const ok = confirm(
-        'Are you sure you want to submit this transaction? You will be unable to edit it again unless it is rejected, so please make sure all information is correct'
+        'Are you sure you want to submit this transaction? You will be unable to edit it again unless it is rejected, so please make sure all information is correct',
       );
       if (ok) {
         if (txn.jobId !== 0) {
@@ -185,7 +194,7 @@ export class TxnCard extends React.PureComponent<props, state> {
             statusMessage =
               'receipt marked as accidental or fraudulent and sent directly to accounting for review';
             const mailBody = `A${getTransactionTypeString(
-              txn
+              txn,
             )} transaction has been reported by ${txn.ownerName} (${
               txn.cardUsed
             }).
@@ -213,7 +222,7 @@ export class TxnCard extends React.PureComponent<props, state> {
               txn.ownerId,
               txn.vendor,
               txn.notes,
-              txn.timestamp
+              txn.timestamp,
             );
           }
           console.log('submitted');
@@ -238,8 +247,23 @@ export class TxnCard extends React.PureComponent<props, state> {
     return hardcodedList.includes(acc.id);
   }
 
+  toggleAddFromGallery = () =>
+    this.setState({ pendingAddFromGallery: !this.state.pendingAddFromGallery });
+
+  addFromGallery = async ({ file, url }: { file: FileType; url: string }) => {
+    this.setState({ pendingAddFromGallery: false });
+    await upsertFile({ id: file.id, ownerId: 0 });
+    await upsertTransactionDocument({
+      transactionId: this.state.txn.id,
+      reference: file.name,
+      fileId: file.id,
+      typeId: 1,
+    });
+    alert('Upload complete');
+  };
+
   deriveCallout(
-    txn: Transaction.AsObject
+    txn: Transaction.AsObject,
   ): { severity: 'error' | 'success'; text: string } {
     if (!txn.costCenter || txn.costCenter.id === 0)
       return {
@@ -271,13 +295,13 @@ export class TxnCard extends React.PureComponent<props, state> {
     await this.DocsClient.upload(
       this.state.txn.id,
       `${timestamp()}-generated.pdf`,
-      fileData
+      fileData,
     );
     await this.refresh();
     await this.props.toggleLoading(() =>
       alert(
-        'A PDF has been generated and uploaded for you, it is recommended you review this document before proceeding'
-      )
+        'A PDF has been generated and uploaded for you, it is recommended you review this document before proceeding',
+      ),
     );
   }
 
@@ -289,7 +313,7 @@ export class TxnCard extends React.PureComponent<props, state> {
           await this.DocsClient.upload(
             this.state.txn.id,
             this.FileInput.current!.files![0].name,
-            new Uint8Array(fr.result as ArrayBuffer)
+            new Uint8Array(fr.result as ArrayBuffer),
           );
         } catch (err) {
           alert('File could not be uploaded');
@@ -314,7 +338,7 @@ export class TxnCard extends React.PureComponent<props, state> {
       });
     } catch (err) {
       alert(
-        'Network error, displayed information may be incorrect. Refresh is advised'
+        'Network error, displayed information may be incorrect. Refresh is advised',
       );
       console.log(err);
     }
@@ -339,101 +363,121 @@ export class TxnCard extends React.PureComponent<props, state> {
   }
 
   render() {
-    const t = this.state.txn;
-    const { isManager } = this.props;
+    const { txn, pendingAddFromGallery } = this.state;
+    const t = txn;
+    const { isManager, userID } = this.props;
     let subheader = `${t.description.split(' ')[0]} - ${t.vendor}`;
     const deriveCallout = this.deriveCallout(t);
     return (
-      <Paper elevation={4} style={{ margin: 16, marginTop: 32 }}>
-        <SectionBar
-          title={`${new Date(
-            t.timestamp.split(' ').join('T')
-          ).toDateString()} - $${t.amount}`}
-          subtitle={subheader}
-          asideContent={
-            <div className="TransactionUser_Actions">
-              <Button label="Add Photo" onClick={this.openFilePrompt} />
-              <AltGallery
-                title="Receipt Photo(s)"
-                text="View Photo(s)"
-                fileList={getGalleryData(this.state.txn)}
-                transactionID={this.state.txn.id}
-                canDelete
-              />
-              <Button label="Submit" onClick={this.submit} />
-              <PDFMaker
-                dateStr={t.timestamp}
-                name={t.ownerName}
-                title="Missing"
-                icon={<CloseIcon />}
-                amount={t.amount}
-                onCreate={this.onPDFGenerate}
-                jobNumber={`${t.jobId}`}
-                vendor={t.vendor}
-                pdfType="Missing Receipt"
-              />
-              {this.props.isManager && (
+      <>
+        <Paper elevation={4} style={{ margin: 16, marginTop: 32 }}>
+          <SectionBar
+            title={`${new Date(
+              t.timestamp.split(' ').join('T'),
+            ).toDateString()} - $${t.amount}`}
+            subtitle={subheader + ' / ' + t.id}
+            asideContent={
+              <div className="TransactionUser_Actions">
+                <Button label="Upload Photo" onClick={this.openFilePrompt} />
+                {/* // TODO finish https://github.com/rmilejcz/kalos-frontend/issues/38
+                <Button
+                  label="Add Photo "
+                  onClick={this.toggleAddFromGallery}
+                /> */}
+                <AltGallery
+                  title="Receipt Photo(s)"
+                  text="View Photo(s)"
+                  fileList={getGalleryData(this.state.txn)}
+                  transactionID={this.state.txn.id}
+                  canDelete
+                />
+                <Button label="Submit" onClick={this.submit} />
                 <PDFMaker
                   dateStr={t.timestamp}
                   name={t.ownerName}
-                  icon={<ReIcon />}
-                  title="Recurring"
+                  title="Missing"
+                  icon={<CloseIcon />}
                   amount={t.amount}
-                  vendor={t.vendor}
-                  jobNumber={`${t.jobId}`}
                   onCreate={this.onPDFGenerate}
-                  pdfType="Retrievable Receipt"
+                  jobNumber={`${t.jobId}`}
+                  vendor={t.vendor}
+                  pdfType="Missing Receipt"
                 />
-              )}
-            </div>
-          }
-          sticky={false}
-        />
-        <Alert severity={deriveCallout.severity}>{deriveCallout.text}</Alert>
-        <div className="TransactionUser_Cards">
-          <AccountPicker
-            onSelect={this.updateCostCenterID}
-            selected={t.costCenterId}
-            sort={costCenterSortByPopularity}
-            filter={
-              !isManager ? (a) => ALLOWED_ACCOUNT_IDS.includes(a.id) : undefined
+                {this.props.isManager && (
+                  <PDFMaker
+                    dateStr={t.timestamp}
+                    name={t.ownerName}
+                    icon={<ReIcon />}
+                    title="Recurring"
+                    amount={t.amount}
+                    vendor={t.vendor}
+                    jobNumber={`${t.jobId}`}
+                    onCreate={this.onPDFGenerate}
+                    pdfType="Retrievable Receipt"
+                  />
+                )}
+              </div>
             }
-            hideInactive
-            renderItem={(i) => (
-              <option value={i.id} key={`${i.id}-${i.description}`}>
-                {i.description} ({i.id})
-              </option>
-            )}
+            sticky={false}
           />
-          <Field
-            name="department"
-            value={t.departmentId || this.props.userDepartmentID}
-            onChange={(val) => this.updateDepartmentID(+val)}
-            type="department"
+          <Alert severity={deriveCallout.severity}>{deriveCallout.text}</Alert>
+          <div className="TransactionUser_Cards">
+            <AccountPicker
+              onSelect={this.updateCostCenterID}
+              selected={t.costCenterId}
+              sort={costCenterSortByPopularity}
+              filter={
+                !isManager ? a => ALLOWED_ACCOUNT_IDS.includes(a.id) : undefined
+              }
+              hideInactive
+              renderItem={i => (
+                <option value={i.id} key={`${i.id}-${i.description}`}>
+                  {i.description} ({i.id})
+                </option>
+              )}
+            />
+            <Field
+              name="department"
+              value={t.departmentId || this.props.userDepartmentID}
+              onChange={val => this.updateDepartmentID(+val)}
+              type="department"
+            />
+            <Field
+              name="jobNumber"
+              label="Job Number"
+              value={t.jobId}
+              onChange={val => this.handleJobNumber(+val)}
+              type="eventId"
+              style={{
+                alignItems: 'flex-start',
+                flexGrow: 0,
+              }}
+            />
+            <NoteField
+              initialValue={t.notes}
+              onChange={debounce(this.updateNotes, 500)}
+            />
+          </div>
+          <input
+            type="file"
+            ref={this.FileInput}
+            onChange={this.handleFile}
+            style={{ display: 'none' }}
           />
-          <Field
-            name="jobNumber"
-            label="Job Number"
-            value={t.jobId}
-            onChange={(val) => this.handleJobNumber(+val)}
-            type="eventId"
-            style={{
-              alignItems: 'flex-start',
-              flexGrow: 0,
-            }}
-          />
-          <NoteField
-            initialValue={t.notes}
-            onChange={debounce(this.updateNotes, 500)}
-          />
-        </div>
-        <input
-          type="file"
-          ref={this.FileInput}
-          onChange={this.handleFile}
-          style={{ display: 'none' }}
-        />
-      </Paper>
+        </Paper>
+        {pendingAddFromGallery && (
+          <Modal open onClose={this.toggleAddFromGallery} fullScreen>
+            <FileGallery
+              loggedUserId={userID}
+              title="Add Receipt from Gallery"
+              bucket="kalos-transactions"
+              onClose={this.toggleAddFromGallery}
+              onAdd={this.addFromGallery}
+              removeFileOnAdd={false}
+            />
+          </Modal>
+        )}
+      </>
     );
   }
 }
@@ -450,7 +494,7 @@ export function getMimeType(fileType: string) {
 
 function costCenterSortByPopularity(
   a: TransactionAccount.AsObject,
-  b: TransactionAccount.AsObject
+  b: TransactionAccount.AsObject,
 ) {
   return b.popularity - a.popularity;
 }
@@ -471,7 +515,7 @@ const ALLOWED_ACCOUNT_IDS = [
 ];
 
 function getGalleryData(txn: Transaction.AsObject): GalleryData[] {
-  return txn.documentsList.map((d) => {
+  return txn.documentsList.map(d => {
     return {
       key: `${txn.id}-${d.reference}`,
       bucket: 'kalos-transactions',
