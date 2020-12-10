4a60009f770fe8e8c5688307b57403a03baa542b
githubacct0 <githubacct0@outlook.com>
2020-12-10 08:40:20 -0500

    Added in Jest to the project for unit and implementation testing

---
+++ modules/SearchIndex/main.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { SearchIndex } from './main';
+
+test('SearchIndex displays correctly', () => {
+  expect(true).toBe(true);
+});
