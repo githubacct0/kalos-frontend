578a65609db718cfbecdd6285512c649ce778a0c
githubacct0 <githubacct0@outlook.com>
2020-12-21 13:54:56 -0500

    Filling first form from refs

--- modules/ComponentsLibrary/PlaceAutocompleteAddressForm/index.tsx
+++ modules/ComponentsLibrary/PlaceAutocompleteAddressForm/index.tsx
@@ -4,6 +4,7 @@ import { getKeyByKeyName } from '../../../helpers';
 import { Modal } from '../Modal';
 import { Form, Schema } from '../Form';
 import { Address } from './Address';
+import { Field } from '../Field';
 import './styles.less';
 
 // Convenience call, will be removed later
@@ -40,7 +41,7 @@ export class PlaceAutocompleteAddressForm extends React.PureComponent<
   // @ts-ignore
   autoCompleteSections: google.maps.places.Autocomplete[2] = [];
   fieldRef: any = React.createRef();
-  inputArray: [] = [];
+  inputArray: any[] = [];
   constructor(props: Props) {
     super(props);
 
@@ -53,20 +54,6 @@ export class PlaceAutocompleteAddressForm extends React.PureComponent<
   }
 
   getInputFields = () => {
-    /*
-    const group = document.getElementsByClassName('LocationForm');
-    let inputs = [];
-    for (let i = 0; i < group.length; i++) {
-      inputs.push(
-        (group[i] as HTMLElement).getElementsByClassName('FieldInput'),
-      );
-    }
-
-    if (!inputs) {
-      console.error('No input fields found.');
-      return [];
-    }*/
-
     let inputs: any[] = [];
 
     this.inputArray.forEach(element => {
@@ -117,98 +104,6 @@ export class PlaceAutocompleteAddressForm extends React.PureComponent<
     );
   };
 
-  getInputFieldByIndex = (index: number, formIndex?: number): any => {
-    let result: any[] = [];
-
-    const inputFields = this.getInputFields();
-    for (let i = 0; i < inputFields.length; i++) {
-      if (formIndex) {
-        if (i != formIndex) {
-          continue;
-        }
-      }
-
-      if (!inputFields[i][index]) {
-        console.error(
-          'Index out of bounds in getInputFieldByIndex inside of for loop. Breaking from loop.',
-        );
-        break;
-      }
-
-      result.push(
-        inputFields[i][index].getElementsByTagName(
-          'input',
-        )[0] as HTMLInputElement,
-      );
-    }
-
-    return result[0];
-  };
-
-  getInputElementFromInputField = (inputField: any): any => {
-    return inputField.lastElementChild?.firstChild;
-  };
-
-  getInputFieldByLabelContent = (
-    content: string,
-    indexOfResult?: number,
-  ): any => {
-    const inputFields = this.getInputFields();
-    console.log('Input fields: ', inputFields);
-    let results = [],
-      indexOfResultInputField = [];
-    let inputField;
-    for (let j = 0; j < 6 * this.props.addressFields - 1; j++) {
-      inputField = inputFields[j];
-      for (let i = 0; i < inputField.length; i++) {
-        if (!inputField[i].parentNode) {
-          console.error(
-            'There is no parent node for one of the input fields: ',
-            inputFields[i],
-          );
-          return null;
-        }
-        const childs = inputField[i].parentNode!.childNodes;
-        const filtered = Array.from(childs).filter(
-          (child: any) => child.textContent == content,
-        );
-
-        if (filtered.length > 0) {
-          results.push(inputField[i]);
-          indexOfResultInputField.push(j);
-        }
-      }
-    }
-
-    console.log('Results : ', results);
-
-    if (indexOfResult) {
-      if (results.length <= indexOfResult) {
-        console.error(
-          'The index provided was greater than the results that were received.',
-        );
-        return null;
-      }
-    }
-    if (results.length > 1) {
-      if (indexOfResult) {
-        return results[indexOfResult] as HTMLInputElement;
-      } else {
-        return results[0];
-      }
-    }
-
-    if (results.length > 0) {
-      return results[0];
-    }
-
-    console.error(
-      'Failed to find the desired input element with the label: ',
-      content,
-    );
-    return null;
-  };
-
   handleLoad = () => {
     // Create the autocomplete object, restricting the search predictions to
     // geographical location types.
@@ -218,10 +113,9 @@ export class PlaceAutocompleteAddressForm extends React.PureComponent<
       this.autoCompleteSections[
         i
         // @ts-ignore
-      ] = new google.maps.places.Autocomplete(
-        this.getInputFieldByLabelContent('Address', i),
-        { types: ['geocode'] },
-      );
+      ] = new google.maps.places.Autocomplete(this.inputArray[0], {
+        types: ['geocode'],
+      });
 
       // Avoid paying for data that you don't need by restricting the set of
       // place fields that are returned to just the address components.
@@ -234,6 +128,30 @@ export class PlaceAutocompleteAddressForm extends React.PureComponent<
         this.handlePlaceSelect(i, 0);
       });
     }
+  };
+
+  getInputFieldByLabelContent = (labelText: string) => {
+    let index = 0;
+
+    for (let i = 0; i < this.props.schema.length; i++) {
+      for (let j = 0; j < this.props.schema[j].length; j++) {
+        if (
+          this.props.schema[i][j].label == labelText &&
+          this.props.schema[i][j].type == 'text'
+        ) {
+          console.log(this.props.schema[i][j].label);
+          console.log(labelText);
+          console.log('Returning value: ', this.inputArray[index]);
+          return this.inputArray[index];
+        }
+
+        if (this.props.schema[i][j].type == 'text') {
+          index++;
+        }
+      }
+    }
+
+    return null;
   };
 
   handlePlaceSelect = (indexOfForm: any, startIndex: number) => {
@@ -256,17 +174,18 @@ export class PlaceAutocompleteAddressForm extends React.PureComponent<
         // @ts-ignore
         const val = component[componentForm[addressType]];
 
+        console.log('Schema: ', this.props.schema);
+
         if (addressType == 'route') {
-          this.getInputElementFromInputField(
-            this.getInputFieldByLabelContent('Street Address', indexOfForm),
-          )!.value = street_number + ' ' + val;
+          this.getInputFieldByLabelContent('Street Address')!.value =
+            street_number + ' ' + val;
           index++;
           continue;
         }
 
         addressType == 'street_number'
           ? (street_number = val)
-          : (this.getInputFieldByIndex(index, indexOfForm)!.value = val);
+          : (this.inputArray[index].value = val);
         index++;
       }
     }
