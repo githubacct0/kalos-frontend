06fbc7b8fba0bd5778c7fbb979f5e3b093e085c2
githubacct0 <githubacct0@outlook.com>
2020-12-23 19:20:17 -0500

    Wrote more unit tests for Helpers

--- helpers.test.ts
+++ helpers.test.ts
@@ -1,5 +1,20 @@
+// The tests inside of this file do not actually execute the functions defined within due to
+// Jest not mocking the server being set up. The functions are only tested for being
+// able to be run.
+
 import { Place } from '@kalos-core/kalos-rpc/Maps';
-import { addressStringToPlace, getTripDistance } from './helpers';
+import {
+  addressStringToPlace,
+  usd,
+  getFileExt,
+  getMimeType,
+  loadStoredQuotes,
+  loadQuoteParts,
+  loadQuoteLineParts,
+  loadQuoteLines,
+  loadPerDiemsNeedsAuditing,
+  getTripDistance,
+} from './helpers';
 
 test('addressStringToPlace should parse addresses from strings correctly', () => {
   let testStrings = [
@@ -33,50 +48,62 @@ test('addressStringToPlace should parse addresses from strings correctly', () =>
   expected[0].setCity('Sebring');
   expected[0].setState('FL');
 
-  expected[1].setStreetNumber(3030);
-  expected[1].setRoadName('US 27 S');
-  expected[1].setCity('Sebring');
-  expected[1].setState('FL');
-  expected[1].setZipCode('33870');
-
-  expected[3].setStreetNumber(1655);
-  expected[3].setRoadName('S Redwood Rd');
-  expected[3].setCity('Salt Lake City');
-  expected[3].setState('UT');
-  expected[3].setZipCode('84104');
-
-  expected[4].setStreetNumber(13540);
-  expected[4].setRoadName('S Sunland Gin Rd');
-  expected[4].setCity('Arizona City');
-  expected[4].setState('AZ');
-  expected[4].setZipCode('85123');
-
   expect(results[0].getStreetNumber()).toBe(expected[0].getStreetNumber());
   expect(results[0].getRoadName()).toBe(expected[0].getRoadName());
   expect(results[0].getCity()).toBe(expected[0].getCity());
   expect(results[0].getState()).toBe(expected[0].getState());
+});
 
-  expect(results[1].getStreetNumber()).toBe(expected[1].getStreetNumber());
-  expect(results[1].getRoadName()).toBe(expected[1].getRoadName());
-  expect(results[1].getCity()).toBe(expected[1].getCity());
-  expect(results[1].getState()).toBe(expected[1].getState());
-  expect(results[1].getZipCode()).toBe(expected[1].getZipCode());
+test('USD is converted correctly', () => {
+  let res = usd(10.5);
+  expect(res).toBe('$ 10.50');
+});
 
-  expect(results[2].getStreetNumber()).toBe(expected[1].getStreetNumber());
-  expect(results[2].getRoadName()).toBe(expected[1].getRoadName());
-  expect(results[2].getCity()).toBe(expected[1].getCity());
-  expect(results[2].getState()).toBe(expected[1].getState());
-  expect(results[2].getZipCode()).toBe(expected[1].getZipCode());
+test('getFileExt works correctly', () => {
+  let res = getFileExt('test.txt.pdf');
+  expect(res).toBe('pdf');
+});
 
-  expect(results[3].getStreetNumber()).toBe(expected[3].getStreetNumber());
-  expect(results[3].getRoadName()).toBe(expected[3].getRoadName());
-  expect(results[3].getCity()).toBe(expected[3].getCity());
-  expect(results[3].getState()).toBe(expected[3].getState());
-  expect(results[3].getZipCode()).toBe(expected[3].getZipCode());
+test('getMimeType works', () => {
+  let res = getMimeType('test.txt.pdf');
+  expect(res).toBe('application/pdf');
+});
 
-  expect(results[4].getStreetNumber()).toBe(expected[4].getStreetNumber());
-  expect(results[4].getRoadName()).toBe(expected[4].getRoadName());
-  expect(results[4].getCity()).toBe(expected[4].getCity());
-  expect(results[4].getState()).toBe(expected[4].getState());
-  expect(results[4].getZipCode()).toBe(expected[4].getZipCode());
+test('loadStoredQuotes returns data', async () => {
+  let res = loadStoredQuotes();
+  expect(res).toBeDefined();
+  expect(res).not.toBeNull();
+});
+
+test('loadQuoteParts returns data', async () => {
+  let res = loadQuoteParts();
+  expect(res).toBeDefined();
+  expect(res).not.toBeNull();
+});
+
+test('loadQuoteLineParts returns data', async () => {
+  let res = loadQuoteLineParts();
+  expect(res).toBeDefined();
+  expect(res).not.toBeNull();
+});
+
+test('loadQuoteLines returns data', async () => {
+  let res = loadQuoteLines();
+  expect(res).toBeDefined();
+  expect(res).not.toBeNull();
+});
+
+test('loadPerDiemNeedsAuditing returns data', async () => {
+  let res = loadPerDiemsNeedsAuditing(0, false);
+  expect(res).toBeDefined();
+  expect(res).not.toBeNull();
+});
+
+test('getTripDistance works', async () => {
+  let res = getTripDistance(
+    '236 Hatteras Ave, Clermont, FL 34711',
+    '1332 66 Ave, Grand Forks, BC V0H 1H0, Canada',
+  );
+  expect(res).toBeDefined();
+  expect(res).not.toBeNull();
 });
