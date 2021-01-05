0ec412e17c029abfedd69c025c27101dbabb5ae7
githubacct0 <githubacct0@outlook.com>
2021-01-05 16:18:35 +0100

    Payroll

--- modules/Payroll/main.tsx
+++ modules/Payroll/main.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect, useCallback } from 'react';
 import { startOfWeek } from 'date-fns';
 import { PageWrapper, PageWrapperProps } from '../PageWrapper/main';
 import { CalendarHeader } from '../ComponentsLibrary/CalendarHeader';
+import { UserType, UserClientService, getCustomerName } from '../../helpers';
 
 interface Props {
   userID: number;
@@ -9,16 +10,31 @@ interface Props {
 }
 
 export const Payroll: FC<Props & PageWrapperProps> = props => {
+  const { loggedUserId, userID } = props;
+  const [initiated, setInitiated] = useState<boolean>(false);
+  const [selectedDate, setSelectedDate] = useState<Date>(
+    startOfWeek(new Date()),
+  );
+  const [user, setUser] = useState<UserType>();
+  const initiate = useCallback(async () => {
+    const user = await UserClientService.loadUserById(userID);
+    setUser(user);
+  }, [userID]);
+  useEffect(() => {
+    if (!initiated) {
+      setInitiated(true);
+      initiate();
+    }
+  }, [initiated]);
   return (
-    <PageWrapper {...props} userID={props.loggedUserId} withHeader>
+    <PageWrapper {...props} userID={loggedUserId} withHeader>
       <CalendarHeader
-        selectedDate={startOfWeek(new Date())}
-        title="John Smith"
-        onDateChange={console.log}
+        selectedDate={selectedDate}
+        title={getCustomerName(user)}
+        onDateChange={setSelectedDate}
         onSubmit={() => console.log('SUBMIT')}
         weekStartsOn={6}
       />
-      <h1>Payroll!</h1>
     </PageWrapper>
   );
 };
