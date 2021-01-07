db23c0024335470bd6f9277b14dca060e2349889
githubacct0 <githubacct0@outlook.com>
2021-01-07 15:11:17 +0100

    Payroll

--- modules/ComponentsLibrary/Payroll/index.tsx
+++ modules/ComponentsLibrary/Payroll/index.tsx
@@ -1,8 +1,89 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useCallback, useEffect } from 'react';
+import { SectionBar } from '../SectionBar';
+import { PlainForm, Schema } from '../PlainForm';
+import { Loader } from '../../Loader/main';
+import {
+  UserType,
+  loadTimesheetDepartments,
+  TimesheetDepartmentType,
+  getDepartmentName,
+  loadTechnicians,
+  getCustomerName,
+} from '../../../helpers';
+import { OPTION_ALL } from '../../../constants';
 import './styles.less';
 
 interface Props {
   userID: number;
 }
 
-export const Payroll: FC<Props> = ({ userID }) => <div>Payroll {userID}</div>;
+type FilterData = {
+  departmentId: number;
+  employeeId: number;
+};
+
+export const Payroll: FC<Props> = ({ userID }) => {
+  const [initiated, setInitiated] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [filter, setFilter] = useState<FilterData>({
+    departmentId: 0,
+    employeeId: 0,
+  });
+  const [departments, setDepartments] = useState<TimesheetDepartmentType[]>([]);
+  const [employees, setEmployees] = useState<UserType[]>([]);
+  const init = useCallback(async () => {
+    const departments = await loadTimesheetDepartments();
+    setDepartments(departments);
+    const employees = await loadTechnicians();
+    setEmployees(employees);
+    setInitiated(true);
+  }, []);
+  useEffect(() => {
+    if (!initiated) {
+      init();
+    }
+  }, [initiated]);
+  const SCHEMA: Schema<FilterData> = [
+    [
+      {
+        name: 'departmentId',
+        label: 'Select Department',
+        options: [
+          { label: OPTION_ALL, value: 0 },
+          ...departments.map(el => ({
+            label: getDepartmentName(el),
+            value: el.id,
+          })),
+        ],
+      },
+      {
+        name: 'employeeId',
+        label: 'Select Employee',
+        options: [
+          { label: OPTION_ALL, value: 0 },
+          ...employees
+            .filter(el => {
+              if (filter.departmentId === 0) return true;
+              return el.employeeDepartmentId === filter.departmentId;
+            })
+            .map(el => ({
+              label: getCustomerName(el),
+              value: el.id,
+            })),
+        ],
+      },
+    ],
+  ];
+  return (
+    <div>
+      <SectionBar title="Payroll" />
+      {initiated ? (
+        <>
+          <PlainForm data={filter} onChange={setFilter} schema={SCHEMA} />
+        </>
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
+};
