9ff57fcc786059f3fc3d5d3ea4b8a5848f6ff34f
githubacct0 <githubacct0@outlook.com>
2021-01-08 14:42:00 -0500

    Added more to examples for TripSummary

--- modules/ComponentsLibrary/TripSummary/examples.tsx
+++ modules/ComponentsLibrary/TripSummary/examples.tsx
@@ -10,10 +10,13 @@ export default () => (
     <ExampleTitle>Cannot Delete Trips</ExampleTitle>
     <TripSummary perDiemRowIds={[1]} loggedUserId={101275} cannotDeleteTrips />
 
-    <ExampleTitle>Viewing as Olbinski</ExampleTitle>
+    <ExampleTitle>Viewing as Olbinski and cannot delete trips</ExampleTitle>
     <TripSummary perDiemRowIds={[1]} loggedUserId={101253} cannotDeleteTrips />
 
-    <ExampleTitle>Viewing All Trips for Example Week 1</ExampleTitle>
+    <ExampleTitle>
+      Viewing All Trips for Example Week 1 and cannot delete trips
+    </ExampleTitle>
+    {/* You can add in 0 as the logged user id to view all users for that week */}
     <TripSummary perDiemRowIds={[1]} loggedUserId={0} cannotDeleteTrips />
   </>
 );
