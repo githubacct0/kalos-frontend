4a444eb05645db908fcc3c0d61c491300b5ccb74
githubacct0 <githubacct0@outlook.com>
2021-01-21 16:29:28 -0500

    Started really working with the SlackMessageButton

--- modules/ComponentsLibrary/SlackMessageButton/examples.tsx
+++ modules/ComponentsLibrary/SlackMessageButton/examples.tsx
@@ -4,10 +4,25 @@ import { AddressPair } from '../PlaceAutocompleteAddressForm/Address';
 import { Schema } from '../Form';
 import { ExampleTitle } from '../helpers';
 import { PerDiemRow } from '@kalos-core/kalos-rpc/PerDiem';
+import { UserClientService } from '../../../helpers';
+
+const userName1 = 'Justin Farrell';
+const messageToSend = 'Test message';
+const userId = 101275;
 
 export default () => (
   <>
     <ExampleTitle>Default</ExampleTitle>
-    <SlackMessageButton label="Message on slack!" userId={10010} />
+    <SlackMessageButton
+      label="Message on slack!"
+      userName={userName1}
+      textToSend={messageToSend}
+      loggedUserId={userId}
+      onClick={() => {
+        alert(
+          `Would send a message to the user with ID ${userId} saying "${messageToSend}"`,
+        );
+      }}
+    />
   </>
 );
--- modules/ComponentsLibrary/SlackMessageButton/index.tsx
+++ modules/ComponentsLibrary/SlackMessageButton/index.tsx
@@ -1,29 +1,65 @@
 import React, { CSSProperties } from 'react';
+import {
+  getSlackID,
+  getSlackList,
+  slackNotify,
+  UserClientService,
+} from '../../../helpers';
 import { Button } from '../Button/index';
 
 interface Props {
   label: string;
   url?: string;
-  onClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
   disabled?: boolean;
   variant?: 'contained' | 'outlined' | 'text';
   color?: 'primary' | 'secondary';
   fullWidth?: boolean;
+  onClick?: () => void;
   className?: string;
   span?: boolean;
   startIcon?: JSX.Element;
   style?: CSSProperties;
   loading?: boolean;
-  userId: number;
+  userName: string;
+  textToSend: string;
+  loggedUserId: number;
 }
 
 interface State {}
 
 export class SlackMessageButton extends React.PureComponent<Props, State> {
+  userName: string = '';
   constructor(props: Props) {
     super(props);
     this.state = {};
+
+    this.getUserNameFromId();
   }
+
+  getUserNameFromId = async () => {
+    try {
+      let user = await UserClientService.loadUserById(this.props.loggedUserId);
+      this.userName = `${user.firstname} ${user.lastname}`;
+    } catch (error: any) {
+      console.error('Could not fetch the user name from the given ID.');
+    }
+  };
+
+  onClick = async () => {
+    if (this.userName == '') {
+      console.error(
+        'An error occurred while sending the message. Please try again.',
+      );
+      return;
+    }
+
+    const messageToSend = `*From ${this.userName}*: ${this.props.textToSend}.`;
+
+    const slackUser = await getSlackID(this.props.userName);
+    await slackNotify(slackUser, messageToSend);
+
+    console.log('Message sent successfully.');
+  };
 
   render() {
     return (
@@ -31,7 +67,7 @@ export class SlackMessageButton extends React.PureComponent<Props, State> {
         <Button
           label={this.props.label}
           url={this.props.url}
-          onClick={this.props.onClick}
+          onClick={this.props.onClick ? this.props.onClick : this.onClick}
           disabled={this.props.disabled}
           variant={this.props.variant}
           color={this.props.color}
