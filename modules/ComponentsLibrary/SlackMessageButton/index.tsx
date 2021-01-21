c01fc8e87694607765aa06ef8b9615ea2356418a
githubacct0 <githubacct0@outlook.com>
2021-01-21 14:47:29 -0500

    Created shell of component for SlackMessageButton

---
+++ modules/ComponentsLibrary/SlackMessageButton/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC, useState, useCallback, PureComponent } from 'react';
+
+interface Props {}
+
+interface State {}
+
+export class SlackMessageButton extends PureComponent<Props, State> {}
