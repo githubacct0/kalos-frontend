f399f28bc3859ef6e85f722b7fa6d17443901c66
githubacct0 <githubacct0@outlook.com>
2021-01-27 10:36:42 -0500

    commenting the trips button

--- modules/ComponentsLibrary/Timesheet/index.tsx
+++ modules/ComponentsLibrary/Timesheet/index.tsx
@@ -181,6 +181,7 @@ export const Timesheet: FC<Props> = props => {
       name: 'Task',
       url: 'https://app.kalosflorida.com/index.cfm?action=admin:tasks.addtask',
     },
+    /*
     {
       icon: <DriveEtaIcon />,
       name: 'Trips',
@@ -188,6 +189,7 @@ export const Timesheet: FC<Props> = props => {
         setTripsOpen(true);
       },
     },
+    */
     /*{
         icon: <AssessmentIcon />,
         name: 'Timesheet Weekly Report',
