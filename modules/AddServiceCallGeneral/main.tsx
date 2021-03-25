6257de7945f15303bc978a42c34406b8036bdc99
githubacct0 <githubacct0@outlook.com>
2021-03-25 19:41:05 -0400

    Projects now only display gantt chart in AddServiceCallGeneral

--- modules/AddServiceCallGeneral/main.tsx
+++ modules/AddServiceCallGeneral/main.tsx
@@ -55,59 +55,71 @@ export const AddServiceCallGeneral: FC<Props & PageWrapperProps> = props => {
           tabs={Array.from(Array(2)).map((_, idx) => ({
             label: idx == 0 ? `Add Service Call` : `Add Project`,
             content: (
-              <AddServiceCall {...props} asProject={idx == 0 ? false : true} />
+              <>
+                <AddServiceCall
+                  {...props}
+                  asProject={idx == 0 ? false : true}
+                />
+                {idx != 0 && (
+                  <>
+                    {parentId != confirmedParentId && parentId != null && (
+                      <Confirm
+                        title="Confirm Parent"
+                        open={true}
+                        onClose={() => handleSetParentId(null)}
+                        onConfirm={() => handleSetConfirmedIsChild(parentId)}
+                      >
+                        Are you sure you want to set this project as the parent
+                        to the new project?
+                      </Confirm>
+                    )}
+                    {loaded ? (
+                      <GanttChart
+                        events={projects.map(task => {
+                          const {
+                            id,
+                            description,
+                            dateStarted: dateStart,
+                            dateEnded: dateEnd,
+                            logJobStatus,
+                            color,
+                          } = task;
+                          const [startDate, startHour] = dateStart.split(' ');
+                          const [endDate, endHour] = dateEnd.split(' ');
+                          return {
+                            id,
+                            startDate,
+                            endDate,
+                            startHour,
+                            endHour,
+                            notes: description,
+                            statusColor: '#' + color,
+                            onClick: () => {
+                              console.log('Service call id of clicked: ', id);
+                              handleSetParentId(id);
+                              // setServiceCallId(id);
+                              // setLoaded(false);
+                              // setLoadedInit(false);
+                              // // load();
+                            },
+                          };
+                        })}
+                        startDate={projects[0].dateStarted.substr(0, 10)}
+                        endDate={projects[projects.length - 1].dateEnded.substr(
+                          0,
+                          10,
+                        )}
+                        loading={loading}
+                      />
+                    ) : (
+                      <Loader />
+                    )}
+                  </>
+                )}
+              </>
             ),
           }))}
         />
-        {parentId != confirmedParentId && parentId != null && (
-          <Confirm
-            title="Confirm Parent"
-            open={true}
-            onClose={() => handleSetParentId(null)}
-            onConfirm={() => handleSetConfirmedIsChild(parentId)}
-          >
-            Are you sure you want to set this project as the parent to the new
-            project?
-          </Confirm>
-        )}
-        {loaded ? (
-          <GanttChart
-            events={projects.map(task => {
-              const {
-                id,
-                description,
-                dateStarted: dateStart,
-                dateEnded: dateEnd,
-                logJobStatus,
-                color,
-              } = task;
-              const [startDate, startHour] = dateStart.split(' ');
-              const [endDate, endHour] = dateEnd.split(' ');
-              return {
-                id,
-                startDate,
-                endDate,
-                startHour,
-                endHour,
-                notes: description,
-                statusColor: '#' + color,
-                onClick: () => {
-                  console.log('Service call id of clicked: ', id);
-                  handleSetParentId(id);
-                  // setServiceCallId(id);
-                  // setLoaded(false);
-                  // setLoadedInit(false);
-                  // // load();
-                },
-              };
-            })}
-            startDate={projects[0].dateStarted.substr(0, 10)}
-            endDate={projects[projects.length - 1].dateEnded.substr(0, 10)}
-            loading={loading}
-          />
-        ) : (
-          <Loader />
-        )}
       </>
     </PageWrapper>
   );
