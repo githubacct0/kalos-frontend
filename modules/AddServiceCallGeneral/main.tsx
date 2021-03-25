8e331fcca75404196f9d92a043f265385213651e
githubacct0 <githubacct0@outlook.com>
2021-03-25 15:05:31 -0400

    Add new service call and project now with tabs for selecting project

--- modules/AddServiceCallGeneral/main.tsx
+++ modules/AddServiceCallGeneral/main.tsx
@@ -1,9 +1,20 @@
 import React, { FC } from 'react';
 import { AddServiceCall, Props } from './components/AddServiceCall';
 import { PageWrapper, PageWrapperProps } from '../PageWrapper/main';
+import { Tabs } from '../ComponentsLibrary/Tabs';
 
 export const AddServiceCallGeneral: FC<Props & PageWrapperProps> = props => (
   <PageWrapper {...props} userID={props.loggedUserId}>
-    <AddServiceCall {...props} />
+    <Tabs
+      tabs={Array.from(Array(2)).map((_, idx) => ({
+        label: idx == 0 ? `Add Service Call` : `Add Project`,
+        content:
+          idx == 0 ? (
+            <AddServiceCall {...props} />
+          ) : (
+            <AddServiceCall {...props} asProject />
+          ),
+      }))}
+    />
   </PageWrapper>
 );
