c2c9aae93ebfe2e1e87893178c895a0f01576a4b
githubacct0 <githubacct0@outlook.com>
2021-04-20 15:40:43 -0400

    Removed transaction accounts payable module - rewriting that module from the ground up

--- modules/TransactionAccountsPayable/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Transaction from './main';
-
-ReactDOM.render(
-  <Transaction userID={213} isAdmin withHeader />,
-  document.getElementById('txn-root'),
-);
