3fbddf27e22baebcc53da92cdffeffe51f047df9
githubacct0 <githubacct0@outlook.com>
2021-05-05 09:04:59 -0400

    Changing Payroll Summary logic to show more techs

--- modules/ComponentsLibrary/Payroll/components/PayrollSummary.tsx
+++ modules/ComponentsLibrary/Payroll/components/PayrollSummary.tsx
@@ -6,7 +6,14 @@ import Visibility from '@material-ui/icons/Visibility';
 import { SectionBar } from '../../../ComponentsLibrary/SectionBar';
 import { InfoTable } from '../../../ComponentsLibrary/InfoTable';
 import { Modal } from '../../../ComponentsLibrary/Modal';
-import { TimesheetLineType, makeFakeRows, UserType } from '../../../../helpers';
+import {
+  TimesheetLineType,
+  makeFakeRows,
+  UserType,
+  UserClientService,
+  TimesheetLineClientService,
+  TimesheetDepartmentClientService,
+} from '../../../../helpers';
 import { ROWS_PER_PAGE, OPTION_ALL } from '../../../../constants';
 import {
   TimesheetLine,
@@ -17,6 +24,7 @@ import { ENDPOINT, NULL_TIME } from '../../../../constants';
 import { RoleType } from '../index';
 import { NULL_TIME_VALUE } from '../../Timesheet/constants';
 import { CostSummary } from '../../CostSummary';
+import { TimesheetDepartment } from '@kalos-core/kalos-rpc/TimesheetDepartment';
 interface Props {
   departmentId: number;
   employeeId: number;
@@ -24,7 +32,12 @@ interface Props {
   type: RoleType;
   loggedUser: number;
 }
-
+const getDepartmentNameById = async (id: number) => {
+  let req = new TimesheetDepartment();
+  req.setId(id);
+  const dept = await TimesheetDepartmentClientService.Get(req);
+  return dept.value;
+};
 const formatWeek = (date: string) => {
   const d = parseISO(date);
   return `Week of ${format(d, 'yyyy')} ${format(d, 'MMMM')}, ${format(
@@ -32,7 +45,12 @@ const formatWeek = (date: string) => {
     'do',
   )}`;
 };
-
+export type TechnicianSummaryType = {
+  id: number;
+  name: string;
+  timesheetExist: boolean;
+  department: string;
+};
 export const PayrollSummary: FC<Props> = ({
   employeeId,
   departmentId,
@@ -41,11 +59,13 @@ export const PayrollSummary: FC<Props> = ({
   loggedUser,
 }) => {
   const [loading, setLoading] = useState<boolean>(false);
-  const [timesheets, setTimesheets] = useState<TimesheetLineType[]>([]);
+  const [loadedTechnicians, setLoadedTechnicians] = useState<
+    TechnicianSummaryType[]
+  >([]);
   const [page, setPage] = useState<number>(0);
   const [count, setCount] = useState<number>(0);
   const [toggle, setToggle] = useState<boolean>(false);
-  const [pendingView, setPendingView] = useState<TimesheetLineType>();
+  const [pendingView, setPendingView] = useState<TechnicianSummaryType>();
   const [pendingViewDefault, setPendingViewDefault] = useState<boolean>(false);
   const [startDay, setStartDay] = useState<Date>(
     startOfWeek(subDays(new Date(), 7), { weekStartsOn: 6 }),
@@ -62,28 +82,81 @@ export const PayrollSummary: FC<Props> = ({
       startDate: format(startDay, 'yyyy-MM-dd'),
       endDate: format(endDay, 'yyyy-MM-dd'),
     };
+
     if (week !== OPTION_ALL) {
       Object.assign(filter, {
         startDate: week,
         endDate: format(addDays(new Date(week), 7), 'yyyy-MM-dd'),
       });
     }
+    const req = new TimesheetLine();
+    req.setDateRangeList([
+      '>=',
+      format(startDay, 'yyyy-MM-dd'),
+      '<=',
+      format(endDay, 'yyyy-MM-dd'),
+    ]);
+    req.setWithoutLimit(true);
+    let technicians = [];
+    if (departmentId) {
+      technicians = await UserClientService.loadUsersByDepartmentId(
+        departmentId,
+      );
+      const user = new User();
+      user.setEmployeeDepartmentId(departmentId);
+      req.setSearchUser(user);
+    } else if (employeeId) {
+      technicians = [await UserClientService.loadUserById(employeeId)];
+    } else {
+      technicians = await UserClientService.loadTechnicians();
+    }
+    const createdTimesheets = (
+      await TimesheetLineClientService.BatchGet(req)
+    ).getResultsList();
+    const completeTechnicianList = [];
     const getTimesheets = createTimesheetFetchFunction(filter);
-    const { resultsList, totalCount } = (await getTimesheets()).toObject();
-    let sortedResultsList = resultsList.sort((a, b) =>
-      a.technicianUserName.split(' ')[1] > b.technicianUserName.split(' ')[1]
-        ? 1
-        : -1,
-    );
-    setTimesheets(sortedResultsList);
-    setCount(totalCount);
+
+    const { resultsList } = (await getTimesheets()).toObject();
+    for (let i = 0; i < technicians.length; i++) {
+      let found = false;
+      const userDep = await getDepartmentNameById(
+        technicians[i].employeeDepartmentId,
+      );
+      for (let j = 0; j < createdTimesheets.length; j++) {
+        if (createdTimesheets[j].getTechnicianUserId() === technicians[i].id) {
+          found = true;
+        }
+      }
+      for (let l = 0; l < resultsList.length; l++) {
+        if (resultsList[l].technicianUserId === technicians[i].id) {
+          found = true;
+          completeTechnicianList.push({
+            id: technicians[i].id,
+            timesheetExist: true,
+            name: technicians[i].firstname + ' ' + technicians[i].lastname,
+            department: userDep,
+          });
+        }
+      }
+      if (found === false) {
+        //we didnt find the tech, so either they didn't work last week, or they don't create timecards
+        completeTechnicianList.push({
+          id: technicians[i].id,
+          timesheetExist: false,
+          name: technicians[i].firstname + ' ' + technicians[i].lastname,
+          department: userDep,
+        });
+      }
+    }
+    setLoadedTechnicians(completeTechnicianList);
+    setCount(completeTechnicianList.length);
     setLoading(false);
   }, [page, employeeId, week, type, toggle, departmentId, endDay, startDay]);
   useEffect(() => {
     load();
   }, [load]);
   const handleTogglePendingView = useCallback(
-    (pendingView?: TimesheetLineType) => () => {
+    (pendingView?: TechnicianSummaryType) => () => {
       setPendingView(pendingView);
       load();
     },
@@ -93,11 +166,9 @@ export const PayrollSummary: FC<Props> = ({
     let tempPendingView = pendingView;
     setPendingView(undefined);
     if (tempPendingView != undefined) {
-      for (let i = 0; i < timesheets.length; i++) {
-        if (
-          timesheets[i].technicianUserId === tempPendingView.technicianUserId
-        ) {
-          tempPendingView = timesheets[i + 1];
+      for (let i = 0; i < loadedTechnicians.length; i++) {
+        if (loadedTechnicians[i].id === tempPendingView.id) {
+          tempPendingView = loadedTechnicians[i + 1];
           break;
         }
       }
@@ -138,30 +209,28 @@ export const PayrollSummary: FC<Props> = ({
         columns={[
           { name: 'Employee' },
           { name: 'Department' },
-          { name: 'Admin Approval Date' },
+          { name: 'Did the User Create a Timesheet Last Week?' },
         ]}
         loading={loading}
         data={
           loading
             ? makeFakeRows(3, 4)
-            : timesheets.map(e => {
+            : loadedTechnicians.map(e => {
                 return [
                   {
-                    value: e.technicianUserName,
+                    value: e.name,
                     onClick: handleTogglePendingView(e),
                   },
                   {
-                    value: e.searchUser?.employeeDepartmentId
-                      ? e.searchUser.employeeDepartmentId
-                      : e.departmentName,
+                    value: e.department,
                     onClick: handleTogglePendingView(e),
                   },
 
                   {
                     value:
-                      e.adminApprovalDatetime === NULL_TIME_VALUE
-                        ? 'Not Approved'
-                        : formatWeek(e.adminApprovalDatetime),
+                      e.timesheetExist === false
+                        ? 'No Timesheet Found'
+                        : 'Timesheet Processed',
                     onClick: handleTogglePendingView(e),
                     actions: [
                       <IconButton
@@ -184,10 +253,8 @@ export const PayrollSummary: FC<Props> = ({
           fullScreen
         >
           <CostSummary
-            userId={
-              employeeId === 0 ? pendingView.technicianUserId : employeeId
-            }
-            username={pendingView.technicianUserName}
+            userId={employeeId === 0 ? pendingView.id : employeeId}
+            username={pendingView.name}
             onClose={handleTogglePendingView(undefined)}
             loggedUserId={loggedUser}
             notReady={toggle}
