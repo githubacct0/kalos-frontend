54bac813f8a5a4a968f03161b84d3cca28b773a5
githubacct0 <githubacct0@outlook.com>
2021-05-06 13:44:06 -0400

    changing perDiem query value

--- modules/ComponentsLibrary/CostSummary/index.tsx
+++ modules/ComponentsLibrary/CostSummary/index.tsx
@@ -218,7 +218,7 @@ export const CostSummary: FC<Props> = ({
     req.setPageNumber(0);
     req.setDateRangeList([
       '>=',
-      formatDateFns(startDay),
+      formatDateFns(subDays(perDiemEndDay, 2)),
       '<',
       formatDateFns(perDiemEndDay),
     ]);
