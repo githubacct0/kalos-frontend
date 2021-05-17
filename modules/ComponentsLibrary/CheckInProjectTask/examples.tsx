d81600119b4997985fc1a87206dcf0fd4db37cb9
githubacct0 <githubacct0@outlook.com>
2021-05-17 19:57:28 -0400

    CheckInProjectTask example fixed up

--- modules/ComponentsLibrary/CheckInProjectTask/examples.tsx
+++ modules/ComponentsLibrary/CheckInProjectTask/examples.tsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CheckInProjectTask } from '.';
+import { EventClientService } from '../../../helpers';
+import { Event } from '@kalos-core/kalos-rpc/Event/index';
+import { ExampleTitle } from '../helpers';
 
 export default () => {
-  return <> </>;
+  const [project, setProject] = useState<Event.AsObject>({} as Event.AsObject);
+  const load = async () => {
+    const req = new Event();
+    req.setId(70);
+    setProject(await EventClientService.Get(req));
+  };
+  useEffect(() => {
+    load();
+  }, []);
+  return (
+    <>
+      <ExampleTitle>Default (logged as 8418)</ExampleTitle>
+      <CheckInProjectTask
+        projectToUse={project}
+        loggedUserId={8418}
+        serviceCallId={1}
+      />
+    </>
+  );
 };
