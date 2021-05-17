1d3b8854c99c847467d0d40a1f3e0def13e9198a
githubacct0 <githubacct0@outlook.com>
2021-05-16 20:18:44 -0400

    Realized I forgot to surround it in the token thing

--- modules/TransactionAccountsPayable/index.tsx
+++ modules/TransactionAccountsPayable/index.tsx
@@ -1,8 +1,13 @@
+import { UserClient } from '@kalos-core/kalos-rpc/User';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { ENDPOINT } from '../../constants';
 import TransactionAccountsPayable from './main';
 
-ReactDOM.render(
-  <TransactionAccountsPayable loggedUserId={98217} hasActions />,
-  document.getElementById('txn-root'),
-);
+const u = new UserClient(ENDPOINT);
+u.GetToken('test', 'test').then(() => {
+  ReactDOM.render(
+    <TransactionAccountsPayable loggedUserId={98217} hasActions />,
+    document.getElementById('txn-root'),
+  );
+});
