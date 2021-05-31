a8127afd84bab5637dcab6ce81265d777f099e14
githubacct0 <githubacct0@outlook.com>
2021-05-31 00:33:19 -0400

    Logs all show up correctly (activity logs with event ids that have department ids not equal to zero on them)

--- modules/ComponentsLibrary/ProjectDetail/components/Logs.tsx
+++ modules/ComponentsLibrary/ProjectDetail/components/Logs.tsx
@@ -1,7 +1,70 @@
-import React, { FC } from 'react';
+import { ActivityLog } from '@kalos-core/kalos-rpc/ActivityLog';
+import React, { FC, useCallback, useEffect, useState } from 'react';
+import {
+  ActivityLogClientService,
+  EventClientService,
+} from '../../../../helpers';
+import { EventClient, Event } from '@kalos-core/kalos-rpc/Event';
+import { Data, InfoTable, Row } from '../../InfoTable';
 
 export interface Props {}
 
 export const LogsTab: FC<Props> = ({}) => {
-  return <>Will be billing tab</>;
+  const [projectLogs, setProjectLogs] = useState<ActivityLog[]>();
+
+  const load = useCallback(async () => {
+    let req: any = new ActivityLog();
+    req.setNotEqualsList(['EventId']);
+    const logs = await ActivityLogClientService.BatchGet(req);
+
+    // Would have done all of this with a protobuffer field and an @inject_tag in the backend,
+    // but the ActivityLog would have to import Event to display an event and that would cause a
+    // circular dependency
+
+    // As a result, I'm just grabbing all of the projects and checking each real fast and filtering the logs
+    // by that. This isn't the fastest solution but it works for now and it's not terrible, still ~O(n^2)
+    req = new Event();
+    req.setNotEqualsList(['DepartmentId']);
+    req.setDepartmentId(0);
+    const projectEvents = await EventClientService.BatchGet(req);
+    let newResList = logs.getResultsList().filter(log => {
+      let isInside = false;
+      projectEvents.getResultsList().forEach(projectEvent => {
+        if (projectEvent.getId() === log.getEventId()) {
+          isInside = true;
+        }
+      });
+
+      return isInside;
+    });
+    logs.setResultsList(newResList);
+    setProjectLogs(logs.getResultsList());
+    console.log('Has loaded! ', projectLogs);
+  }, [setProjectLogs]);
+  useEffect(() => {
+    load();
+  }, [load]);
+  return (
+    <>
+      <InfoTable
+        key={projectLogs?.toString()}
+        data={projectLogs?.map(log => {
+          return [
+            { value: log.getId() },
+            { value: log.getUserId() },
+            { value: log.getActivityName() },
+            { value: log.getActivityDate() },
+            { name: log.getPropertyId() },
+          ] as Row;
+        })}
+        columns={[
+          { name: 'ID' },
+          { name: 'User ID' },
+          { name: 'Description' },
+          { name: 'Date' },
+          { name: 'Property ID' },
+        ]}
+      />
+    </>
+  );
 };
