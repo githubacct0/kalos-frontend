5f876cf17015c84a6871681af02aa66db0295070
githubacct0 <githubacct0@outlook.com>
2021-06-02 00:59:14 -0400

    Put the calls into a promise chain to load them faster since theyre slow

--- modules/ComponentsLibrary/ProjectDetail/components/Logs.tsx
+++ modules/ComponentsLibrary/ProjectDetail/components/Logs.tsx
@@ -1,10 +1,13 @@
-import { ActivityLog } from '@kalos-core/kalos-rpc/ActivityLog';
+import {
+  ActivityLog,
+  ActivityLogList,
+} from '@kalos-core/kalos-rpc/ActivityLog';
 import React, { FC, useCallback, useEffect, useState } from 'react';
 import {
   ActivityLogClientService,
   EventClientService,
 } from '../../../../helpers';
-import { EventClient, Event } from '@kalos-core/kalos-rpc/Event';
+import { EventClient, Event, EventList } from '@kalos-core/kalos-rpc/Event';
 import { Data, InfoTable, Row } from '../../InfoTable';
 import { Loader } from '../../../Loader/main';
 
@@ -16,34 +19,61 @@ export const LogsTab: FC<Props> = ({}) => {
 
   const load = useCallback(async () => {
     setLoading(true);
-    let req: any = new ActivityLog();
-    req.setNotEqualsList(['EventId']);
-    let logs = await ActivityLogClientService.BatchGet(req);
-
-    // Would have done all of this with a protobuffer field and an @inject_tag in the backend,
-    // but the ActivityLog would have to import Event to display an event and that would cause a
-    // circular dependency
-
-    // As a result, I'm just grabbing all of the projects and checking each real fast and filtering the logs
-    // by that. This isn't the fastest solution but it works for now and it's not terrible, still ~O(n^2)
-    req = new Event();
-    req.setNotEqualsList(['DepartmentId']);
-    req.setDepartmentId(0);
-    const projectEvents = await EventClientService.BatchGet(req);
-    let newResList = logs.getResultsList().filter(log => {
-      let isInside = false;
-      projectEvents.getResultsList().forEach(projectEvent => {
-        if (projectEvent.getId() === log.getEventId()) {
-          isInside = true;
+    let promises = []; // Doing promise array thing because these are pretty heavy calls and loading taking 30 seconds atm is killing me
+    let logs: ActivityLogList;
+    let projectEvents: EventList;
+    promises.push(
+      new Promise<void>(async resolve => {
+        try {
+          let req: any = new ActivityLog();
+          req.setNotEqualsList(['EventId']);
+          logs = await ActivityLogClientService.BatchGet(req);
+          resolve();
+        } catch (err) {
+          console.error(`An error occurred while getting activity logs: `, err);
+          resolve();
         }
-      });
+      }),
+    );
 
-      return isInside;
+    promises.push(
+      new Promise<void>(async resolve => {
+        try {
+          let req = new Event();
+          req.setNotEqualsList(['DepartmentId']);
+          req.setDepartmentId(0);
+          projectEvents = await EventClientService.BatchGet(req);
+          resolve();
+        } catch (err) {
+          console.error(`An error occurred while getting projects: `, err);
+          resolve();
+        }
+      }),
+    );
+
+    Promise.all(promises).then(() => {
+      // Would have done all of this with a protobuffer field and an @inject_tag in the backend,
+      // but the ActivityLog would have to import Event to display an event and that would cause a
+      // circular dependency
+
+      // As a result, I'm just grabbing all of the projects and checking each real fast and filtering the logs
+      // by that. This isn't the fastest solution but it works for now and it's not terrible, still ~O(n^2)
+
+      let newResList = logs.getResultsList().filter(log => {
+        let isInside = false;
+        projectEvents.getResultsList().forEach(projectEvent => {
+          if (projectEvent.getId() === log.getEventId()) {
+            isInside = true;
+          }
+        });
+
+        return isInside;
+      });
+      logs.setResultsList(newResList);
+      setProjectLogs(logs.getResultsList());
+      console.log('Has loaded! ', projectLogs);
+      setLoading(false);
     });
-    logs.setResultsList(newResList);
-    setProjectLogs(logs.getResultsList());
-    console.log('Has loaded! ', projectLogs);
-    setLoading(false);
   }, [setProjectLogs, setLoading]);
   useEffect(() => {
     load();
