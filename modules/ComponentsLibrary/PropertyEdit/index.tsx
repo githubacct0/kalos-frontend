93c578fd2366db50e6f3e65b1d53ee11f8ed372e
githubacct0 <githubacct0@outlook.com>
2021-06-17 12:08:05 -0400

    finished PropertyEdit

--- modules/ComponentsLibrary/PropertyEdit/index.tsx
+++ modules/ComponentsLibrary/PropertyEdit/index.tsx
@@ -1,11 +1,7 @@
 import React, { FC, useState, useCallback, useEffect } from 'react';
 import { Property } from '@kalos-core/kalos-rpc/Property';
 import { Form, Schema } from '../Form';
-import {
-  PropertyType,
-  PropertyClientService,
-  MapClientService,
-} from '../../../helpers';
+import { PropertyClientService, MapClientService } from '../../../helpers';
 import {
   RESIDENTIAL_OPTIONS,
   PROP_LEVEL,
@@ -13,11 +9,11 @@ import {
 } from '../../../constants';
 
 interface Props {
-  onSave?: (data: PropertyType) => void;
+  onSave?: (data: Property) => void;
   onClose: () => void;
   userId: number;
   propertyId?: number;
-  property?: PropertyType;
+  property?: Property;
   viewedAsCustomer?: boolean;
 }
 
@@ -34,9 +30,7 @@ export const PropertyEdit: FC<Props> = ({
   const [loading, setLoading] = useState<boolean>(false);
   const [loaded, setLoaded] = useState<boolean>(false);
   const [formKey, setFormKey] = useState<number>(0);
-  const [entry, setEntry] = useState<PropertyType>(
-    _property || new Property().toObject(),
-  );
+  const [entry, setEntry] = useState<Property>(_property || new Property());
   const load = useCallback(async () => {
     if (propertyId) {
       if (!_property) {
@@ -55,17 +49,22 @@ export const PropertyEdit: FC<Props> = ({
     }
   }, [loaded, setLoaded, load]);
   const handleCheckLocation = useCallback(async () => {
-    const { address, city, state: addressState, zip } = entry;
+    const address = entry.getAddress();
+    const city = entry.getCity();
+    const addressState = entry.getState();
+    const zip = entry.getZip();
     const geo = await MapClientService.loadGeoLocationByAddress(
       `${address}, ${city}, ${addressState} ${zip}`,
     );
     if (geo) {
-      setEntry({ ...entry, ...geo });
+      entry.setGeolocationLat(geo.geolocationLat);
+      entry.setGeolocationLng(geo.geolocationLng);
+      setEntry(entry);
       setFormKey(formKey + 1);
     }
   }, [entry, setEntry, formKey, setFormKey]);
   const handleSave = useCallback(
-    async (data: PropertyType) => {
+    async (data: Property) => {
       setSaving(true);
       const entry = await PropertyClientService.saveProperty(
         data,
@@ -73,7 +72,7 @@ export const PropertyEdit: FC<Props> = ({
         propertyId,
       );
       setEntry(entry);
-      setPropertyId(entry.id);
+      setPropertyId(entry.getId());
       setSaving(false);
       if (onSave) {
         onSave(entry);
@@ -81,7 +80,7 @@ export const PropertyEdit: FC<Props> = ({
     },
     [userId, propertyId, onSave, setSaving, setPropertyId],
   );
-  const SCHEMA_PROPERTY_INFORMATION: Schema<PropertyType> = [
+  const SCHEMA_PROPERTY_INFORMATION: Schema<Property> = [
     ...(viewedAsCustomer
       ? []
       : ([
@@ -93,9 +92,9 @@ export const PropertyEdit: FC<Props> = ({
             },
           ],
           [
-            { label: 'First Name', name: 'firstname' },
-            { label: 'Last Name', name: 'lastname' },
-            { label: 'Business Name', name: 'businessname' },
+            { label: 'First Name', name: 'setFirstname' },
+            { label: 'Last Name', name: 'setLastname' },
+            { label: 'Business Name', name: 'setBusinessname' },
           ],
           [
             {
@@ -105,22 +104,22 @@ export const PropertyEdit: FC<Props> = ({
             },
           ],
           [
-            { label: 'Primary Phone', name: 'phone' },
-            { label: 'Alternate Phone', name: 'altphone' },
-            { label: 'Email', name: 'email' },
+            { label: 'Primary Phone', name: 'setPhone' },
+            { label: 'Alternate Phone', name: 'setAltphone' },
+            { label: 'Email', name: 'setEmail' },
           ],
-        ] as Schema<PropertyType>)),
+        ] as Schema<Property>)),
     [{ label: 'Address Details', headline: true }],
     [
-      { label: 'Address', name: 'address', required: true, multiline: true },
-      { label: 'City', name: 'city', required: true },
+      { label: 'Address', name: 'setAddress', required: true, multiline: true },
+      { label: 'City', name: 'setCity', required: true },
       {
         label: 'State',
-        name: 'state',
+        name: 'setState',
         options: USA_STATES_OPTIONS,
         required: true,
       },
-      { label: 'Zip Code', name: 'zip', required: true },
+      { label: 'Zip Code', name: 'setZip', required: true },
     ],
     [
       {
@@ -141,8 +140,8 @@ export const PropertyEdit: FC<Props> = ({
       },
     ],
     [
-      { label: 'Directions', name: 'directions', multiline: true },
-      { label: 'Subdivision', name: 'subdivision' },
+      { label: 'Directions', name: 'setDirections', multiline: true },
+      { label: 'Subdivision', name: 'setSubdivision' },
     ],
     ...(viewedAsCustomer
       ? []
@@ -150,18 +149,18 @@ export const PropertyEdit: FC<Props> = ({
           [
             {
               label: 'Zoning',
-              name: 'isResidential',
+              name: 'setIsResidential',
               options: RESIDENTIAL_OPTIONS,
             },
-            { label: 'Latitude', name: 'geolocationLat', type: 'number' },
-            { label: 'Longitude', name: 'geolocationLng', type: 'number' },
+            { label: 'Latitude', name: 'setGeolocationLat', type: 'number' },
+            { label: 'Longitude', name: 'setGeolocationLng', type: 'number' },
           ],
-        ] as Schema<PropertyType>)),
+        ] as Schema<Property>)),
     [{ label: 'Notes', headline: true }],
-    [{ label: 'Notes', name: 'notes', multiline: true }],
+    [{ label: 'Notes', name: 'setNotes', multiline: true }],
   ];
   return (
-    <Form<PropertyType>
+    <Form<Property>
       key={formKey}
       title={propertyId ? 'Edit Property Information' : 'Add Property'}
       schema={SCHEMA_PROPERTY_INFORMATION}
