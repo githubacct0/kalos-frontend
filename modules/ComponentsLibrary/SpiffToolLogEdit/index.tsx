d6048b669a870eca5244bd4d3bea1fd80274f9af
githubacct0 <githubacct0@outlook.com>
2021-06-24 18:36:31 -0400

    Fixed issue in spiffs in ServiceCall

--- modules/ComponentsLibrary/SpiffToolLogEdit/index.tsx
+++ modules/ComponentsLibrary/SpiffToolLogEdit/index.tsx
@@ -148,7 +148,7 @@ export const getStatusFormInit = (status: number, reviewedBy?: string) => {
   if (reviewedBy) {
     entry.setReviewedBy(reviewedBy);
   }
-  return entry.toObject();
+  return entry;
 };
 
 export const SpiffToolLogEdit: FC<Props> = ({
@@ -205,59 +205,59 @@ export const SpiffToolLogEdit: FC<Props> = ({
       setStatusDeleting(statusDeleting),
     [setStatusDeleting],
   );
-  const handleFileLoad = useCallback(file => setDocumentFile(file), [
-    setDocumentFile,
-  ]);
+  const handleFileLoad = useCallback(
+    file => setDocumentFile(file),
+    [setDocumentFile],
+  );
   const handleDocumentUpload = useCallback(
-    (onClose, onReload) => async ({
-      filename,
-      description,
-    }: DocumentUpload) => {
-      setUploadFailed(false);
-      setUploading(true);
-      const ext = filename.split('.').pop();
-      const fileName =
-        kebabCase(
-          [
-            data.getId(),
-            data.getReferenceNumber(),
-            timestamp(true).split('-').reverse(),
-            description.trim() || filename.replace('.' + ext, ''),
-          ].join(' '),
-        ) +
-        '.' +
-        ext;
-      const status = await uploadFileToS3Bucket(
-        fileName,
-        documentFile,
-        'testbuckethelios', // FIXME is it correct bucket name for those docs?
-      );
-      if (status === 'ok') {
-        await DocumentClientService.createTaskDocument(
+    (onClose, onReload) =>
+      async ({ filename, description }: DocumentUpload) => {
+        setUploadFailed(false);
+        setUploading(true);
+        const ext = filename.split('.').pop();
+        const fileName =
+          kebabCase(
+            [
+              data.getId(),
+              data.getReferenceNumber(),
+              timestamp(true).split('-').reverse(),
+              description.trim() || filename.replace('.' + ext, ''),
+            ].join(' '),
+          ) +
+          '.' +
+          ext;
+        const status = await uploadFileToS3Bucket(
           fileName,
-          data.getId(),
-          loggedUserId,
-          description,
+          documentFile,
+          'testbuckethelios', // FIXME is it correct bucket name for those docs?
         );
-        onClose();
-        onReload();
-        setUploading(false);
-      } else {
-        setUploadFailed(true);
-        setUploading(false);
-      }
-    },
+        if (status === 'ok') {
+          await DocumentClientService.createTaskDocument(
+            fileName,
+            data.getId(),
+            loggedUserId,
+            description,
+          );
+          onClose();
+          onReload();
+          setUploading(false);
+        } else {
+          setUploadFailed(true);
+          setUploading(false);
+        }
+      },
     [documentFile, loggedUserId, data, setUploadFailed, setUploading],
   );
   const handleDocumentUpdate = useCallback(
-    (onClose, onReload, { id }) => async (form: Document) => {
-      setDocumentSaving(true);
-      const description = form.getDescription();
-      await DocumentClientService.updateDocumentDescription(id, description);
-      setDocumentSaving(false);
-      onClose();
-      onReload();
-    },
+    (onClose, onReload, { id }) =>
+      async (form: Document) => {
+        setDocumentSaving(true);
+        const description = form.getDescription();
+        await DocumentClientService.updateDocumentDescription(id, description);
+        setDocumentSaving(false);
+        onClose();
+        onReload();
+      },
     [setDocumentSaving],
   );
   const handleSave = useCallback(
