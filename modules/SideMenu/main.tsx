e9055dade39e19849eff8d7f0d9c73452e77e3ea
githubacct0 <githubacct0@outlook.com>
2021-06-25 15:38:27 -0400

    Fixed most bugs in SideMenu

--- modules/SideMenu/main.tsx
+++ modules/SideMenu/main.tsx
@@ -37,14 +37,14 @@ export type Props = {
 type State = {
   isManager: boolean;
   isOpen: boolean;
-  user: User.AsObject;
+  user: User;
   reportBugFormShown: boolean;
 };
 
 type Action =
   | { type: 'toggleMenu' }
   | { type: 'closeMenu' }
-  | { type: 'fetchedUser'; user: User.AsObject; isManager: boolean }
+  | { type: 'fetchedUser'; user: User; isManager: boolean }
   | { type: 'showReportBugForm' }
   | { type: 'hideReportBugForm' };
 
@@ -85,7 +85,7 @@ const SideMenu = ({
   imgURL = 'https://app.kalosflorida.com/app/assets/images/kalos-logo-new.png',
 }: Props) => {
   const [state, dispatch] = useReducer(reducer, {
-    user: new User().toObject(),
+    user: new User(),
     isManager: false,
     isOpen: false,
     reportBugFormShown: false,
@@ -111,11 +111,15 @@ const SideMenu = ({
       await userClient.GetToken('test', 'test');
       const userResult = await UserClientService.loadUserById(userID);
       //customerCheck(userResult);
-      if (userResult.isSu === 1) {
+      if (userResult.getIsSu() === 1) {
         dispatch({ type: 'fetchedUser', user: userResult, isManager: true });
       } else {
         try {
-          if (userResult.permissionGroupsList.find(p => p.name === 'Manager')) {
+          if (
+            userResult
+              .getPermissionGroupsList()
+              .find(p => p.getName() === 'Manager')
+          ) {
             dispatch({
               type: 'fetchedUser',
               user: userResult,
@@ -135,7 +139,7 @@ const SideMenu = ({
     })();
   }, [userID]);
 
-  if (!user?.id) {
+  if (!user?.getId()) {
     return null;
   }
 
@@ -165,7 +169,7 @@ const SideMenu = ({
         style={{ width: 250, padding: 10 }}
       >
         <List style={{ width: 250 }}>
-          {user.isEmployee === 1 &&
+          {user.getIsEmployee() === 1 &&
             employeeItems({
               toggleUploadReceipt: toggleOpenUploadReceipt,
             }).map(item => (
@@ -175,7 +179,7 @@ const SideMenu = ({
                 userId={userID}
               />
             ))}
-          {user.isAdmin === 1 && (
+          {user.getIsAdmin() === 1 && (
             <>
               {adminItems.map(item => (
                 <KalosMenuItem
@@ -197,7 +201,7 @@ const SideMenu = ({
               ))}
             </>
           )}
-          {user.isEmployee === 0 &&
+          {user.getIsEmployee() === 0 &&
             customerItems(toggleMenu).map(item => (
               <KalosMenuItem
                 key={`customer_${item?.title || 'divider'}`}
@@ -205,7 +209,7 @@ const SideMenu = ({
                 userId={userID}
               />
             ))}
-          {user.isEmployee === 1 &&
+          {user.getIsEmployee() === 1 &&
             commonItems.map(item => {
               if (item.title === 'Report a Bug') {
                 return (
