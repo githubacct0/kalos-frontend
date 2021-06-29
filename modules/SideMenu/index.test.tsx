9097d986bf60b5bd7908f361978d326e70ece6a6
githubacct0 <githubacct0@outlook.com>
2021-06-29 19:07:32 -0400

    Removed a ton of jest related files, re-installed

--- modules/SideMenu/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import SideMenu from './main';
-
-test('SideMenu renders correctly', () => {
-  const tree = renderer.create(<SideMenu userID={8418} />).toJSON();
-  expect(tree).toMatchSnapshot(); // Rendered correctly
-});
