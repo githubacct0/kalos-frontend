4e1744323088b1a8badc04a85bbbabfc45949715
githubacct0 <githubacct0@outlook.com>
2021-06-29 19:48:03 -0400

    First passing Mocha/chai test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should flat out work', function() {
+      
+      expect(1).to.equal(1)
+    });
+  });
+});
