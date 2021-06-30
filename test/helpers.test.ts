0fc577c1dae52bf801b4fb413d2b47f7999d64b6
githubacct0 <githubacct0@outlook.com>
2021-06-30 14:01:19 -0400

    Making a (failing) call to EventClientService within test

--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -6,6 +6,8 @@ const loadActivityLogsByFilter =
 const LoadActivityLogsByFilter =
   require('../helpers.ts').LoadActivityLogsByFilter;
 const milesFactor = require('../constants.ts').IRS_SUGGESTED_MILES_FACTOR;
+const EventType = require('@kalos-core/kalos-rpc/Event/index.ts').Event;
+const EventClientService = require('../helpers.ts').EventClientService;
 
 describe('helpers', () => {
   describe('#getMimeType', () => {
@@ -22,6 +24,27 @@ describe('helpers', () => {
     it('should not be null if given proper arguments', () => {
       expectImport(loadActivityLogsByFilter({ page: 0, filter: {}, sort: {} }))
         .not.to.be.null;
+    });
+  });
+});
+
+describe('rpc', () => {
+  describe('#EventClientService.Get', () => {
+    it('should work correctly for id 1', async () => {
+      let eventOut = new EventType();
+      eventOut.setId(1);
+      try {
+        console.log('Making the call');
+        const res = await EventClientService.Get(eventOut);
+        console.log('Got the event');
+      } catch (err) {
+        console.error(
+          `The EventClientService ran into an issue while getting the event: ${err}`,
+        );
+        expectImport.fail(
+          `The EventClientService ran into an issue while getting the event: ${err}`,
+        );
+      }
     });
   });
 });
