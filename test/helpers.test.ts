964d2d9795ff22cad2152328d09412a8ce694069
githubacct0 <githubacct0@outlook.com>
2021-06-30 02:47:12 -0400

    Made the imports the ts files, added all the helpers tests to one block and removed jest.config.ts (and added a watch command to test scripts)

--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -1,6 +1,6 @@
 const expectImport = require('chai').expect;
-const getMimeType = require('../helpers.js').getMimeType;
-const perDiemTripMilesToUsd = require('../helpers.js').perDiemTripMilesToUsd;
+const getMimeType = require('../helpers.ts').getMimeType;
+const perDiemTripMilesToUsd = require('../helpers.ts').perDiemTripMilesToUsd;
 const loadActivityLogsByFilter =
   require('../helpers.ts').loadActivityLogsByFilter;
 const LoadActivityLogsByFilter =
@@ -9,21 +9,15 @@ const milesFactor = require('../constants.ts').IRS_SUGGESTED_MILES_FACTOR;
 
 describe('helpers', () => {
   describe('#getMimeType', () => {
-    it('should return PNG', () => {
+    it('should return image/png when "test.png" is provided as argument', () => {
       expectImport(getMimeType('test.png')).to.equal('image/png');
     });
   });
-});
-
-describe('helpers', () => {
   describe('#perDiemTripMilesToUsd', () => {
-    it('should return the correct trip miles to usd depending on the constant set up', () => {
+    it('should return $ 33.60 when given 60 miles', () => {
       expectImport(perDiemTripMilesToUsd(60)).to.equal('$ 33.60');
     });
   });
-});
-
-describe('helpers', () => {
   describe('#loadActivityLogsByFilter', () => {
     it('should not be null if given proper arguments', () => {
       expectImport(loadActivityLogsByFilter({ page: 0, filter: {}, sort: {} }))
