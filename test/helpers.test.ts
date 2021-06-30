1d2b23cde2a99c864ba8ece304748e489eedc042
githubacct0 <githubacct0@outlook.com>
2021-06-30 01:54:34 -0400

    Third test

--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -1,7 +1,11 @@
-var expectImport = require('chai').expect;
-var getMimeType = require('../helpers.js').getMimeType;
-var perDiemTripMilesToUsd = require('../helpers.js').perDiemTripMilesToUsd;
-var milesFactor = require('../constants.ts').IRS_SUGGESTED_MILES_FACTOR;
+const expectImport = require('chai').expect;
+const getMimeType = require('../helpers.js').getMimeType;
+const perDiemTripMilesToUsd = require('../helpers.js').perDiemTripMilesToUsd;
+const loadActivityLogsByFilter =
+  require('../helpers.ts').loadActivityLogsByFilter;
+const LoadActivityLogsByFilter =
+  require('../helpers.ts').LoadActivityLogsByFilter;
+const milesFactor = require('../constants.ts').IRS_SUGGESTED_MILES_FACTOR;
 
 describe('helpers', () => {
   describe('#getMimeType', () => {
@@ -15,6 +19,15 @@ describe('helpers', () => {
   describe('#perDiemTripMilesToUsd', () => {
     it('should return the correct trip miles to usd depending on the constant set up', () => {
       expectImport(perDiemTripMilesToUsd(60)).to.equal('$ 33.60');
+    });
+  });
+});
+
+describe('helpers', () => {
+  describe('#loadActivityLogsByFilter', () => {
+    it('should not be null if given proper arguments', () => {
+      expectImport(loadActivityLogsByFilter({ page: 0, filter: {}, sort: {} }))
+        .not.to.be.null;
     });
   });
 });
