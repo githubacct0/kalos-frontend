9de94791d220c664f9def5ef550d7e3205380145
githubacct0 <githubacct0@outlook.com>
2021-06-30 01:42:46 -0400

    Wrote another test for a helpers function

--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -1,10 +1,20 @@
 var expectImport = require('chai').expect;
-var mimeType = require('../helpers.js').getMimeType;
+var getMimeType = require('../helpers.js').getMimeType;
+var perDiemTripMilesToUsd = require('../helpers.js').perDiemTripMilesToUsd;
+var milesFactor = require('../constants.ts').IRS_SUGGESTED_MILES_FACTOR;
 
 describe('helpers', () => {
   describe('#getMimeType', () => {
     it('should return PNG', () => {
-      expectImport(mimeType('test.png')).to.equal('image/png');
+      expectImport(getMimeType('test.png')).to.equal('image/png');
+    });
+  });
+});
+
+describe('helpers', () => {
+  describe('#perDiemTripMilesToUsd', () => {
+    it('should return the correct trip miles to usd depending on the constant set up', () => {
+      expectImport(perDiemTripMilesToUsd(60)).to.equal('$ 33.60');
     });
   });
 });
