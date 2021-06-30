1112691e5bc0662dc718e02a9f0c4031ec05456a
githubacct0 <githubacct0@outlook.com>
2021-06-30 01:38:35 -0400

    Passing test

--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -1,21 +1,10 @@
-// import {expect} from 'chai'
-// import * as helpers from '../helpers.js'
-
 var expectImport = require('chai').expect;
 var mimeType = require('../helpers.js').getMimeType;
-
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should flat out work', function () {
-      expectImport(1).to.equal(1);
-    });
-  });
-});
 
 describe('helpers', () => {
   describe('#getMimeType', () => {
     it('should return PNG', () => {
-      expectImport(mimeType('test.png')).to.equal('PNG');
+      expectImport(mimeType('test.png')).to.equal('image/png');
     });
   });
 });
