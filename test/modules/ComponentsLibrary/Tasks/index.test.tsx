301d099b7e2403b0e3f85ca724303711b7d7bdad
githubacct0 <githubacct0@outlook.com>
2021-07-08 03:25:05 -0400

    Checks that Tasks renders with a SectionBar and a PlainForm inside of a test (AKA we have enzyme completely working and rendering correctly for testing purposes, which is fantastic)

--- test/modules/ComponentsLibrary/Tasks/index.test.tsx
+++ test/modules/ComponentsLibrary/Tasks/index.test.tsx
@@ -1,6 +1,12 @@
+/* eslint-disable react/jsx-key */
+// ! Disabled key errors in ESLint because they incorrectly label the elements within certain expectations as needing keys when they don't and will not work with keys
 const Tasks = require('../../../../modules/ComponentsLibrary/Tasks').Tasks;
 const React = require('react');
 const shallow = require('enzyme').shallow;
+const SectionBar =
+  require('../../../../modules/ComponentsLibrary/SectionBar/index').SectionBar;
+const PlainForm =
+  require('../../../../modules/ComponentsLibrary/PlainForm/index').PlainForm;
 
 require('../../../grpc-endpoint.js'); // ? Required to run tests with RPCs in Mocha (because Mocha runs in a Node environment)
 require('../../../enzyme-setup.js'); // ? Required to run tests with Enzyme for React
@@ -8,8 +14,8 @@ const expectImport = require('../../../chai-setup.js').expectImport;
 
 describe('ComponentsLibrary', () => {
   describe('Tasks', () => {
-    describe('<Tasks />', () => {
-      it('renders a fragment wrapper', () => {
+    describe('<Tasks externalCode="customers" externalId={2573} loggedUserId={101253}/>', () => {
+      it('renders with a section bar and a plain form', () => {
         const wrapper = shallow(
           <Tasks
             externalCode="customers"
@@ -18,7 +24,12 @@ describe('ComponentsLibrary', () => {
           />,
         );
 
-        expectImport(wrapper.contains(<></>)).to.equal(true);
+        expectImport(
+          wrapper.containsAllMatchingElements([
+            <SectionBar></SectionBar>,
+            <PlainForm></PlainForm>,
+          ]),
+        ).to.equal(true);
       });
     });
   });
