4b2589864374ea4960cf8c37abd21ead68e15a46
githubacct0 <githubacct0@outlook.com>
2021-07-09 00:56:02 -0400

    Added a new test for CustomerDetails

--- test/modules/CustomerDetails/index.test.tsx
+++ test/modules/CustomerDetails/index.test.tsx
@@ -10,7 +10,8 @@ let SETUP_PATH_FROM_TEST_MODULES =
 
 const ContractInfo =
   require(`${MODULES_PATH_FROM_TEST}/CustomerDetails/components/ContractInfo`).ContractInfo;
-
+const SectionBar =
+  require(`${MODULES_PATH_FROM_TEST}/ComponentsLibrary/SectionBar/index`).SectionBar;
 let CustomerDetails =
   require(`${MODULES_PATH_FROM_TEST}/CustomerDetails/components/CustomerDetails`).CustomerDetails;
 let React = require('react');
@@ -23,12 +24,19 @@ let expectImport =
 
 describe('CustomerDetails', () => {
   describe('<CustomerDetails userID={2573} loggedUserId={101253} withHeader />', () => {
-    it('renders with a section bar and a plain form', () => {
+    it('renders a customer information div', () => {
       const wrapper = shallow(
         <CustomerDetails userID={2573} loggedUserId={101253} withHeader />,
       ).dive();
-
       expectImport(wrapper.find('div.CustomerInformation')).to.have.lengthOf(1);
+    });
+    it('renders a component with the title "Customer Information"', () => {
+      const wrapper = shallow(
+        <CustomerDetails userID={2573} loggedUserId={101253} withHeader />,
+      ).dive();
+      expectImport(
+        wrapper.find({ title: 'Customer Information' }),
+      ).to.have.lengthOf(1);
     });
   });
 });
