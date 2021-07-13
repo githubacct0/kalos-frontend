1c06aea83e345ff97f6232661dc85b5bf35725e8
githubacct0 <githubacct0@outlook.com>
2021-07-13 13:38:28 -0400

    Added top-level to list of scopes

--- test/test-constants/constants.ts
+++ test/test-constants/constants.ts
@@ -3,10 +3,11 @@ export const COMPONENTS_LIBRARY_PATH_FROM_TEST =
   '../../../../modules/ComponentsLibrary';
 
 export const MODULES_PATH_FROM_TEST = '../../../modules';
+export const MODULES_PATH_FROM_TOP_TEST = '../../modules';
 
 export const SETUP_PATH_FROM_TEST_MODULES = '../../test-setup';
 
-type Scope = ['ComponentsLibrary', 'Modules'];
+type Scope = ['ComponentsLibrary', 'Modules', 'TopLevel'];
 /**
  * @param  {string} name  The name of the module or component
  * @param  {keyofScope} scope  The scope which the call is being done in
@@ -26,6 +27,10 @@ export const GetPathFromName = (
       }`;
     case 'Modules':
       return `${MODULES_PATH_FROM_TEST}/${name}${includeIndex ? '/index' : ''}`;
+    case 'TopLevel':
+      return `${`${MODULES_PATH_FROM_TOP_TEST}/${name}${
+        includeIndex ? '/index' : ''
+      }`}`;
     default:
       console.error('Could not get path from name: The scope is not valid.');
   }
