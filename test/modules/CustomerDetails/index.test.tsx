9bc3de0a6108f171a7f5e68235d111ed7886f379
githubacct0 <githubacct0@outlook.com>
2021-07-22 19:24:51 -0400

    Fixed up CustomerDetails test and converted imports

--- test/modules/CustomerDetails/index.test.tsx
+++ test/modules/CustomerDetails/index.test.tsx
@@ -3,34 +3,34 @@ export {};
 /* eslint-disable react/jsx-key */
 // ! Disabled key errors in ESLint because they incorrectly label the elements within certain expectations as needing keys when they don't and will not work with keys
 
-const GetPathFromName =
-  require('../../test-constants/constants').GetPathFromName;
+import CustomerDetailsModule = require('../../../modules/CustomerDetails/components/CustomerDetails');
+import React = require('react');
+import Enzyme = require('enzyme');
 
-const CustomerDetails = require(`${GetPathFromName(
-  'CustomerDetails',
-  'Modules',
-)}/components/CustomerDetails`).CustomerDetails;
-const React = require('react');
-const shallow = require('enzyme').shallow;
-
-const expect = require('chai').expect;
+import Chai = require('chai');
 
 describe('CustomerDetails', () => {
   describe('<CustomerDetails userID={2573} loggedUserId={101253} withHeader />', () => {
     it('renders a customer information div', () => {
       // FIXME write out a more extensive "mount" test when possible now that we have mount working
-      const wrapper = shallow(
-        <CustomerDetails userID={2573} loggedUserId={101253} withHeader />,
+      const wrapper = Enzyme.shallow(
+        <CustomerDetailsModule.CustomerDetails
+          userID={2573}
+          loggedUserId={101253}
+        />,
       ).dive();
-      expect(wrapper.find('div.CustomerInformation')).to.have.lengthOf(1);
+      Chai.expect(wrapper.find('div.CustomerInformation')).to.have.lengthOf(1);
     });
     it('renders a component with the title "Customer Information"', () => {
-      const wrapper = shallow(
-        <CustomerDetails userID={2573} loggedUserId={101253} withHeader />,
+      const wrapper = Enzyme.shallow(
+        <CustomerDetailsModule.CustomerDetails
+          userID={2573}
+          loggedUserId={101253}
+        />,
       ).dive();
-      expect(wrapper.find({ title: 'Customer Information' })).to.have.lengthOf(
-        1,
-      );
+      Chai.expect(
+        wrapper.find({ title: 'Customer Information' }),
+      ).to.have.lengthOf(1);
     });
   });
 });
