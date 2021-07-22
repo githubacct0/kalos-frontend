e9eb4f16184c0309451ba9c89b906deb46314bed
githubacct0 <githubacct0@outlook.com>
2021-07-22 19:02:05 -0400

    Changed test consts to imports for Tasks

--- test/modules/ComponentsLibrary/Tasks/index.test.tsx
+++ test/modules/ComponentsLibrary/Tasks/index.test.tsx
@@ -3,36 +3,33 @@ export {};
 /* eslint-disable react/jsx-key */
 // ! Disabled key errors in ESLint because they incorrectly label the elements within certain expectations as needing keys when they don't and will not work with keys
 
-const GetPathFromName =
-  require('../../../test-constants/constants').GetPathFromName;
+import TasksModule = require('../../../../modules/ComponentsLibrary/Tasks/index');
+import React = require('react');
+import Enzyme = require('enzyme');
+import SectionBarModule = require('../../../../modules/ComponentsLibrary/SectionBar/index');
+import PlainFormModule = require('../../../../modules/ComponentsLibrary/PlainForm/index');
 
-const Tasks = require(GetPathFromName('Tasks', 'ComponentsLibrary')).Tasks;
-const React = require('react');
-const shallow = require('enzyme').shallow;
-const SectionBar =
-  require(GetPathFromName('SectionBar', 'ComponentsLibrary', true)).SectionBar;
-const PlainForm =
-  require(GetPathFromName('PlainForm', 'ComponentsLibrary', true)).PlainForm;
-
-const expect = require('chai').expect;
+import Chai = require('chai');
 
 describe('ComponentsLibrary', () => {
   describe('Tasks', () => {
     describe('<Tasks externalCode="customers" externalId={2573} loggedUserId={101253}/>', () => {
       it('renders with a section bar and a plain form', () => {
         // FIXME write out a more extensive "mount" test when possible now that we have mount working
-        const wrapper = shallow(
-          <Tasks
+        const wrapper = Enzyme.shallow(
+          <TasksModule.Tasks
             externalCode="customers"
             externalId={2573}
             loggedUserId={101253}
           />,
         );
 
-        expect(
+        Chai.expect(
           wrapper.containsAllMatchingElements([
-            <SectionBar></SectionBar>,
-            <PlainForm></PlainForm>,
+            <SectionBarModule.SectionBar></SectionBarModule.SectionBar>,
+            // ? Suppressing props errors because they aren't quite applicable for the uses of this test (we aren't rendering the component)
+            // @ts-expect-error
+            <PlainFormModule.PlainForm></PlainFormModule.PlainForm>,
           ]),
         ).to.equal(true);
       });
