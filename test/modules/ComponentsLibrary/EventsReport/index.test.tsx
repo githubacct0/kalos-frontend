df2a421eb1607a39991db402b4b5ea534132f9ed
githubacct0 <githubacct0@outlook.com>
2021-07-22 19:15:00 -0400

    Fixed imports to not be const in EventsReport test and got rid of Jest types to ensure that Jest doesn't give false errors when using autocomplete with Chai

--- test/modules/ComponentsLibrary/EventsReport/index.test.tsx
+++ test/modules/ComponentsLibrary/EventsReport/index.test.tsx
@@ -3,23 +3,19 @@ export {};
 /* eslint-disable react/jsx-key */
 // ! Disabled key errors in ESLint because they incorrectly label the elements within certain expectations as needing keys when they don't and will not work with keys
 
-const GetPathFromName =
-  require('../../../test-constants/constants').GetPathFromName;
+import EventsReportModule = require('../../../../modules/ComponentsLibrary/EventsReport/index');
+import React = require('react');
+import Enzyme = require('enzyme');
 
-const EventsReport =
-  require(GetPathFromName('EventsReport', 'ComponentsLibrary')).EventsReport;
-const React = require('react');
-const mount = require('enzyme').mount;
-
-const expect = require('chai').expect;
+import Chai = require('chai');
 
 describe('ComponentsLibrary', () => {
   describe('EventsReport', () => {
     describe('<EventsReport loggedUserId={101253} kind="jobStatus"filter={{ status: "Completed", startDate: "2019-10-01", endDate: "2019-10-03" }} onClose={() => console.log("CLOSE")} />', () => {
       it('renders with a "Job Status Report" title', () => {
         // FIXME make this test more thorough, it uses mount but only basically checks that the entire thing renders
-        const wrapper = mount(
-          <EventsReport
+        const wrapper = Enzyme.mount(
+          <EventsReportModule.EventsReport
             loggedUserId={101253}
             kind="jobStatus"
             filter={{
@@ -32,9 +28,9 @@ describe('ComponentsLibrary', () => {
         );
 
         // ! Print page and section bar title both have Job Status Report
-        expect(wrapper.find({ title: 'Job Status Report' })).to.have.lengthOf(
-          2,
-        );
+        Chai.expect(
+          wrapper.find({ title: 'Job Status Report' }),
+        ).to.have.lengthOf(2);
       });
     });
   });
