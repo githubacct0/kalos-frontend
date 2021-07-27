4e0d515147228899043f4d955a4d502e1ab28f25
githubacct0 <githubacct0@outlook.com>
2021-07-26 21:29:05 -0400

    Fixed errors in EditTransaction

--- modules/ComponentsLibrary/EditTransaction/examples.tsx
+++ modules/ComponentsLibrary/EditTransaction/examples.tsx
@@ -5,32 +5,38 @@ import { TransactionClientService } from '../../../helpers';
 import { ExampleTitle } from '../helpers';
 
 export default () => {
-  const [transactionTest, setTransactionTest] = useState<Transaction.AsObject>(
-    {} as Transaction.AsObject,
+  const [transactionTest, setTransactionTest] = useState<Transaction>(
+    {} as Transaction,
   );
+  const [loaded, setLoaded] = useState<boolean>();
 
   const handleLoadTransaction = useCallback(async () => {
     let txn: Transaction = new Transaction();
     txn.setId(3);
     setTransactionTest(await TransactionClientService.Get(txn));
-  }, [setTransactionTest, TransactionClientService]);
+    setLoaded(true);
+  }, [setTransactionTest, setLoaded]);
 
   useEffect(() => {
     // Grabs a transaction:
     handleLoadTransaction();
-  }, [transactionTest, handleLoadTransaction, TransactionClientService]);
+  }, [transactionTest, handleLoadTransaction]);
   return (
     <>
       <ExampleTitle>Transaction with ID 3 in the database: </ExampleTitle>
-      <EditTransaction
-        key={transactionTest.id}
-        transactionInput={transactionTest}
-        onSave={saved => {
-          alert('Check the console for results of save');
-          console.log(saved);
-        }}
-        onClose={() => alert('Would close')}
-      />
+      {loaded ? (
+        <EditTransaction
+          key={transactionTest.getId()}
+          transactionInput={transactionTest}
+          onSave={saved => {
+            alert('Check the console for results of save');
+            console.log(saved);
+          }}
+          onClose={() => alert('Would close')}
+        />
+      ) : (
+        <>Loading Example</>
+      )}
     </>
   );
 };
