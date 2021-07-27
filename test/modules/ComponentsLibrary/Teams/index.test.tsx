953dbe8a1b7fcb777225288a23fc40f5751ebb03
githubacct0 <githubacct0@outlook.com>
2021-07-26 23:00:15 -0400

    Put together shell of tests in preparation of TDD

---
+++ test/modules/ComponentsLibrary/Teams/index.test.tsx
@@ -0,0 +1,10 @@
+import React = require('react');
+import Enzyme = require('enzyme');
+import Chai = require('chai');
+import Teams = require('../../../../modules/ComponentsLibrary/Teams/index');
+
+describe('ComponentsLibrary', () => {
+  describe('Teams', () => {
+    describe('<Teams />', () => {});
+  });
+});
