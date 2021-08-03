c9a2833f09881104bb6487acaeb1e61b099d427f
githubacct0 <githubacct0@outlook.com>
2021-08-02 22:28:59 -0400

    Implemented more of the Teams module

--- modules/ComponentsLibrary/Teams/index.tsx
+++ modules/ComponentsLibrary/Teams/index.tsx
@@ -1,12 +1,38 @@
-import React, { FC, useState, useCallback, useEffect } from 'react';
-import './styles.less';
+import React, { FC, useCallback, useEffect, useState } from 'react';
+import { InfoTable } from '../InfoTable';
+import { Modal } from '../Modal';
+import { SectionBar } from '../SectionBar';
+import { CreateNewTeam } from './components/CreateNewTeam';
 
-type Style = {};
-
-interface Props extends Style {}
+interface Props {}
 
 export const Teams: FC<Props> = () => {
+  const [createTeamModalOpen, setCreateTeamModalOpen] = useState<boolean>();
+
+  const handleSetCreateTeamModalOpen = useCallback(
+    (isOpen: boolean) => setCreateTeamModalOpen(isOpen),
+    [setCreateTeamModalOpen],
+  );
+
   const load = useCallback(() => {}, []);
   useEffect(() => load(), [load]);
-  return <></>;
+  return (
+    <>
+      {createTeamModalOpen && (
+        <Modal open={true} onClose={() => handleSetCreateTeamModalOpen(false)}>
+          <CreateNewTeam onClose={() => handleSetCreateTeamModalOpen(false)} />
+        </Modal>
+      )}
+      <SectionBar
+        title="Teams"
+        actions={[
+          {
+            label: 'Create New Team',
+            onClick: () => handleSetCreateTeamModalOpen(true),
+          },
+        ]}
+      />
+      <InfoTable columns={[{ name: 'Team Name' }, { name: 'Team Admins' }]} />
+    </>
+  );
 };
