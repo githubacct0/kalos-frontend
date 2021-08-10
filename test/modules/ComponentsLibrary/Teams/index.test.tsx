fe1288668b3a1ca970105cde2125c00633b6c234
githubacct0 <githubacct0@outlook.com>
2021-08-09 23:50:33 -0400

    re-skipped the teams tests until I have time to work on them again

--- test/modules/ComponentsLibrary/Teams/index.test.tsx
+++ test/modules/ComponentsLibrary/Teams/index.test.tsx
@@ -219,7 +219,7 @@ describe('ComponentsLibrary', () => {
         });
       });
 
-      describe('"Create New Teams" modal', () => {
+      describe.skip('"Create New Teams" modal', () => {
         it('has a search field to search for an existing team', () => {
           throw new Error('Needs to be implemented');
         });
