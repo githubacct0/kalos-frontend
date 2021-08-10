9521151f5d14392e295fbdf0d8667d7502b81859
githubacct0 <githubacct0@outlook.com>
2021-08-10 15:01:08 -0400

    fixed some forgot 3.0 stuff added getToken to dashboard

--- modules/Dashboard/index.tsx
+++ modules/Dashboard/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Dashboard } from './main';
-
-ReactDOM.render(
-  <Dashboard userId={103285} withHeader />,
-  document.getElementById('root'),
-);
+import { UserClient } from '@kalos-core/kalos-rpc/User';
+import { ENDPOINT } from '../../constants';
+const u = new UserClient(ENDPOINT);
+u.GetToken('test', 'test').then(() => {
+  ReactDOM.render(
+    <Dashboard userId={103285} withHeader />,
+    document.getElementById('root'),
+  );
+});
