f65f4de18faab98a2b656c145595c2f7e6265181
githubacct0 <githubacct0@outlook.com>
2021-08-16 20:32:35 -0400

    Put all of the buttons into "describe" categories for tests in Transactions Table

--- test/modules/ComponentsLibrary/TransactionTable/index.test.tsx
+++ test/modules/ComponentsLibrary/TransactionTable/index.test.tsx
@@ -123,84 +123,104 @@ describe('ComponentsLibrary', () => {
 
         describe('Table row', () => {
           describe('Actions', () => {
-            it('has an icon to Copy Data to Clipboard', async () => {
-              await Constants.ReRenderAfterLoad();
-              wrapper.update();
-              Chai.expect(
-                wrapper
-                  .find({ title: 'Copy data to clipboard' })
-                  .filter('button'),
-              ).to.be.lengthOf(1);
+            describe('"Copy Data to Clipboard" button', () => {
+              it('has an icon to Copy Data to Clipboard', async () => {
+                await Constants.ReRenderAfterLoad();
+                wrapper.update();
+                Chai.expect(
+                  wrapper
+                    .find({ title: 'Copy data to clipboard' })
+                    .filter('button'),
+                ).to.be.lengthOf(1);
+              });
             });
 
-            it('has an icon to edit the transaction', async () => {
-              await Constants.ReRenderAfterLoad();
-              wrapper.update();
-              Chai.expect(
-                wrapper
-                  .find({ title: 'Edit this transaction' })
-                  .filter('button'),
-              ).to.be.lengthOf(1);
+            describe('"Edit this transaction" button', () => {
+              it('has an icon to edit the transaction', async () => {
+                await Constants.ReRenderAfterLoad();
+                wrapper.update();
+                Chai.expect(
+                  wrapper
+                    .find({ title: 'Edit this transaction' })
+                    .filter('button'),
+                ).to.be.lengthOf(1);
+              });
             });
 
-            it('has an icon to Upload File', async () => {
-              await Constants.ReRenderAfterLoad();
-              wrapper.update();
-              Chai.expect(
-                wrapper.find({ title: 'Upload File' }).filter('button'),
-              ).to.be.lengthOf(1);
+            describe('"Upload File" button', () => {
+              it('has an icon to Upload File', async () => {
+                await Constants.ReRenderAfterLoad();
+                wrapper.update();
+                Chai.expect(
+                  wrapper.find({ title: 'Upload File' }).filter('button'),
+                ).to.be.lengthOf(1);
+              });
             });
 
-            it('has an icon to View Photos and Documents', async () => {
-              await Constants.ReRenderAfterLoad();
-              wrapper.update();
-              Chai.expect(
-                wrapper
-                  .find({ title: 'View Photos and Documents' })
-                  .filter('span'), // Span because this is the one generated from the gallery, which doesn't output an HTML button
-              ).to.be.lengthOf(1);
+            describe('"View Photos and Documents" button', () => {
+              it('has an icon to View Photos and Documents', async () => {
+                await Constants.ReRenderAfterLoad();
+                wrapper.update();
+                Chai.expect(
+                  wrapper
+                    .find({ title: 'View Photos and Documents' })
+                    .filter('span'), // Span because this is the one generated from the gallery, which doesn't output an HTML button
+                ).to.be.lengthOf(1);
+              });
             });
 
-            it('has an icon to View activity log', async () => {
-              await Constants.ReRenderAfterLoad();
-              wrapper.update();
-              Chai.expect(
-                wrapper.find({ title: 'View activity log' }).filter('button'),
-              ).to.be.lengthOf(1);
+            describe('"View activity log" button', () => {
+              it('has an icon to View activity log', async () => {
+                await Constants.ReRenderAfterLoad();
+                wrapper.update();
+                Chai.expect(
+                  wrapper.find({ title: 'View activity log' }).filter('button'),
+                ).to.be.lengthOf(1);
+              });
             });
 
-            it('has an icon to View notes', async () => {
-              await Constants.ReRenderAfterLoad();
-              wrapper.update();
-              Chai.expect(
-                wrapper.find({ title: 'View notes' }).filter('span'),
-              ).to.be.lengthOf(1);
+            describe('"View Notes" button', () => {
+              it('has an icon to View notes', async () => {
+                await Constants.ReRenderAfterLoad();
+                wrapper.update();
+                Chai.expect(
+                  wrapper.find({ title: 'View notes' }).filter('span'),
+                ).to.be.lengthOf(1);
+              });
             });
 
-            it('has an icon to Mark as accepted', async () => {
-              await Constants.ReRenderAfterLoad();
-              wrapper.update();
-              Chai.expect(
-                wrapper.find({ title: 'Mark as accepted' }).filter('button'),
-              ).to.be.lengthOf(1);
+            describe('"Mark as accepted" button', () => {
+              it('has an icon to Mark as accepted', async () => {
+                await Constants.ReRenderAfterLoad();
+                wrapper.update();
+                Chai.expect(
+                  wrapper.find({ title: 'Mark as accepted' }).filter('button'),
+                ).to.be.lengthOf(1);
+              });
             });
 
-            it('has an icon to Assign an employee to this task', async () => {
-              await Constants.ReRenderAfterLoad();
-              wrapper.update();
-              Chai.expect(
-                wrapper
-                  .find({ title: 'Assign an employee to this task' })
-                  .filter('button'),
-              ).to.be.lengthOf(1);
+            describe('"Assign an employee to this task" button', () => {
+              it('has an icon to Assign an employee to this task', async () => {
+                await Constants.ReRenderAfterLoad();
+                wrapper.update();
+                Chai.expect(
+                  wrapper
+                    .find({ title: 'Assign an employee to this task' })
+                    .filter('button'),
+                ).to.be.lengthOf(1);
+              });
             });
 
-            it('has an icon to Reject transaction', async () => {
-              await Constants.ReRenderAfterLoad();
-              wrapper.update();
-              Chai.expect(
-                wrapper.find({ title: 'Reject transaction' }).filter('button'),
-              ).to.be.lengthOf(1);
+            describe('"Reject transaction" button', () => {
+              it('has an icon to Reject transaction', async () => {
+                await Constants.ReRenderAfterLoad();
+                wrapper.update();
+                Chai.expect(
+                  wrapper
+                    .find({ title: 'Reject transaction' })
+                    .filter('button'),
+                ).to.be.lengthOf(1);
+              });
             });
           });
         });
