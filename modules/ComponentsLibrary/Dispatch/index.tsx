bb1594f25747173653223891bdd6eeec58a09a64
githubacct0 <githubacct0@outlook.com>
2021-08-18 16:48:13 -0400

    added department multipicker to dispatch

--- modules/ComponentsLibrary/Dispatch/index.tsx
+++ modules/ComponentsLibrary/Dispatch/index.tsx
@@ -3,11 +3,13 @@ import { PageWrapper } from '../../PageWrapper/main';
 import { State, reducer } from './reducer';
 import { DispatchTechs } from './dispatchTechnicians';
 import { DispatchCalls } from './dispatchCalls';
+import { FormData } from './reducer';
 // import {} from './dispatchMap';
-import { 
+import {
   DispatchClientService,
   TimeoffRequestClientService,
   ServicesRenderedClientService,
+  TimesheetDepartmentClientService,
 } from '../../../helpers';
 import { InfoTable } from '../InfoTable';
 import { DateRange } from '@kalos-core/kalos-rpc/compiled-protos/common_pb';
@@ -15,6 +17,7 @@ import { format } from 'date-fns';
 import { SectionBar } from '../SectionBar';
 import { PlainForm, Schema } from '../PlainForm';
 import { DepartmentPicker } from '../Pickers';
+import { TimesheetDepartment } from '@kalos-core/kalos-rpc/TimesheetDepartment';
 
 export interface Props {
   userID: number;
@@ -24,78 +27,106 @@ export interface Props {
 //   departmentIds : number[];
 //   jobTypes : number[];
 // };
+const initialFormData: FormData = {
+  departmentIds: [],
+};
+const initialState: State = {
+  techs: [],
+  dismissedTechs: [],
+  calls: [],
+  departmentIds: [0],
+  jobTypes: [0],
+  departmentList: [],
+  formData: initialFormData,
+};
 
-const initialState : State = {
-  techs : [],
-  dismissedTechs : [],
-  calls : [],
-  departmentIds : [0],
-  jobTypes : [0],
-}
-
-export const DispatchDashboard: React.FC<Props> = function DispatchDashboard({ userID }) {
+export const DispatchDashboard: React.FC<Props> = function DispatchDashboard({
+  userID,
+}) {
   const [state, dispatchDashboard] = useReducer(reducer, initialState);
 
-  const handleSetFilter = (data : number) => {
+  const handleSetFilter = (data: number) => {
     if (state.departmentIds.length == 1 && state.departmentIds[0] == 0) {
-      dispatchDashboard({ type: 'setDepartmentIds', data: [data]});
+      dispatchDashboard({ type: 'setDepartmentIds', data: [data] });
     } else {
       let currentDepartmentIds = state.departmentIds;
       currentDepartmentIds.push(data);
-      dispatchDashboard({ type: 'setDepartmentIds', data: currentDepartmentIds})
+      dispatchDashboard({
+        type: 'setDepartmentIds',
+        data: currentDepartmentIds,
+      });
     }
-  }
+  };
 
-  const fetchTechs = async function() {
+  const fetchTechs = async function () {
     const dateRange = new DateRange();
-    dateRange.setStart("2012-01-01");
-    dateRange.setEnd(format(new Date(), "yyyy-MM-dd"));
-    const result = await DispatchClientService.GetDispatchableTechnicians(dateRange);
+    dateRange.setStart('2012-01-01');
+    dateRange.setEnd(format(new Date(), 'yyyy-MM-dd'));
+    const result = await DispatchClientService.GetDispatchableTechnicians();
     console.log(result);
-    dispatchDashboard({ type : 'setTechs', data : result.getResultsList()});
-  }
+    dispatchDashboard({ type: 'setTechs', data: result.getResultsList() });
+  };
 
-  const fetchCalls = async function() {
+  const fetchCalls = async function () {
     const result = await DispatchClientService.GetDispatchCalls();
-    dispatchDashboard({ type: 'setCalls', data : result.getResultsList()});
-  }
+    dispatchDashboard({ type: 'setCalls', data: result.getResultsList() });
+  };
+  const handleChange = useCallback((formData: FormData) => {
+    dispatchDashboard({
+      type: 'setFormData',
+      data: formData,
+    });
+  }, []);
 
-  const load = useCallback( async () => {
+  const load = useCallback(async () => {
     await fetchTechs();
     await fetchCalls();
-  },[]);
+    const departmentReq = new TimesheetDepartment();
+    departmentReq.setIsActive(1);
+    const departments = await TimesheetDepartmentClientService.BatchGet(
+      departmentReq,
+    );
+    dispatchDashboard({
+      type: 'setDepartmentList',
+      data: departments.getResultsList(),
+    });
+  }, []);
+  const SCHEMA_PRINT: Schema<FormData> = [
+    [
+      {
+        name: 'departmentIds',
+        label: 'Department(s)',
+        options: state.departmentList.map(el => ({
+          label: el.getDescription(),
+          value: el.getId(),
+        })),
+        type: 'multiselect',
+      },
+    ],
+  ];
 
   useEffect(() => {
     load();
   }, [load]);
-  
-    return (
-      <PageWrapper userID={userID}> 
-        <SectionBar title="Dispatch" />
-        <DepartmentPicker
-          // @ts-ignore
-          selected={state.departmentIds}
-          renderItem={i => (
-            <option
-              value={i.getId()}
-              key={`${i.getId()}-department-select`}
-            >
-              {i.getDescription()} - {i.getValue()}
-            </option>
-          )}
-          multiple
-          // @ts-ignore
-          onSelect={(departmentID : React.ChangeEvent<{ value : Array }>) => {
-            handleSetFilter(departmentID.target.value[1]);
-            // if (departmentID.target.value) {
-              // dispatchDashboard({ type: 'setDepartmentId', data: departmentID);
-            // }
-          }}
-          withinForm
-        />
-        <DispatchTechs userID={userID} techs={state.techs} departmentIDs={state.departmentIds}/>
-        <DispatchCalls userID={userID} calls={state.calls} jobTypes={state.jobTypes}/>
-      </PageWrapper>
-    )
-  
-}
+  console.log(state.formData);
+  return (
+    <PageWrapper userID={userID}>
+      <SectionBar title="Dispatch" />
+      <PlainForm
+        schema={SCHEMA_PRINT}
+        data={initialFormData}
+        onChange={handleChange}
+      />
+      <DispatchTechs
+        userID={userID}
+        techs={state.techs}
+        departmentIDs={state.departmentIds}
+      />
+      <DispatchCalls
+        userID={userID}
+        calls={state.calls}
+        jobTypes={state.jobTypes}
+      />
+    </PageWrapper>
+  );
+};
--- modules/ComponentsLibrary/Dispatch/reducer.tsx
+++ modules/ComponentsLibrary/Dispatch/reducer.tsx
@@ -1,45 +1,63 @@
-import { DispatchableTech, DispatchCall } from "@kalos-core/kalos-rpc/Dispatch";
-import { Event } from "@kalos-core/kalos-rpc/Event";
+import { DispatchableTech, DispatchCall } from '@kalos-core/kalos-rpc/Dispatch';
+import { Event } from '@kalos-core/kalos-rpc/Event';
+import { TimesheetDepartment } from '@kalos-core/kalos-rpc/TimesheetDepartment';
 
+export type FormData = {
+  departmentIds: number[];
+};
 export interface State {
-  techs : DispatchableTech[];
-  dismissedTechs : DispatchableTech[];
-  calls : DispatchCall[];
-  departmentIds : number[];
-  jobTypes : number[];
+  techs: DispatchableTech[];
+  dismissedTechs: DispatchableTech[];
+  calls: DispatchCall[];
+  departmentIds: number[];
+  jobTypes: number[];
+  departmentList: TimesheetDepartment[];
+  formData: FormData;
 }
 
-export type Action = 
-  | { type : 'setTechs'; data : DispatchableTech[] }
-  | { type : 'setDismissedTechs'; data : DispatchableTech[] }
-  | { type : 'setCalls'; data : DispatchCall[] }
-  | { type : 'setDepartmentIds'; data : number[] }
-  | { type : 'setJobTypes'; data: number[] }
+export type Action =
+  | { type: 'setTechs'; data: DispatchableTech[] }
+  | { type: 'setDismissedTechs'; data: DispatchableTech[] }
+  | { type: 'setCalls'; data: DispatchCall[] }
+  | { type: 'setDepartmentIds'; data: number[] }
+  | { type: 'setJobTypes'; data: number[] }
+  | { type: 'setDepartmentList'; data: TimesheetDepartment[] }
+  | { type: 'setFormData'; data: FormData };
 
-export const reducer = (state : State, action : Action) => {
-  switch (action.type){
+export const reducer = (state: State, action: Action) => {
+  switch (action.type) {
     case 'setTechs':
       return {
         ...state,
-        techs : action.data
-      }
+        techs: action.data,
+      };
     case 'setDismissedTechs':
       return {
         ...state,
-        dismissedtechs : action.data
-      }
+        dismissedtechs: action.data,
+      };
     case 'setCalls':
       return {
         ...state,
-        calls : action.data
-      }
+        calls: action.data,
+      };
     case 'setDepartmentIds':
       return {
         ...state,
-        departmentIds : action.data
-      }
+        departmentIds: action.data,
+      };
+    case 'setDepartmentList':
+      return {
+        ...state,
+        departmentList: action.data,
+      };
+    case 'setFormData':
+      console.log(state.formData);
+      return {
+        ...state,
+        formData: action.data,
+      };
     default:
-      return state
+      return state;
   }
-
-}
+};
