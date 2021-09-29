a7606823dc2aa46731f7981dfa0d502f97357c37
githubacct0 <githubacct0@outlook.com>
2021-09-28 22:07:04 -0400

    Fleshed out NewContract component

--- modules/ComponentsLibrary/NewContract/index.tsx
+++ modules/ComponentsLibrary/NewContract/index.tsx
@@ -4,58 +4,72 @@
 
 */
 
+import { Contract } from '@kalos-core/kalos-rpc/Contract';
 import React, { useReducer, useEffect, useCallback, FC } from 'react';
+import { Form, Schema } from '../Form';
+import { SectionBar } from '../SectionBar';
 import { reducer, ACTIONS } from './reducer';
 
-// add any prop types here
-interface props { }
+interface props {
+  userID: number;
+  onSave: (savedContract: Contract) => any;
+  onClose: () => any;
+}
 
-export const NewContract: FC<props> = function NewContract() {
-  const [state, dispatch] = useReducer(reducer, { 
+const NEW_CONTRACT_SCHEMA: Schema<Contract> = [
+  [
+    {
+      label: 'Start Date',
+      type: 'date',
+      name: 'getDateStarted',
+      required: true,
+    },
+  ],
+  [
+    {
+      label: 'End Date',
+      type: 'date',
+      name: 'getDateEnded',
+      required: true,
+    },
+  ],
+];
+
+export const NewContract: FC<props> = ({ onSave, onClose }) => {
+  const [state, dispatch] = useReducer(reducer, {
     isLoaded: false,
+    contractData: new Contract(),
   });
 
   const load = useCallback(() => {
-    // RPCs that are in here should be stubbed in the tests at least 9 times out of 10. 
-    // This ensures that the fake data gets "loaded" instantly and the tests can progress quickly and without RPC errors 
-    // For some examples, check out /test/modules/Teams or /test/modules/Payroll
-
-    dispatch({type: ACTIONS.SET_LOADED, data: true});
+    dispatch({ type: ACTIONS.SET_LOADED, data: true });
   }, []);
 
-  const cleanup = useCallback(() => {
-    // TODO clean up your function calls here (called once the component is unmounted, prevents "Can't perform a React state update on an unmounted component" errors)
-    // This is important for long-term performance of our components
-  }, []);
+  const cleanup = useCallback(() => {}, []);
+
+  const save = useCallback(() => {
+    onSave(state.contractData);
+  }, [onSave, state.contractData]);
 
   useEffect(() => {
     load();
 
     return () => {
-      cleanup(); 
+      cleanup();
     };
   }, [load, cleanup]);
 
-  return ( 
-    <> 
-      <h1>NewContract works!</h1>
-      <h2>Checklist</h2>
-      <ul>
-        <li>Please add more tests for your component as you write your functionality (preferably to a spec or design document of some kind)</li>
-        <li>Please ensure your component can properly display errors (for a good example, look at errors in <code>InfoTable</code>)</li>
-        <li>Please link your design doc / spec at the top of this file inside the comments (and in the test file as well in the same place)</li>
-      </ul>
-      <h2>About this boilerplate</h2>
-      <ul>
-        <li>Frontend unit tests were created for this module automatically in <code>/test/modules/ComponentsLibrary/NewContract</code></li>
-        <li>You can see the templates for <code>yarn make</code> inside <code>/templates</code></li>
-      </ul>
-      <h2>Some resources to get you started with unit testing in Mocha with Enzyme: </h2>
-      <ul>
-        <li><a href="https://www.robinwieruch.de/react-testing-mocha-chai-enzyme-sinon">This article</a> is extremely long, but it <strong>MOSTLY</strong> shows you how to do what we aim to do</li>
-        <li>There is a readme in <code>/test</code> that goes into how to get started from a technical standpoint in our current setup</li>
-        <li><code>/modules/ComponentsLibrary/Test</code> is the component that was unit-tested on call during our meeting, and <code>/test/modules/ComponentsLibrary/Test</code> is where its corresponding tests are located</li> 
-      </ul>
-    </>
+  return (
+    <SectionBar
+      title="New Contract"
+      actions={[{ label: 'Cancel', onClick: () => onClose() }]}
+    >
+      <Form
+        schema={NEW_CONTRACT_SCHEMA}
+        data={state.contractData}
+        onSave={() => save()}
+        onClose={() => onClose()}
+      />
+    </SectionBar>
   );
 };
--- test/modules/ComponentsLibrary/NewContract/index.test.tsx
+++ test/modules/ComponentsLibrary/NewContract/index.test.tsx
@@ -7,7 +7,7 @@
 
   Design Specification: 
 
-*/ 
+*/
 
 export {};
 
@@ -16,24 +16,164 @@ import React = require('react');
 import Enzyme = require('enzyme');
 
 import Chai = require('chai');
+
+let saves = false;
+let closes = false;
 describe('ComponentsLibrary', () => {
   describe('NewContract', () => {
-    describe('<NewContract />', () => {
-      let wrapper: Enzyme.ReactWrapper; 
+    describe('<NewContract userID={8418} />', () => {
+      let wrapper: Enzyme.ReactWrapper;
       before(() => {
         wrapper = Enzyme.mount(
-          <NewContract.NewContract />,
+          <NewContract.NewContract
+            userID={8418}
+            onSave={() => {
+              saves = true;
+            }}
+            onClose={() => {
+              closes = true;
+            }}
+          />,
         );
       });
       after(() => {
         wrapper.unmount();
       });
+      afterEach(() => {
+        // Reset these after each test
+        saves = false;
+        closes = false;
+      });
 
       it('renders correctly', () => {
-        Chai.expect(wrapper.text().includes('NewContract works!')).to.equal(true);      
-      })
+        Chai.expect(wrapper.text().includes('New Contract')).to.equal(true);
+      });
 
-      // Rest of the spec tests here, make them pass as you go
+      describe('New Contract Section', () => {
+        it('exists', () => {
+          Chai.expect(wrapper.find({ title: 'New Contract' })).to.be.lengthOf(
+            1,
+          );
+        });
+
+        describe('cancel button', () => {
+          it('contains a cancel button', () => {
+            Chai.expect(
+              wrapper
+                .find('.MuiButton-label')
+                .filterWhere(button => button.text() === 'Cancel'),
+            ).to.be.lengthOf(1);
+          });
+
+          it('fires an onClose off when clicked', () => {
+            wrapper
+              .find('.MuiButton-label')
+              .filterWhere(button => button.text() === 'Cancel')
+              .simulate('click');
+            Chai.expect(closes).to.be.equal(true);
+          });
+        });
+
+        describe('Start Date Field', () => {
+          it('Contains a start date field', () => {
+            Chai.expect(wrapper.text().includes('Start Date')).to.be.equal(
+              true,
+            );
+          });
+
+          it('is required', () => {
+            Chai.expect(
+              wrapper.find({ label: 'Start Date' }).find({ required: true }),
+            ).to.be.lengthOf(1);
+          });
+
+          it('is a date selector', () => {
+            Chai.expect(
+              wrapper.find({ type: 'date' }).find({ label: 'Start Date' }),
+            ).to.be.lengthOf(1);
+          });
+        });
+
+        describe('End Date Field', () => {
+          it('Contains an end date field', () => {
+            Chai.expect(wrapper.text().includes('End Date')).to.be.equal(true);
+          });
+          it('is required', () => {
+            Chai.expect(
+              wrapper.find({ label: 'End Date' }).find({ required: true }),
+            ).to.be.lengthOf(1);
+          });
+
+          it('is a date selector', () => {
+            Chai.expect(
+              wrapper.find({ type: 'date' }).find({ label: 'End Date' }),
+            ).to.be.lengthOf(1);
+          });
+        });
+
+        describe('Frequency Field', () => {
+          it('Contains a frequency field');
+          it('is required');
+
+          describe('dropdown', () => {
+            it('has a monthly setting');
+
+            it('has a bi-monthly setting');
+
+            it('has a quarterly setting');
+
+            it('has a bi-annual setting');
+
+            it('has a yearly setting');
+          });
+        });
+
+        describe('Billing Section', () => {
+          it('Contains a billing section');
+          it('is required');
+
+          describe('dropdown', () => {
+            it('has a site setting');
+            it('has a group setting');
+          });
+        });
+
+        describe('Payment Type Section', () => {
+          it('Contains a payment type section');
+          it('is required');
+          it('has a payment type dropdown');
+        });
+
+        describe('Payment Status Section', () => {
+          it('Contains a payment status section');
+          it('is required');
+          describe('dropdown', () => {
+            // Pending billed cancelled paid
+            it('has a pending setting');
+            it('has a billed setting');
+            it('has a cancelled setting');
+            it('has a paid setting');
+          });
+        });
+
+        describe('Payment Terms Section', () => {
+          it('Contains a payment terms section');
+
+          it('is a single-line field');
+        });
+
+        describe('Notes Section', () => {
+          it('Contains a notes section');
+
+          it('is a multi-line field');
+        });
+
+        describe('Property Selector Section', () => {
+          it('Contains a property selector');
+          it('is required');
+          it('is a checkbox selection field');
+        });
+      });
     });
-  })
+  });
 });
