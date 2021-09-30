b5cf1891cbacd70c69ed57e63357ce321f696414
githubacct0 <githubacct0@outlook.com>
2021-09-30 16:12:21 -0400

    modified spiff tool log to fully support tool stuff, such as applying. Started working on reducer, but got distracted with the former

--- modules/SpiffToolLogs/index.tsx
+++ modules/SpiffToolLogs/index.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import { SpiffToolLogs } from './main';
 
 ReactDOM.render(
-  <SpiffToolLogs type="Spiff" loggedUserId={66} withHeader />,
+  <SpiffToolLogs type="Tool" loggedUserId={2512} withHeader />,
   document.getElementById('root'),
 );
