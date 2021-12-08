9a7280b81fadaf006cd973163195e36c13be31ea
githubacct0 <githubacct0@outlook.com>
2021-12-08 10:41:23 -0500

    fixing payroll access

--- modules/ComponentsLibrary/Payroll/index.tsx
+++ modules/ComponentsLibrary/Payroll/index.tsx
@@ -269,7 +269,6 @@ export const Payroll: FC<Props> = ({ userID }) => {
             {role === 'Manager' && (
               <Button
                 label={'Open Job Report(s)'}
-                disabled={true}
                 onClick={() => setOpenReport(true)}
               ></Button>
             )}
