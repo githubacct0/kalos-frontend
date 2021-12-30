9e7318be2ca0e96e29438180b0ed18ecb9f0c719
githubacct0 <githubacct0@outlook.com>
2021-12-30 16:36:36 -0500

    Set up error handling in InvoicePreview for the rpcs

--- modules/ComponentsLibrary/InvoicePreview/examples.tsx
+++ modules/ComponentsLibrary/InvoicePreview/examples.tsx
@@ -5,6 +5,9 @@ import { ExampleTitle } from '../helpers';
 export default () => (
   <>
     <ExampleTitle>Default</ExampleTitle>
+    <InvoicePreview loggedUserId={8418} propertyId={4404} invoiceId={15} />
+    <ExampleTitle>Error</ExampleTitle>
+    {/* @ts-expect-error */}
     <InvoicePreview loggedUserId={8418} />
   </>
 );
--- modules/ComponentsLibrary/InvoicePreview/index.tsx
+++ modules/ComponentsLibrary/InvoicePreview/index.tsx
@@ -9,82 +9,114 @@
 import React, { useReducer, useEffect, useCallback, FC } from 'react';
 import { reducer, ACTIONS } from './reducer';
 import { Devlog } from '@kalos-core/kalos-rpc/Devlog';
-import {
-  DevlogClientService,
-} from '../../../helpers';
+import { DevlogClientService, EmailClientService } from '../../../helpers';
 import { format } from 'date-fns';
+import Typography from '@material-ui/core/Typography';
+import { Document } from '@kalos-core/kalos-rpc/Document';
 
 // add any prop types here
-interface props { 
+interface props {
   loggedUserId: number;
+  propertyId: number;
+  invoiceId: number;
 }
 
-export const InvoicePreview: FC<props> = ({ loggedUserId }) => {
-  const [state, dispatch] = useReducer(reducer, { 
+export const InvoicePreview: FC<props> = ({
+  loggedUserId,
+  propertyId,
+  invoiceId,
+}) => {
+  const [state, dispatch] = useReducer(reducer, {
     isLoaded: false,
     error: undefined,
   });
-
-  const load = useCallback(() => {
-    // RPCs that are in here should be stubbed in the tests at least 9 times out of 10. 
-    // This ensures that the fake data gets "loaded" instantly and the tests can progress quickly and without RPC errors 
-    // For some examples, check out /test/modules/Teams or /test/modules/Payroll
-
-    dispatch({type: ACTIONS.SET_LOADED, data: true});
-  }, []);
 
   const cleanup = useCallback(() => {
     // TODO clean up your function calls here (called once the component is unmounted, prevents "Can't perform a React state update on an unmounted component" errors)
     // This is important for long-term performance of our components
   }, []);
 
-  const handleError = useCallback(async (errorToSet: string) => {
-    // This will send out an error devlog automatically when called
-    // The idea is that this will be used for any errors which we should be able to look up for debugging
+  const handleError = useCallback(
+    async (errorToSet: string) => {
+      // This will send out an error devlog automatically when called
+      // The idea is that this will be used for any errors which we should be able to look up for debugging
 
-    try {
-      let errorLog = new Devlog();
-      errorLog.setUserId(loggedUserId);
-      errorLog.setTimestamp(format(new Date(), 'yyyy-MM-dd hh:mm:ss'));
-      errorLog.setIsError(1);
-      errorLog.setDescription(
-        `An error occurred in InvoicePreview: ${errorToSet}`,
+      try {
+        let errorLog = new Devlog();
+        errorLog.setUserId(loggedUserId);
+        errorLog.setTimestamp(format(new Date(), 'yyyy-MM-dd hh:mm:ss'));
+        errorLog.setIsError(1);
+        errorLog.setDescription(
+          `An error occurred in InvoicePreview: ${errorToSet}`,
+        );
+        await DevlogClientService.Create(errorLog);
+        dispatch({ type: ACTIONS.SET_ERROR, data: errorToSet });
+      } catch (err) {
+        console.error(`An error occurred while saving a devlog: ${err}`);
+      }
+    },
+    [loggedUserId],
+  );
+
+  const getInvoiceBody = useCallback(async () => {
+    if (!propertyId) {
+      console.error(
+        'No property id was provided for getInvoiceBody. Please supply propertyId as a prop to InvoicePreview. If you see this, report it in #webtech.',
       );
-      await DevlogClientService.Create(errorLog);
-      dispatch({type: ACTIONS.SET_ERROR, data: errorToSet});
-    } catch (err) {
-      console.error(`An error occurred while saving a devlog: ${err}`);
+      dispatch({
+        type: ACTIONS.SET_ERROR,
+        data: 'No property id was provided for getInvoiceBody. Please supply propertyId as a prop to InvoicePreview. If you see this, report it in #webtech.',
+      });
+      return;
     }
-  }, [loggedUserId]);
+    if (!invoiceId) {
+      console.error(
+        'No invoice id was provided for getInvoiceBody. Please supply invoiceId as a prop to InvoicePreview. If you see this, report it in #webtech.',
+      );
+      dispatch({
+        type: ACTIONS.SET_ERROR,
+        data: 'No invoice id was provided for getInvoiceBody. Please supply invoiceId as a prop to InvoicePreview. If you see this, report it in #webtech.',
+      });
+      return;
+    }
+    try {
+      let req = new Document();
+      req.setPropertyId(propertyId);
+      req.setInvoiceId(invoiceId);
+      const res = await EmailClientService.GetInvoiceBody(req);
+      console.log('Got res: ', res);
+      return res;
+    } catch (err) {
+      console.error(`An error occurred while getting an invoice body:`);
+      console.error(err);
+      handleError(err as string);
+    }
+  }, [handleError, invoiceId, propertyId]);
+
+  const load = useCallback(async () => {
+    await getInvoiceBody();
+    dispatch({ type: ACTIONS.SET_LOADED, data: true });
+  }, [getInvoiceBody]);
 
   useEffect(() => {
     if (!state.isLoaded) load();
 
     return () => {
-      cleanup(); 
+      cleanup();
     };
   }, [load, cleanup, state.isLoaded]);
 
-  return ( 
-    <> 
-      <h1>InvoicePreview works!</h1>
-      <h2>Ideas for What To Do</h2>
-      <ul>
-        <li>You can add more tests for your component as you write your functionality (preferably to a spec or design document of some kind)</li>
-        <li>Please ensure your component can properly display errors (for a good example, look at errors in <code>InfoTable</code>)</li>
-        <li>Error-handling code was automatically generated as well - please use this whenever an issue occurs (so we can look back on them when an issue arises)</li>
-      </ul>
-      <h2>About this boilerplate</h2>
-      <ul>
-        <li>Frontend unit tests were created for this module automatically in <code>/test/modules/ComponentsLibrary/InvoicePreview</code></li>
-        <li>You can see the templates for <code>yarn make</code> inside <code>/templates</code></li>
-      </ul>
-      <h2>Some resources to get you started with unit testing in Mocha with Enzyme: </h2>
-      <ul>
-        <li><a href="https://www.robinwieruch.de/react-testing-mocha-chai-enzyme-sinon">This article</a> is extremely long, but it <strong>MOSTLY</strong> shows you how to do what we aim to do</li>
-        <li>There is a readme in <code>/test</code> that goes into how to get started from a technical standpoint in our current setup</li>
-        <li><code>/modules/ComponentsLibrary/Test</code> is the component that was unit-tested on call during our meeting, and <code>/test/modules/ComponentsLibrary/Test</code> is where its corresponding tests are located</li> 
-      </ul>
+  return (
+    <>
+      <Typography variant="h1">Invoice Preview</Typography>
+      {state.error && (
+        <>
+          <Typography variant="h2">
+            An error occurred while getting the invoice body:{' '}
+          </Typography>
+          <Typography variant="h4">{state.error}</Typography>
+        </>
+      )}
     </>
   );
 };
