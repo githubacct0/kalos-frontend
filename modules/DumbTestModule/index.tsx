269773021c1add3a969bda7f209d12548074817c
githubacct0 <githubacct0@outlook.com>
2022-04-15 13:29:31 -0400

    removing DumbTestModule

--- modules/DumbTestModule/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom' 
-import { DumbTestModule } from './main' 
-import { UserClientService } from '../../helpers'
-
-UserClientService.GetToken('test','test').then(() => {
-  ReactDOM.render(<DumbTestModule loggedUserId={8418} />, document.getElementById('root'))
-})
